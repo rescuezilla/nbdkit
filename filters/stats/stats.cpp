rescuezilla/nbdkit

commit 9f0226e3c29952578d70da614b49fde6114eb8e7

stats-filter: bucket request sizes and only count successful requests
(1) Group requests into buckets corresponding to the number of bits needed to represent
the request size. Keeping track of every encountered request size has turned out often to
be useless in practice since e.g. ZFS can issue a very large number of differently sizes
requests.

(2) Record only successful requests in the histogram to make the histogram consistent with
the total.

(3) Simplify the code by handling the collection of histogram data in the same structure
and function as the other statistics.

(4) Introduce an assertion when the histogram total differs from the separately calculated
total (to track down issue #7).

Some code had to be moved around to ensure that functions are defined before they are
called.

filters/stats/stats.cpp
@@ -62,22 +62,27 @@ static FILE *fp;
 static struct timeval start_t;
 static double print_threshold = 0.95;
 
+
 typedef struct {
   const char *name;
   uint64_t ops;
   uint64_t bytes;
   uint64_t usecs;
+
+  /* Keeps track of the number of request sizes and alignments. Requests
+   * are split into buckets by the number of bits needed to represent
+   * their size (i.e., floor(log2(req_size))), and the number
+   * of trailing zero-bits in the offset.
+   *
+   * The outer map is indexed by size bits, the inner by alignment bits.
+   * The value is the count of such requests. */
+  std::unordered_map<int,
+    std::unordered_map<int, uint64_t>> count;
+
+  /* Keeps tracks of the aggregated size of all requests in a given
+   * request size bucket. */
+  std::unordered_map<int, uint64_t> size;
 } nbdstat;
-
-/* Keeps track of the number of requests with a given block size and alignment.
- * Alignment is defined as the number of trailing zero-bytes in the offset.
- *
- * The outer map is indexed by block size. The inner map is indexed by
- * alignment bits. The value is the number of requests with the given
- *  block size and alignment. */
-typedef std::unordered_map<size_t,
-  std::unordered_map<int, size_t> > blksize_hist_t;
-
 
 /* This lock protects all the stats. */
 static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
@@ -88,23 +93,20 @@ static nbdstat zero_st    = { "zero" };
 static nbdstat extents_st = { "extents" };
 static nbdstat cache_st   = { "cache" };
 static nbdstat flush_st   = { "flush" };
-static blksize_hist_t blksize_pread_st;
-static blksize_hist_t blksize_pwrite_st;
-static blksize_hist_t blksize_trim_st;
-static blksize_hist_t blksize_zero_st;
+
 
 #define KiB 1024
 #define MiB 1048576
 #define GiB 1073741824
 
 static int
-get_alignment (size_t offset)
+get_alignment (uint64_t offset)
 {
   /* Cache most common alignments */
   static int powers[] = {
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
   };
-  static size_t masks[] = {
+  static uint64_t masks[] = {
     0, (1 << 1) - 1, (1 << 2) - 1, (1 << 3) - 1, (1 << 4) - 1, (1 << 5) - 1, (1 << 6) - 1,
     (1 << 7) - 1, (1 << 8) - 1, (1 << 9) - 1, (1 << 10) - 1, (1 << 11) - 1, (1 << 12) - 1,
     (1 << 13) - 1, (1 << 14) - 1
@@ -114,15 +116,15 @@ get_alignment (size_t offset)
   if (offset==0)
     return -1;
 
-  size_t i = 0;
-  while(++i < sizeof (powers)/sizeof(powers[0])) {
+  int i = 0;
+  while(++i < static_cast<int> (sizeof (powers)/sizeof(powers[0]))) {
     if ((offset & masks[i]) != 0)
       return i - 1;
   }
 
   // Larger alignments
   while (1) {
-    size_t mask = (1ul << i) - 1;
+    uint64_t mask = (1ul << i) - 1;
     if ((offset & mask) != 0)
       return i - 1;
     i++;
@@ -162,6 +164,112 @@ maybe (char *s)
 }
 
 static void
+print_align_hist(const std::unordered_map<int, uint64_t>& align_map)
+{
+  /* Convert to ordered map (convenient, since we need to mutate counts),
+   * find requests for offset zero (any alignment), and calculate total. */
+  std::map<int, uint64_t> align_hist;
+  uint64_t any_align_count = 0;
+  uint64_t total = 0;
+  for (auto &el : align_map) {
+    int bits = el.first;
+    auto requests = el.second;
+    if (bits == -1) {
+      any_align_count = requests;
+    } else {
+      align_hist[bits] = requests;
+    }
+    total += requests;
+  }
+
+  /* "Fix-up" alignment counts (requests with 8-bit alignment also have
+   *  7-bit alignment, 6-bit alignment, etc) */
+  for (auto &el : align_hist) {
+    int bits = el.first;
+    auto requests = el.second;
+    while (--bits >= 0) {
+      auto it = align_hist.find(bits);
+      if (it != align_hist.end())
+        it->second += requests;
+    }
+    el.second += any_align_count;
+  }
+
+  /* The smallest alignment must have the largest number of requests, so we
+   * can iterate in map-order, skipping over bits for which the number of
+   * requests does not change */
+  auto it = align_hist.begin();
+  auto cutoff = static_cast<uint64_t> ((1-print_threshold) * total);
+  while(it != align_hist.end()) {
+    auto bits = it->first;
+    auto requests = it->second;
+
+    if (requests < cutoff) {
+      fprintf (fp, "         %2d+ bit-aligned: %4.1f%% (%" PRIu64 ")\n",
+               bits, static_cast<double> (requests) / total * 100, requests);
+      break;
+    }
+
+    // Only print if number of requests differs from the next alignment
+    it++;
+    if (it == align_hist.end() || it->second != requests) {
+      fprintf (fp, "         %2d bit aligned: %5.1f%% (%" PRIu64 ")\n",
+              bits, static_cast<double>(requests*100) / total, requests);
+    }
+  }
+}
+
+static void
+print_histogram (const nbdstat *st)
+{
+  // Aggregate over alignment and invert map (so counts are keys and
+  // request size bits are values)
+  double total = 0;
+  std::map<uint64_t, int> req_count_size_m;
+  for (auto &el1 : st->count) {
+    auto &align_map = el1.second;
+    uint64_t requests = 0;
+    for (auto &el2 : align_map) {
+      requests += el2.second;
+    }
+    req_count_size_m[requests] = el1.first;
+    total += static_cast<double> (requests);
+  }
+  if (st->ops != static_cast<uint64_t> (total)) {
+    fprintf(stderr, "INTERNAL ERROR: per-bucket count (%f) does "
+            "not match total (%" PRIu64 ")!\n", total, st->ops);
+    abort();
+  }
+
+  /* Print block sizes until we have covered the *print_threshold* percentile */
+  auto to_print = static_cast<uint64_t> (print_threshold * total);
+  uint64_t printed_reqs = 0, printed_sizes = 0;
+  for (auto it = req_count_size_m.rbegin(); it != req_count_size_m.rend(); it++) {
+    if (printed_reqs >= to_print) {
+      auto requests = st->ops - printed_reqs;
+      char *total_size = humansize(st->bytes - printed_sizes);
+      fprintf (fp, "    other sizes: %4.1f%% (%" PRIu64 " reqs, %s total)\n",
+               static_cast<double> (requests) / total * 100,
+               requests, total_size);
+      free(total_size);
+      break;
+    }
+
+    auto bits = it->second;
+    auto requests = it->first;
+    char *total_size = humansize(st->size.at(bits));
+    fprintf (fp, "    %2d bits: %4.1f%% (%" PRIu64 " reqs, %s total)\n", bits,
+             static_cast<double> (requests) / total * 100, requests,
+             total_size);
+    free(total_size);
+    printed_reqs += requests;
+    total_size += st->size.at(bits);
+
+    print_align_hist (st->count.at(bits));
+  }
+}
+
+static void
 print_stat (const nbdstat *st, int64_t usecs)
 {
   if (st->ops > 0) {
@@ -176,6 +284,12 @@ print_stat (const nbdstat *st, int64_t usecs)
     free (size);
     free (op_rate);
     free (total_rate);
+
+    if (print_threshold != 0 and st->count.size() != 0) {
+      fprintf (fp, "  Request size and alignment breakdown:\n"),
+      print_histogram (st);
+      fprintf (fp, "\n");
+    }
   }
 }
 
@@ -196,142 +310,6 @@ print_totals (uint64_t usecs)
   free (rate);
 }
 
-static void
-inc_blksize_ctr (blksize_hist_t &hist, size_t blksize, size_t offset)
-{
-  static bool out_of_memory = false;
-  if (out_of_memory || print_threshold == 0)
-    return;
-
-  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
-
-  try {
-    hist[blksize][get_alignment (offset)]++;
-  }
-  catch (std::bad_alloc const&) {
-    // Avoid reporting the same error over and over again
-    nbdkit_error ("out of memory for blocksize statistics");
-    out_of_memory = true;
-    return;
-  }
-}
-
-static void
-print_align_hist(const std::unordered_map<int, size_t>& align_map)
-{
-  /* Convert to ordered map (convenient, since we need to mutate counts),
-   * find requests for offset zero (any alignment), and calculate total. */
-  std::map<int, size_t> align_hist;
-  size_t any_align_count = 0;
-  size_t total = 0;
-  for (auto &el : align_map) {
-    int bits = el.first;
-    size_t requests = el.second;
-    if (bits == -1) {
-      any_align_count = requests;
-    } else {
-      align_hist[bits] = requests;
-    }
-    total += requests;
-  }
-
-  /* "Fix-up" alignment counts (requests with 8-bit alignment also have
-   *  7-bit alignment, 6-bit alignment, etc) */
-  for (auto &el : align_hist) {
-    int bits = el.first;
-    size_t requests = el.second;
-    while (--bits >= 0) {
-      auto it = align_hist.find(bits);
-      if (it != align_hist.end())
-        it->second += requests;
-    }
-    el.second += any_align_count;
-  }
-
-  /* The smallest alignment must have the largest number of requests, so we
-   * can iterate in map-order, skipping over bits for which the number of
-   * requests does not change */
-  auto it = align_hist.begin();
-  auto cutoff = static_cast<size_t> ((1-print_threshold) * total);
-  while(it != align_hist.end()) {
-    auto bits = it->first;
-    auto requests = it->second;
-
-    if (requests < cutoff) {
-      fprintf (fp, "         %2d+ bit-aligned: %4.1f%% (%zu)\n",
-               bits, static_cast<double> (requests) / total * 100, requests);
-      break;
-    }
-
-    // Only print if number of requests differs from the next alignment
-    it++;
-    if (it == align_hist.end() || it->second != requests) {
-      fprintf (fp, "         %2d bit aligned: %5.1f%% (%zu)\n",
-              bits, static_cast<double>(requests*100) / total, requests);
-    }
-  }
-}
-
-static void
-print_histogram (const blksize_hist_t &hist)
-{
-  if (hist.size() == 0) {
-    fprintf (fp, "    (no such requests)\n");
-    return;
-  }
-
-  /* Aggregate per-(blocksize, alignment) request counts to per-blocksize counts
-   * and compute grand total */
-  double total = 0;
-  std::map<size_t, size_t> rq_blksize_m;
-  for (auto &el1 : hist) {
-    auto blocksize = el1.first;
-    auto &align_map = el1.second;
-    size_t requests = 0;
-    for (auto &el : align_map) {
-      requests += el.second;
-      total += static_cast<double> (el.second);
-    }
-    rq_blksize_m[requests] = blocksize;
-  }
-
-  /* Print block sizes until we have covered the *print_threshold* percentile */
-  size_t to_print = static_cast<size_t> (print_threshold * total);
-  size_t printed = 0;
-  for (auto it = rq_blksize_m.rbegin(); it != rq_blksize_m.rend(); it++) {
-    if (printed >= to_print) {
-      size_t requests = total - printed;
-      fprintf (fp, "    other sizes: %4.1f%% of requests (%zu)\n",
-               static_cast<double> (requests) / total * 100, requests);
-      break;
-    }
-
-    size_t blocksize = it->second;
-    size_t requests = it->first;
-    fprintf (fp, "%9zu bytes: %4.1f%% of requests (%zu)\n",
-             blocksize, static_cast<double> (requests) / total * 100, requests);
-    printed += requests;
-
-    print_align_hist(hist.at(blocksize));
-  }
-}
-
-static void
-print_blocksize_stats (void)
-{
-  fprintf (fp, "\nREAD request sizes:\n");
-  print_histogram (blksize_pread_st);
-
-  fprintf (fp, "\nWRITE request sizes:\n");
-  print_histogram (blksize_pwrite_st);
-
-  fprintf (fp, "\nTRIM request sizes:\n");
-  print_histogram (blksize_trim_st);
-
-  fprintf (fp, "\nZERO request sizes:\n");
-  print_histogram (blksize_zero_st);
-}
-
 static inline void
 print_stats (int64_t usecs)
 {
@@ -343,8 +321,6 @@ print_stats (int64_t usecs)
   print_stat (&extents_st, usecs);
   print_stat (&cache_st,   usecs);
   print_stat (&flush_st,   usecs);
-  if (print_threshold != 0)
-    print_blocksize_stats ();
   fflush (fp);
 }
 
@@ -448,7 +424,8 @@ stats_get_ready (int thread_model)
   "statsappend=<BOOL>  True to append to the log (default false).\n"
 
 static inline void
-record_stat (nbdstat *st, uint32_t count, const struct timeval *start)
+record_stat (nbdstat *st, uint32_t size, uint64_t offset,
+            const struct timeval *start)
 {
   struct timeval end;
   uint64_t usecs;
@@ -456,10 +433,40 @@ record_stat (nbdstat *st, uint32_t count, const struct timeval *start)
   gettimeofday (&end, NULL);
   usecs = tvdiff_usec (start, &end);
 
+  // fast path if not collecting histogram data
+  static bool out_of_memory = false;
+  if (out_of_memory || print_threshold == 0 || size == 0) {
+    ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
+    st->ops++;
+    st->bytes += size;
+    st->usecs += usecs;
+    return;
+  }
+
+  // Calculate bits needed to represent request size
+  int size_bits = 0;
+  auto tmp = size;
+  while (tmp >>= 1) {
+      size_bits++;
+  }
+
+  // Calculate trailing zero bits
+  int align_bits = get_alignment (offset);
+
   ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&lock);
   st->ops++;
-  st->bytes += count;
+  st->bytes += size;
   st->usecs += usecs;
+
+  try {
+    st->count[size_bits][align_bits]++;
+    st->size[size_bits] += size;
+  }
+  catch (std::bad_alloc const&) {
+    // Avoid reporting the same error over and over again
+    nbdkit_error ("out of memory for request size statistics");
+    out_of_memory = true;
+  }
 }
 
 /* Read. */
@@ -471,10 +478,9 @@ stats_pread (nbdkit_next *next,
   struct timeval start;
   int r;
 
-  inc_blksize_ctr (blksize_pread_st, count, offset);
   gettimeofday (&start, NULL);
   r = next->pread (next, buf, count, offset, flags, err);
-  if (r == 0) record_stat (&pread_st, count, &start);
+  if (r == 0) record_stat (&pread_st, count, offset, &start);
   return r;
 }
 
@@ -488,10 +494,9 @@ stats_pwrite (nbdkit_next *next,
   struct timeval start;
   int r;
 
-  inc_blksize_ctr (blksize_pwrite_st, count, offset);
   gettimeofday (&start, NULL);
   r = next->pwrite (next, buf, count, offset, flags, err);
-  if (r == 0) record_stat (&pwrite_st, count, &start);
+  if (r == 0) record_stat (&pwrite_st, count, offset, &start);
   return r;
 }
 
@@ -505,10 +510,9 @@ stats_trim (nbdkit_next *next,
   struct timeval start;
   int r;
 
-  inc_blksize_ctr (blksize_trim_st, count, offset);
   gettimeofday (&start, NULL);
   r = next->trim (next, count, offset, flags, err);
-  if (r == 0) record_stat (&trim_st, count, &start);
+  if (r == 0) record_stat (&trim_st, count, offset, &start);
   return r;
 }
 
@@ -523,7 +527,7 @@ stats_flush (nbdkit_next *next,
 
   gettimeofday (&start, NULL);
   r = next->flush (next, flags, err);
-  if (r == 0) record_stat (&flush_st, 0, &start);
+  if (r == 0) record_stat (&flush_st, 0, 0, &start);
   return r;
 }
 
@@ -537,10 +541,9 @@ stats_zero (nbdkit_next *next,
   struct timeval start;
   int r;
 
-  inc_blksize_ctr (blksize_zero_st, count, offset);
   gettimeofday (&start, NULL);
   r = next->zero (next, count, offset, flags, err);
-  if (r == 0) record_stat (&zero_st, count, &start);
+  if (r == 0) record_stat (&zero_st, count, offset, &start);
   return r;
 }
 
@@ -560,7 +563,7 @@ stats_extents (nbdkit_next *next,
    * will be that are returned to the client (instead of simply using
    * count), given the flags and the complex rules in the protocol.
    */
-  if (r == 0) record_stat (&extents_st, count, &start);
+  if (r == 0) record_stat (&extents_st, count, offset, &start);
   return r;
 }
 
@@ -576,7 +579,7 @@ stats_cache (nbdkit_next *next,
 
   gettimeofday (&start, NULL);
   r = next->cache (next, count, offset, flags, err);
-  if (r == 0) record_stat (&cache_st, count, &start);
+  if (r == 0) record_stat (&cache_st, count, offset, &start);
   return r;
 }
 

filters/stats/nbdkit-stats-filter.pod
@@ -15,47 +15,55 @@ are written to a file once when nbdkit exits.
 
 =head1 EXAMPLE OUTPUT
 
- # nbdkit --filter=exitfirst --filter=stats memory 25G statsfile=example.txt
- # nbd-client localhost /dev/nbd1 && mkfs.ext4 /dev/nbd1 &&
+ # nbdkit --filter=exitlast --filter=stats memory 25G statsfile=example.txt
+ # nbd-client localhost /dev/nbd1 && mkfs.ext4 /dev/nbd1 && sync &&
    nbd-client -d /dev/nbd1
  [....]
  # cat example.txt
- total: 1252 ops, 3.395897 s, 25.13 GiB, 7.40 GiB/s
- read: 145 ops, 0.000133 s, 3.04 MiB, 22.34 GiB/s op, 917.58 KiB/s total
- write: 1088 ops, 0.065065 s, 132.38 MiB, 1.99 GiB/s op, 38.98 MiB/s total
- trim: 14 ops, 0.002037 s, 25.00 GiB, 12272.95 GiB/s op, 7.36 GiB/s total
+ total: 191 ops, 21.917545 s, 25.13 GiB, 1.15 GiB/s
+ read: 136 ops, 0.000230 s, 3.16 MiB, 13.43 GiB/s op, 147.83 KiB/s total
+   Request size and alignment breakdown:
+     12 bits: 68.4% (93 reqs, 372.00 KiB total)
+          12 bit aligned: 100.0% (93)
+          13 bit aligned:  72.0% (67)
+          14 bit aligned:  55.9% (52)
+          15 bit aligned:  48.4% (45)
+          16 bit aligned:  43.0% (40)
+     14 bits: 10.3% (14 reqs, 296.00 KiB total)
+          12 bit aligned: 100.0% (14)
+          13 bit aligned:  35.7% (5)
+          19 bit aligned:  21.4% (3)
+ [....]
+     other sizes:  3.7% (5 reqs, 3.16 MiB total)
+
+ write: 36 ops, 0.125460 s, 132.38 MiB, 1.03 GiB/s op, 6.04 MiB/s total
+   Request size and alignment breakdown:
+     12 bits: 50.0% (18 reqs, 72.00 KiB total)
+          12 bit aligned: 100.0% (18)
+          13 bit aligned:  77.8% (14)
+          27 bit aligned:  72.2% (13)
+          31 bit aligned:  66.7% (12)
+          32 bit aligned:  33.3% (6)
+          33 bit aligned:  22.2% (4)
+          34 bit aligned:  11.1% (2)
+     14 bits: 30.6% (11 reqs, 224.00 KiB total)
+          12 bit aligned: 100.0% (11)
+          27 bit aligned:  90.9% (10)
+     25 bits: 11.1% (4 reqs, 128.00 MiB total)
+          25 bit aligned: 100.0% (4)
+          26 bit aligned:  50.0% (2)
+          27 bit aligned:  25.0% (1)
+     16 bits:  2.8% (1 reqs, 64.00 KiB total)
+          16 bit aligned: 100.0% (1)
+
+ trim: 14 ops, 0.002687 s, 25.00 GiB, 9304.06 GiB/s op, 1.14 GiB/s total
+   Request size and alignment breakdown:
+     31 bits: 85.7% (12 reqs, 24.00 GiB total)
+          24 bit aligned: 100.0% (12)
+     24 bits:  7.1% (1 reqs, 16.00 MiB total)
+
  flush: 5 ops, 0.000002 s, 0 bytes, 0 bytes/s op, 0 bytes/s total
 
- READ request sizes:
-      4096 bytes: 66.2% of requests (96)
-          12 bit aligned: 100.0% (96)
-          13 bit aligned:  69.8% (67)
-          14 bit aligned:  54.2% (52)
-          15 bit aligned:  46.9% (45)
-          16 bit aligned:  41.7% (40)
-          17 bit aligned:  36.5% (35)
-          18 bit aligned:  31.2% (30)
-          19+ bit-aligned: 28.1% (27)
-     16384 bytes:  4.8% of requests (7)
-          13 bit aligned: 100.0% (7)
-          17 bit aligned:  71.4% (5)
-          19 bit aligned:  42.9% (3)
-     other sizes: 29.0% of requests (42)
-
- WRITE request sizes:
-    131072 bytes: 97.1% of requests (1056)
-          17 bit aligned: 100.0% (1056)
-          18 bit aligned:  50.0% (528)
-          19+ bit-aligned: 25.0% (264)
-     other sizes:  2.9% of requests (32)
-
- TRIM request sizes:
- 2147483648 bytes: 85.7% of requests (12)
-          24 bit aligned: 100.0% (12)
-     other sizes: 14.3% of requests (2)
-
- ZERO request sizes:
-     (no such requests)
 
 =head1 PARAMETERS
 
@@ -73,9 +81,15 @@ If set then we append to the file instead of replacing it.
 
 =item B<statsthreshold=>PERCENTILE
 
-Percentile of requests for which block size histogram should be printed.
-Set to zero to disable collection of block size statistics. Set to 100
-to get a list of every block size encountered. Default: 95.
+If non-zero, track request size and alignment distribution and print
+histogram.
+
+Requests are grouped by the number of bits that are needed to represent
+the request size (e.g. a request of size 3892 needs will be grouped
+with all requests of size 2^11=2048 to 2^12-1=4095), and the number of
+trailing zero bits in the offset.
+
+Histogram output is truncated to PERCENTILE of requests. Default: 95.
 
 =back
 

commit 0b696f8011531020f376646c057d45e4be94d478

stats: Remove bogus adjustment of freed pointer
The code freed a pointer and then (apparently in error) added a number
to the pointer.  Although simply adding to but never using such a
pointer wouldn't usually cause a problem, it contravenes the spec, and
GCC warns about it.  Since the adjustment is unused, remove it.

stats.cpp: In function 'void print_histogram(const nbdstat*)':
stats.cpp:266:16: error: pointer 'total_size' may be used after 'void free(void*)' [-Werror=use-after-free]
  266 |     total_size += st->size.at(bits);
      |     ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~
stats.cpp:264:9: note: call to 'void free(void*)' here
  264 |     free(total_size);
      |     ~~~~^~~~~~~~~~~~

Fixes: commit 9f0226e3c29952578d70da614b49fde6114eb8e7

filters/stats/stats.cpp
@@ -263,7 +263,6 @@ print_histogram (const nbdstat *st)
              total_size);
     free(total_size);
     printed_reqs += requests;
-    total_size += st->size.at(bits);
 
     print_align_hist (st->count.at(bits));
   }