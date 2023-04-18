rescuezilla/nbdkit

commit 6b03ec8ad672e5956b41cbacae9c307e5acd786b

common: Move exit-with-parent code from include/ to utils/
The internal API changes with a new can_exit_with_parent function
replacing the old macro, but this is basically a refactoring and adds
no new functionality.

server/main.c
@@ -260,17 +260,18 @@ main (int argc, char *argv[])
       break;
 
     case EXIT_WITH_PARENT_OPTION:
-#ifdef HAVE_EXIT_WITH_PARENT
-      exit_with_parent = true;
-      foreground = true;
+      if (can_exit_with_parent ()) {
+        exit_with_parent = true;
+        foreground = true;
+      }
+      else {
+        fprintf (stderr,
+                 "%s: --exit-with-parent is not implemented "
+                 "for this operating system\n",
+                 program_name);
+        exit (EXIT_FAILURE);
+      }
       break;
-#else
-      fprintf (stderr,
-               "%s: --exit-with-parent is not implemented "
-               "for this operating system\n",
-               program_name);
-      exit (EXIT_FAILURE);
-#endif
 
     case FILTER_OPTION:
       {
@@ -567,14 +568,10 @@ main (int argc, char *argv[])
   /* Implement --exit-with-parent early in case plugin initialization
    * takes a long time and the parent exits during that time.
    */
-#ifdef HAVE_EXIT_WITH_PARENT
-  if (exit_with_parent) {
-    if (set_exit_with_parent () == -1) {
-      perror ("nbdkit: --exit-with-parent");
-      exit (EXIT_FAILURE);
-    }
+  if (set_exit_with_parent () == -1) {
+    perror ("nbdkit: --exit-with-parent");
+    exit (EXIT_FAILURE);
   }
-#endif
 
   /* If the user has mixed up -p/--run/-s/-U/--vsock options, then
    * give an error.

common/include/Makefile.am
@@ -41,7 +41,6 @@ EXTRA_DIST = \
 	byte-swapping.h \
 	checked-overflow.h \
 	compiler-macros.h \
-	exit-with-parent.h \
 	hexdigit.h \
 	isaligned.h \
 	ispowerof2.h \

common/utils/Makefile.am
@@ -41,6 +41,8 @@ libutils_la_SOURCES = \
 	cleanup.h \
 	const-string-vector.h \
 	environ.c \
+	exit-with-parent.c \
+	exit-with-parent.h \
 	full-rw.c \
 	quote.c \
 	nbdkit-string.h \

common/utils/exit-with-parent.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2013-2020 Red Hat Inc.
+ * Copyright (C) 2013-2022 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -34,11 +34,10 @@
  * support it.
  */
 
-#ifndef NBDKIT_EXIT_WITH_PARENT_H
-#define NBDKIT_EXIT_WITH_PARENT_H
-
 #include <config.h>
 
+#include <stdlib.h>
+#include <stdbool.h>
 #include <signal.h>
 
 #ifdef HAVE_SYS_PRCTL_H
@@ -53,27 +52,47 @@
 
 /* For Linux >= 2.1.57. */
 
-static inline int
+int
 set_exit_with_parent (void)
 {
   return prctl (PR_SET_PDEATHSIG, SIGTERM);
 }
 
-#define HAVE_EXIT_WITH_PARENT 1
+bool
+can_exit_with_parent (void)
+{
+  return true;
+}
 
 #elif defined(HAVE_SYS_PROCCTL_H) && defined(PROC_PDEATHSIG_CTL)
 
 /* For FreeBSD >= 11.2 */
 
-static inline int
+int
 set_exit_with_parent (void)
 {
   const int sig = SIGTERM;
   return procctl (P_PID, 0, PROC_PDEATHSIG_CTL, (void*) &sig);
 }
 
-#define HAVE_EXIT_WITH_PARENT 1
+bool
+can_exit_with_parent (void)
+{
+  return true;
+}
+
+#else /* any platform that doesn't support this function */
+
+int
+set_exit_with_parent (void)
+{
+  abort ();
+}
+
+bool
+can_exit_with_parent (void)
+{
+  return false;
+}
 
 #endif
-
-#endif /* NBDKIT_INTERNAL_H */

common/utils/exit-with-parent.h
@@ -1,0 +1,46 @@
+/* nbdkit
+ * Copyright (C) 2013-2022 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef NBDKIT_EXIT_WITH_PARENT_H
+#define NBDKIT_EXIT_WITH_PARENT_H
+
+#include <stdbool.h>
+
+/* Test if the feature is available on the platform. */
+extern bool can_exit_with_parent (void);
+
+/* --exit-with-parent: kill the current process if the parent exits.
+ * This may return -1 on error.
+ */
+extern int set_exit_with_parent (void);
+
+#endif /* NBDKIT_EXIT_WITH_PARENT_H */

tests/Makefile.am
@@ -489,11 +489,14 @@ test_exit_with_parent_SOURCES = \
 	test-exit-with-parent.c test.h \
 	$(NULL)
 test_exit_with_parent_CPPFLAGS = \
-	-I$(top_srcdir)/common/include \
+	-I$(top_srcdir)/common/utils \
 	$(NULL)
 test_exit_with_parent_CFLAGS = \
 	$(WARNINGS_CFLAGS) \
 	$(LIBGUESTFS_CFLAGS) \
+	$(NULL)
+test_exit_with_parent_LDFLAGS = \
+	$(top_builddir)/common/utils/libutils.la \
 	$(NULL)
 
 # PKI files for the TLS tests.

tests/test-exit-with-parent.c
@@ -49,8 +49,6 @@
 #include "exit-with-parent.h"
 #include "test.h"
 
-#ifdef HAVE_EXIT_WITH_PARENT
-
 static char pidpath[] = "/tmp/nbdkitpidXXXXXX";
 
 static void run_test (void);
@@ -58,6 +56,13 @@ static void run_test (void);
 int
 main (int argc, char *argv[])
 {
+  if (! can_exit_with_parent ()) {
+    printf ("%s: --exit-with-parent is not implemented on this platform, "
+            "skipping\n",
+            argv[0]);
+    exit (77);
+  }
+
   run_test ();
   exit (EXIT_SUCCESS);
 }
@@ -192,14 +197,3 @@ run_test (void)
  done:
   ;
 }
-
-#else /* !HAVE_EXIT_WITH_PARENT */
-
-int
-main (int argc, char *argv[])
-{
-  printf ("--exit-with-parent is not implemented on this platform, skipping\n");
-  exit (77);
-}
-
-#endif /* !HAVE_EXIT_WITH_PARENT */

commit beae44398ddd5386add34b43353ed16e2df0f30e

server: Fix exit with parent behaviour if flag is not specified
Commit 6b03ec8ad6 ("common: Move exit-with-parent code from include/
to utils/") was supposed to be a largely neutral refactoring of the
--exit-with-parent flag.  However I made a mistake in one hunk of that
patch:

    -#ifdef HAVE_EXIT_WITH_PARENT
    -  if (exit_with_parent) {
    -    if (set_exit_with_parent () == -1) {
    -      perror ("nbdkit: --exit-with-parent");
    -      exit (EXIT_FAILURE);
    -    }
    +  if (set_exit_with_parent () == -1) {
    +    perror ("nbdkit: --exit-with-parent");
    +    exit (EXIT_FAILURE);
       }
    -#endif

by removing the test for exit_with_parent.  The effect is similar to
if we always specified --exit-with-parent when nbdkit is running in
the foreground.

Somewhat surprisingly this didn't have any noticable effect.  That's
for a few of reasons: (1) Mostly if you don't fork then you should be
using --exit-with-parent (and the bulk of the tests do this).  (2) If
we do fork into the background then the PR_SET_PDEATHSIG flag is
cleared by Linux across the fork.  (3) It actually _did_ break Windows
because there set_exit_with_parent calls abort, but for unrelated
reasons our Windows CI was broken (and never tested running
nbdkit.exe).

Fix this with the obvious adjustment.

This broke nbdkit 1.32 and 1.34.

Fixes: commit 6b03ec8ad672e5956b41cbacae9c307e5acd786b
Reviewed-by: Laszlo Ersek <lersek@redhat.com>

server/main.c
@@ -574,9 +574,11 @@ main (int argc, char *argv[])
   /* Implement --exit-with-parent early in case plugin initialization
    * takes a long time and the parent exits during that time.
    */
-  if (set_exit_with_parent () == -1) {
-    perror ("nbdkit: --exit-with-parent");
-    exit (EXIT_FAILURE);
+  if (exit_with_parent) {
+    if (set_exit_with_parent () == -1) {
+      perror ("nbdkit: --exit-with-parent");
+      exit (EXIT_FAILURE);
+    }
   }
 
   /* If the user has mixed up -p/--run/-s/-U/--vsock options, then