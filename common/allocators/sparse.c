rescuezilla/nbdkit

commit 9b5b93a0737e4770faa0b66d1ae292438714cd0d

allocators: sparse: Improve performance by using a read-write lock
We've long known that nbdkit-memory-plugin with the default sparse
array allocator suffers because a global mutex must be taken whenever
any read or write request is performed.  This commit aims to safely
improve performance by converting the mutex into a read-write lock.

The shared (read) lock still must be acquired for any access to the
sparse array.  However if you wish to make any changes to the L1/L2
directory, which includes allocating or deallocating pages, then you
must upgrade to the exclusive (write) lock.

Unfortunately POSIX read-write locks are not upgradable, and previous
attempts to work around this lead to many potential safety issues
and/or deadlocks.  For example if at the point where you want to
update the metadata, you unlock the shared lock and attempt to acquire
the exclusive lock, then other threads can jump in and modify the
metadata underneath you.  It's very difficult to work around this
limitation safely.  This patch takes a different approach: We attempt
write requests using the shared lock, but if we detect that we need to
update the metadata during an operation then we retry the entire
request from the start with the exclusive lock.  This should be safe,
albeit slower.

Either the test program (contrib/sparseloadtest.c), or fio, can be
used to test performance.  The test program is better because it
exercises page deallocation, which is not exercised by fio testing.

For unpatched nbdkit 1.34:

  - 4 threads, mix of 80% read, 10% write, 10% trim:

    READ: 77669.5 ops/s 5087519644.9 bytes/s
    WRITE: 9707.1 ops/s 635528046.7 bytes/s
    TRIM: 9712.0 ops/s 636737940.6 bytes/s
    TOTAL: 97088.6 ops/s 6359785632.1 bytes/s

  - 4 threads, mix of 20% read, 40% write, 40% trim:

    READ: 20135.1 ops/s 1318832390.2 bytes/s
    WRITE: 40293.6 ops/s 2640703848.7 bytes/s
    TRIM: 40240.7 ops/s 2636021905.5 bytes/s
    TOTAL: 100669.4 ops/s 6595558144.3 bytes/s

common/allocators/sparse.c
@@ -129,11 +129,20 @@ DEFINE_VECTOR_TYPE (l1_dir, struct l1_entry);
 struct sparse_array {
   struct allocator a;           /* Must come first. */
 
-  /* This lock is highly contended.  When hammering the memory plugin
-   * with 8 fio threads, about 30% of the total system time was taken
-   * just waiting for this lock.  Fixing this is quite hard.
+  /* The shared (read) lock must be held if you just want to access
+   * the data without modifying any of the L1/L2 metadata or
+   * allocating or freeing any page.
+   *
+   * To modify the L1/L2 metadata including allocating or freeing any
+   * page you must hold the exclusive (write) lock.
+   *
+   * Because POSIX rwlocks are not upgradable this presents a problem.
+   * We solve it below by speculatively performing the request while
+   * holding the shared lock, but if we run into an operation that
+   * needs to update the metadata, we restart the entire request
+   * holding the exclusive lock.
    */
-  pthread_mutex_t lock;
+  pthread_rwlock_t lock;
   l1_dir l1_dir;                /* L1 directory. */
 };
 
@@ -158,7 +167,7 @@ sparse_array_free (struct allocator *a)
     for (i = 0; i < sa->l1_dir.len; ++i)
       free_l2_dir (sa->l1_dir.ptr[i].l2_dir);
     free (sa->l1_dir.ptr);
-    pthread_mutex_destroy (&sa->lock);
+    pthread_rwlock_destroy (&sa->lock);
     free (sa);
   }
 }
@@ -305,7 +314,10 @@ sparse_array_read (struct allocator *a,
                    void *buf, uint64_t count, uint64_t offset)
 {
   struct sparse_array *sa = (struct sparse_array *) a;
-  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&sa->lock);
+  /* Because reads never modify any metadata, it is always safe to
+   * only hold the shared (read) lock.
+   */
+  ACQUIRE_RDLOCK_FOR_CURRENT_SCOPE (&sa->lock);
   uint64_t n;
   void *p;
 
@@ -327,19 +339,27 @@ sparse_array_read (struct allocator *a,
   return 0;
 }
 
+#define RESTART_EXCLUSIVE -2
+
 static int
-sparse_array_write (struct allocator *a,
-                    const void *buf, uint64_t count, uint64_t offset)
+do_write (bool exclusive, struct sparse_array *sa,
+          const void *buf, uint64_t count, uint64_t offset)
 {
-  struct sparse_array *sa = (struct sparse_array *) a;
-  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&sa->lock);
   uint64_t n;
   void *p;
 
   while (count > 0) {
-    p = lookup (sa, offset, true, &n, NULL);
-    if (p == NULL)
-      return -1;
+    if (!exclusive) {
+      /* If we only hold the shared lock, try it without allocating. */
+      p = lookup (sa, offset, false, &n, NULL);
+      if (p == NULL)
+        return RESTART_EXCLUSIVE;
+    }
+    else {
+      p = lookup (sa, offset, true, &n, NULL);
+      if (p == NULL)
+        return -1;
+    }
 
     if (n > count)
       n = count;
@@ -351,6 +371,28 @@ sparse_array_write (struct allocator *a,
   }
 
   return 0;
+}
+
+static int
+sparse_array_write (struct allocator *a,
+                    const void *buf, uint64_t count, uint64_t offset)
+{
+  struct sparse_array *sa = (struct sparse_array *) a;
+  int r;
+
+  /* First try the write with the shared (read) lock held. */
+  {
+    ACQUIRE_RDLOCK_FOR_CURRENT_SCOPE (&sa->lock);
+    r = do_write (false, sa, buf, count, offset);
+  }
+
+  /* If that failed because we need the exclusive lock, restart. */
+  if (r == RESTART_EXCLUSIVE) {
+    ACQUIRE_WRLOCK_FOR_CURRENT_SCOPE (&sa->lock);
+    r = do_write (true, sa, buf, count, offset);
+  }
+
+  return r;
 }
 
 static int sparse_array_zero (struct allocator *a,
@@ -367,7 +409,8 @@ sparse_array_fill (struct allocator *a, char c,
   if (c == 0)
     return sparse_array_zero (a, count, offset);
 
-  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&sa->lock);
+  /* Since fill is never called on a hot path, use the exclusive lock. */
+  ACQUIRE_WRLOCK_FOR_CURRENT_SCOPE (&sa->lock);
 
   while (count > 0) {
     p = lookup (sa, offset, true, &n, NULL);
@@ -386,10 +429,9 @@ sparse_array_fill (struct allocator *a, char c,
 }
 
 static int
-sparse_array_zero (struct allocator *a, uint64_t count, uint64_t offset)
+do_zero (bool exclusive, struct sparse_array *sa,
+         uint64_t count, uint64_t offset)
 {
-  struct sparse_array *sa = (struct sparse_array *) a;
-  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&sa->lock);
   uint64_t n;
   void *p;
   struct l2_entry *l2_entry;
@@ -407,6 +449,9 @@ sparse_array_zero (struct allocator *a, uint64_t count, uint64_t offset)
 
       /* If the whole page is now zero, free it. */
       if (n >= PAGE_SIZE || is_zero (l2_entry->page, PAGE_SIZE)) {
+        if (!exclusive)
+          return RESTART_EXCLUSIVE;
+
         if (sa->a.debug)
           nbdkit_debug ("%s: freeing zero page at offset %" PRIu64,
                         __func__, offset);
@@ -423,13 +468,35 @@ sparse_array_zero (struct allocator *a, uint64_t count, uint64_t offset)
 }
 
 static int
+sparse_array_zero (struct allocator *a, uint64_t count, uint64_t offset)
+{
+  struct sparse_array *sa = (struct sparse_array *) a;
+  int r;
+
+  /* First try the zero with the shared (read) lock held. */
+  {
+    ACQUIRE_RDLOCK_FOR_CURRENT_SCOPE (&sa->lock);
+    r = do_zero (false, sa, count, offset);
+  }
+
+  /* If that failed because we need the exclusive lock, restart. */
+  if (r == RESTART_EXCLUSIVE) {
+    ACQUIRE_WRLOCK_FOR_CURRENT_SCOPE (&sa->lock);
+    r = do_zero (true, sa, count, offset);
+  }
+
+  return r;
+}
+
+static int
 sparse_array_blit (struct allocator *a1,
                    struct allocator *a2,
                    uint64_t count,
                    uint64_t offset1, uint64_t offset2)
 {
   struct sparse_array *sa2 = (struct sparse_array *) a2;
-  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&sa2->lock);
+  /* Since blit is never called on a hot path, use the exclusive lock. */
+  ACQUIRE_WRLOCK_FOR_CURRENT_SCOPE (&sa2->lock);
   uint64_t n;
   void *p;
   struct l2_entry *l2_entry;
@@ -474,7 +541,10 @@ sparse_array_extents (struct allocator *a,
                       struct nbdkit_extents *extents)
 {
   struct sparse_array *sa = (struct sparse_array *) a;
-  ACQUIRE_LOCK_FOR_CURRENT_SCOPE (&sa->lock);
+  /* Reading extents never modifies any metadata, so it is always safe
+   * to only hold the shared (read) lock.
+   */
+  ACQUIRE_RDLOCK_FOR_CURRENT_SCOPE (&sa->lock);
   uint64_t n;
   uint32_t type;
   void *p;
@@ -523,7 +593,7 @@ sparse_array_create (const void *paramsv)
     nbdkit_error ("calloc: %m");
     return NULL;
   }
-  pthread_mutex_init (&sa->lock, NULL);
+  pthread_rwlock_init (&sa->lock, NULL);
 
   return (struct allocator *) sa;
 }

commit eb9c92bc49ed1f4a4eddbd798a4f46a0ce85a5d7

allocators: sparse: Remove out of date comment
Fixes: commit 9b5b93a0737e4770faa0b66d1ae292438714cd0d

common/allocators/sparse.c
@@ -61,9 +61,6 @@
  * sized disks, while permitting huge (but sparse) disks for testing.
  * Everything allocated has to be stored in memory.  There is no
  * temporary file backing.
- *
- * XXX It would be nice to change the locking to use r/w locks here,
- * as that ought to greatly benefit read-heavy loads using multi-conn.
  */
 
 /* Two level directory for the sparse array.