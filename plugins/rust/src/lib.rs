rescuezilla/nbdkit

commit 859bd1f64664bfd02c8789d3d6090b6674d0587b

rust: Use core::ffi::c_char for very old RHEL and Debian
Commit 1742e2d4 ("rust: Use c_char instead of signed type for
aarch64") changed the debug! macro to use c_char instead of i8, to
cope with platforms that use unsigned char.  However this broke very
old versions of Rust:

  AlmaLinux 8 - rust 1.62.1 - broken
  Debian Sid - rust 1.62.1 - broken
  Ubuntu 20.04 - rust 1.61 - broken
  RHEL 8.8 - rust 1.66.1 - working
  Fedora - rust 1.66.1 - working

std::ffi just reexports core::ffi, so it should be possible to use
core::ffi here instead and work on all platforms.  Once the older
distro upgrade we can change this back.

The error was:

error[E0412]: cannot find type `c_char` in module `std::ffi`
  --> examples/ramdisk.rs:72:9
   |
72 |         debug!("connection opened: readonly={}, tls={}", _readonly, tls);
   |         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ not found in `std::ffi`
   |
   = note: this error originates in the macro `debug` (in Nightly builds, run with -Z macro-backtrace for more info)
help: consider importing one of these items
   |
28 | use core::ffi::c_char;
28 | use libc::c_char;
28 | use std::os::raw::c_char;

Updates: commit 1742e2d4335babf27fa2aa7942dc1d0b6501ca7d

plugins/rust/src/lib.rs
@@ -1096,7 +1096,7 @@ macro_rules! debug {
             Ok(msg) =>
                 unsafe {
                     $crate::nbdkit_debug(b"%s\0".as_ptr()
-                                             as *const ::std::ffi::c_char,
+                                             as *const ::core::ffi::c_char,
                                          msg.as_ptr());
                 },
             Err(_) =>

commit f9fff38a39e4c1f7be456cb722c384492427d143

rust: Use libc::c_char instead of {core,std}::ffi::c_char
Thanks: Alan Somers
Fixes: commit 859bd1f64664bfd02c8789d3d6090b6674d0587b
Fixes: https://gitlab.com/nbdkit/nbdkit/-/issues/12

plugins/rust/src/lib.rs
@@ -1096,7 +1096,7 @@ macro_rules! debug {
             Ok(msg) =>
                 unsafe {
                     $crate::nbdkit_debug(b"%s\0".as_ptr()
-                                             as *const ::core::ffi::c_char,
+                                             as *const ::libc::c_char,
                                          msg.as_ptr());
                 },
             Err(_) =>