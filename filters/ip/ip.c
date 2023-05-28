rescuezilla/nbdkit

commit fe3371c2c64f954539042d9f0048e77569404d68

info, ip: Make this plugin and filter work without inet_ntop/inet_pton
Or rather, remove all the functions that cannot work.  This
is for Windows.

filters/ip/ip.c
@@ -84,10 +84,12 @@ static struct rule *deny_rules, *deny_rules_last;
 static void
 print_rule (const char *name, const struct rule *rule, const char *suffix)
 {
+#if defined(INET_NTOP)
   union {
     char addr4[INET_ADDRSTRLEN];
     char addr6[INET6_ADDRSTRLEN];
   } u;
+#endif
 
   switch (rule->type) {
   case ANY:
@@ -99,6 +101,7 @@ print_rule (const char *name, const struct rule *rule, const char *suffix)
   case ANYV6:
     nbdkit_debug ("%s=anyipv6%s", name, suffix);
     break;
+#if defined(INET_NTOP)
   case IPV4:
     inet_ntop (AF_INET, &rule->u.ipv4, u.addr4, sizeof u.addr4);
     nbdkit_debug ("%s=ipv4:%s/%u%s", name, u.addr4, rule->prefixlen, suffix);
@@ -107,6 +110,7 @@ print_rule (const char *name, const struct rule *rule, const char *suffix)
     inet_ntop (AF_INET6, &rule->u.ipv6, u.addr6, sizeof u.addr6);
     nbdkit_debug ("%s=ipv6:[%s]/%u%s", name, u.addr6, rule->prefixlen, suffix);
     break;
+#endif
 
   case ANYUNIX:
     nbdkit_debug ("%s=anyunix%s", name, suffix);
@@ -186,11 +190,13 @@ parse_ip_address (const char *value, size_t n, struct rule *ret)
   strncpy (addr, value, n);
   addr[n] = '\0';
 
+#ifdef INET_PTON
   if (inet_pton (AF_INET, addr, &ret->u.ipv4) == 1)
     return IPV4;
 
   if (inet_pton (AF_INET6, addr, &ret->u.ipv6) == 1)
     return IPV6;
+#endif
 
   return 0;
 }

configure.ac
@@ -373,6 +373,8 @@ AC_CHECK_FUNCS([\
         fdatasync \
         flockfile \
         funlockfile \
+        inet_ntop \
+        inet_pton \
         mkostemp \
         mlock \
         mlockall \

plugins/info/info.c
@@ -99,8 +99,14 @@ info_config (const char *key, const char *value)
       return -1;
 #endif
     }
-    else if (ascii_strcasecmp (value, "address") == 0)
+    else if (ascii_strcasecmp (value, "address") == 0) {
+#ifdef HAVE_INET_NTOP
       mode = MODE_ADDRESS;
+#else
+      nbdkit_error ("the plugin was compiled without inet_ntop");
+      return -1;
+#endif
+    }
     else if (ascii_strcasecmp (value, "time") == 0)
       mode = MODE_TIME;
     else if (ascii_strcasecmp (value, "uptime") == 0)
@@ -130,6 +136,9 @@ info_dump_plugin (void)
 {
 #ifdef HAVE_BASE64
   printf ("info_base64=yes\n");
+#endif
+#ifdef HAVE_INET_NTOP
+  printf ("info_inet_ntop=yes\n");
 #endif
 }
 
@@ -183,6 +192,7 @@ static int
 handle_address (struct sockaddr *sa, socklen_t addrlen,
                 struct handle *ret)
 {
+#ifdef HAVE_INET_NTOP
   struct sockaddr_in *addr = (struct sockaddr_in *) sa;
   struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *) sa;
   union {
@@ -245,6 +255,10 @@ handle_address (struct sockaddr *sa, socklen_t addrlen,
     ret->len = 0;
     return 0;
   }
+#else
+  nbdkit_error ("the plugin was compiled without inet_ntop");
+  return -1;
+#endif
 }
 
 /* Create the per-connection handle.

commit 168d56d8154e388adcd9594c306fdaf69c857db3

ip: Fix test for inet_ntop/inet_pton
In commit fe3371c2c6 ("info, ip: Make this plugin and filter work
without inet_ntop/inet_pton") I fixed a build problem on Windows which
does not have inet_ntop.  Unfortunately I used the wrong macro name so
inet_ntop / inet_pton support was always disabled in this filter.
This was only used to implement -D ip.rules=1

Fixes: commit fe3371c2c64f954539042d9f0048e77569404d68

filters/ip/ip.c
@@ -86,7 +86,7 @@ static struct rule *deny_rules, *deny_rules_last;
 static void
 print_rule (const char *name, const struct rule *rule, const char *suffix)
 {
-#if defined (INET_NTOP)
+#ifdef HAVE_INET_NTOP
   union {
     char addr4[INET_ADDRSTRLEN];
     char addr6[INET6_ADDRSTRLEN];
@@ -103,7 +103,7 @@ print_rule (const char *name, const struct rule *rule, const char *suffix)
   case ANYV6:
     nbdkit_debug ("%s=anyipv6%s", name, suffix);
     break;
-#if defined (INET_NTOP)
+#ifdef HAVE_INET_NTOP
   case IPV4:
     inet_ntop (AF_INET, &rule->u.ipv4, u.addr4, sizeof u.addr4);
     nbdkit_debug ("%s=ipv4:%s/%u%s", name, u.addr4, rule->prefixlen, suffix);
@@ -192,7 +192,7 @@ parse_ip_address (const char *value, size_t n, struct rule *ret)
   strncpy (addr, value, n);
   addr[n] = '\0';
 
-#ifdef INET_PTON
+#ifdef HAVE_INET_PTON
   if (inet_pton (AF_INET, addr, &ret->u.ipv4) == 1)
     return IPV4;
 