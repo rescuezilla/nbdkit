rescuezilla/nbdkit

commit c879d31047ae149cedabd5abbdb0a95e610f01c5

Rewrite xz plugin as a filter.
The xz plugin should really be a filter so that it can be applied on
top of other sources, especially curl:

  nbdkit --filter=xz curl url=https://download.fedoraproject.org/pub/fedora/linux/releases/29/Cloud/x86_64/images/Fedora-Cloud-Base-29-1.2.x86_64.raw.xz

This repurposes the xz test to test only the filter.  The plugin is no
longer tested as it will be deprecated in the next commit.

filters/xz/xz.c
@@ -1,0 +1,230 @@
+/* nbdkit
+ * Copyright (C) 2013-2018 Red Hat Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <lzma.h>
+
+#include <nbdkit-filter.h>
+
+#include "xzfile.h"
+#include "blkcache.h"
+
+static uint64_t maxblock = 512 * 1024 * 1024;
+static size_t maxdepth = 8;
+
+static int
+xz_config (nbdkit_next_config *next, void *nxdata,
+           const char *key, const char *value)
+{
+  if (strcmp (key, "xz-max-block") == 0) {
+    int64_t r = nbdkit_parse_size (value);
+    if (r == -1)
+      return -1;
+    maxblock = (uint64_t) r;
+    return 0;
+  }
+  else if (strcmp (key, "xz-max-depth") == 0) {
+    size_t r;
+
+    if (sscanf (value, "%zu", &r) != 1) {
+      nbdkit_error ("could not parse 'xz-max-depth' parameter");
+      return -1;
+    }
+    if (r == 0) {
+      nbdkit_error ("'xz-max-depth' parameter must be >= 1");
+      return -1;
+    }
+
+    maxdepth = r;
+    return 0;
+  }
+  else
+    return next (nxdata, key, value);
+}
+
+#define xz_config_help \
+  "xz-max-block=<SIZE> (optional) Maximum block size allowed (default: 512M)\n"\
+  "xz-max-depth=<N>    (optional) Maximum blocks in cache (default: 4)\n"
+
+/* The per-connection handle. */
+struct xz_handle {
+  xzfile *xz;
+
+  /* Block cache. */
+  blkcache *c;
+};
+
+/* Create the per-connection handle. */
+static void *
+xz_open (nbdkit_next_open *next, void *nxdata, int readonly)
+{
+  struct xz_handle *h;
+
+  /* Always pass readonly=1 to the underlying plugin. */
+  if (next (nxdata, 1) == -1)
+    return NULL;
+
+  h = malloc (sizeof *h);
+  if (h == NULL) {
+    nbdkit_error ("malloc: %m");
+    return NULL;
+  }
+
+  h->c = new_blkcache (maxdepth);
+  if (!h->c) {
+    free (h);
+    return NULL;
+  }
+
+  /* Initialized in xz_prepare. */
+  h->xz = NULL;
+
+  return h;
+}
+
+/* Free up the per-connection handle. */
+static void
+xz_close (void *handle)
+{
+  struct xz_handle *h = handle;
+  blkcache_stats stats;
+
+  if (h->c) {
+    blkcache_get_stats (h->c, &stats);
+
+    nbdkit_debug ("cache: hits = %zu, misses = %zu", stats.hits, stats.misses);
+  }
+
+  xzfile_close (h->xz);
+  free_blkcache (h->c);
+  free (h);
+}
+
+static int
+xz_prepare (struct nbdkit_next_ops *next_ops, void *nxdata, void *handle)
+{
+  struct xz_handle *h = handle;
+
+  h->xz = xzfile_open (next_ops, nxdata);
+  if (!h->xz)
+    return -1;
+
+  if (maxblock < xzfile_max_uncompressed_block_size (h->xz)) {
+    nbdkit_error ("xz file largest block is bigger than maxblock\n"
+                  "Either recompress the xz file with smaller blocks (see nbdkit-xz-plugin(1))\n"
+                  "or make maxblock parameter bigger.\n"
+                  "maxblock = %" PRIu64 " (bytes)\n"
+                  "largest block in xz file = %" PRIu64 " (bytes)",
+                  maxblock,
+                  xzfile_max_uncompressed_block_size (h->xz));
+    return -1;
+  }
+
+  return 0;
+}
+
+/* Get the file size. */
+static int64_t
+xz_get_size (struct nbdkit_next_ops *next_ops, void *nxdata, void *handle)
+{
+  struct xz_handle *h = handle;
+
+  return xzfile_get_size (h->xz);
+}
+
+/* Read data from the file. */
+static int
+xz_pread (struct nbdkit_next_ops *next_ops, void *nxdata,
+          void *handle, void *buf, uint32_t count, uint64_t offset,
+          uint32_t flags, int *err)
+{
+  struct xz_handle *h = handle;
+  char *data;
+  uint64_t start, size;
+  uint32_t n;
+
+  /* Find the block in the cache. */
+  data = get_block (h->c, offset, &start, &size);
+  if (!data) {
+    /* Not in the cache.  We need to read the block from the xz file. */
+    data = xzfile_read_block (h->xz, next_ops, nxdata, flags, err,
+                              offset, &start, &size);
+    if (data == NULL)
+      return -1;
+    put_block (h->c, start, size, data);
+  }
+
+  /* It's possible if the blocks are really small or oddly aligned or
+   * if the requests are large that we need to read the following
+   * block to satisfy the request.
+   */
+  n = count;
+  if (start + size - offset < n)
+    n = start + size - offset;
+
+  memcpy (buf, &data[offset-start], n);
+  buf += n;
+  count -= n;
+  offset += n;
+  if (count > 0)
+    return xz_pread (next_ops, nxdata, h, buf, count, offset, flags, err);
+
+  return 0;
+}
+
+#define THREAD_MODEL NBDKIT_THREAD_MODEL_SERIALIZE_REQUESTS
+
+static struct nbdkit_filter filter = {
+  .name              = "xz",
+  .longname          = "nbdkit XZ filter",
+  .version           = PACKAGE_VERSION,
+  .config            = xz_config,
+  .config_help       = xz_config_help,
+  .open              = xz_open,
+  .close             = xz_close,
+  .prepare           = xz_prepare,
+  .get_size          = xz_get_size,
+  .pread             = xz_pread,
+};
+
+NBDKIT_REGISTER_FILTER(filter)

TODO
@@ -90,8 +90,6 @@ Suggestions for filters
 
 * tar plugin should really be a filter
 
-* xz plugin should really be a filter
-
 * gzip plugin should really be a filter
 
 * masking plugin features for testing clients (see 'nozero' and 'fua'

configure.ac
@@ -599,10 +599,10 @@ AS_IF([test "$with_zlib" != "no"],[
 ])
 AM_CONDITIONAL([HAVE_ZLIB],[test "x$ZLIB_LIBS" != "x"])
 
-dnl Check for liblzma (only if you want to compile the xz plugin).
+dnl Check for liblzma (only if you want to compile the xz filter).
 AC_ARG_WITH([liblzma],
     [AS_HELP_STRING([--without-liblzma],
-                    [disable xz plugin @<:@default=check@:>@])],
+                    [disable xz filter @<:@default=check@:>@])],
     [],
     [with_liblzma=check])
 AS_IF([test "$with_liblzma" != "no"],[
@@ -611,7 +611,7 @@ AS_IF([test "$with_liblzma" != "no"],[
         AC_SUBST([LIBLZMA_LIBS])
         AC_DEFINE([HAVE_LIBLZMA],[1],[liblzma found at compile time.])
     ],
-    [AC_MSG_WARN([liblzma not found, xz plugin will be disabled])])
+    [AC_MSG_WARN([liblzma not found, xz filter will be disabled])])
 ])
 AM_CONDITIONAL([HAVE_LIBLZMA],[test "x$LIBLZMA_LIBS" != "x"])
 
@@ -728,6 +728,7 @@ filters="\
         offset \
         partition \
         truncate \
+        xz \
         "
 AC_SUBST([plugins])
 AC_SUBST([lang_plugins])
@@ -790,6 +791,7 @@ AC_CONFIG_FILES([Makefile
                  filters/offset/Makefile
                  filters/partition/Makefile
                  filters/truncate/Makefile
+                 filters/xz/Makefile
                  src/Makefile
                  src/nbdkit.pc
                  tests/functions.sh

filters/xz/Makefile.am
@@ -1,0 +1,71 @@
+# nbdkit
+# Copyright (C) 2013-2018 Red Hat Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+include $(top_srcdir)/common-rules.mk
+
+EXTRA_DIST = nbdkit-xz-filter.pod
+
+if HAVE_LIBLZMA
+
+filter_LTLIBRARIES = nbdkit-xz-filter.la
+
+nbdkit_xz_filter_la_SOURCES = \
+	blkcache.c \
+	blkcache.h \
+	xz.c \
+	xzfile.c \
+	xzfile.h \
+	$(top_srcdir)/include/nbdkit-filter.h
+
+nbdkit_xz_filter_la_CPPFLAGS = \
+	-I$(top_srcdir)/include
+nbdkit_xz_filter_la_CFLAGS = \
+	$(WARNINGS_CFLAGS) \
+	$(LIBLZMA_CFLAGS)
+nbdkit_xz_filter_la_LIBADD = \
+	$(LIBLZMA_LIBS)
+nbdkit_xz_filter_la_LDFLAGS = \
+	-module -avoid-version -shared
+
+if HAVE_POD
+
+man_MANS = nbdkit-xz-filter.1
+CLEANFILES += $(man_MANS)
+
+nbdkit-xz-filter.1: nbdkit-xz-filter.pod
+	$(PODWRAPPER) --section=1 --man $@ \
+	    --html $(top_builddir)/html/$@.html \
+	    $<
+
+endif HAVE_POD
+
+endif

filters/xz/blkcache.c
@@ -1,0 +1,146 @@
+/* nbdkit
+ * Copyright (C) 2013 Red Hat Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+#include <nbdkit-plugin.h>
+
+#include "blkcache.h"
+
+/* Implemented as a very simple LRU list with a fixed depth. */
+struct blkcache {
+  size_t maxdepth;
+  struct block *blocks;
+  blkcache_stats stats;
+};
+
+struct block {
+  uint64_t start;
+  uint64_t size;
+  char *data;
+};
+
+blkcache *
+new_blkcache (size_t maxdepth)
+{
+  blkcache *c;
+
+  c = malloc (sizeof *c);
+  if (!c) {
+    nbdkit_error ("malloc: %m");
+    return NULL;
+  }
+
+  c->blocks = calloc (maxdepth, sizeof (struct block));
+  if (!c->blocks) {
+    nbdkit_error ("calloc: %m");
+    free (c);
+    return NULL;
+  }
+  c->maxdepth = maxdepth;
+  c->stats.hits = c->stats.misses = 0;
+
+  return c;
+}
+
+void
+free_blkcache (blkcache *c)
+{
+  size_t i;
+
+  for (i = 0; i < c->maxdepth; ++i)
+    free (c->blocks[i].data);
+  free (c->blocks);
+  free (c);
+}
+
+char *
+get_block (blkcache *c, uint64_t offset, uint64_t *start, uint64_t *size)
+{
+  size_t i;
+  struct block tmp;
+
+  for (i = 0; i < c->maxdepth; ++i) {
+    if (c->blocks[i].data != NULL &&
+        c->blocks[i].start <= offset &&
+        offset < c->blocks[i].start + c->blocks[i].size) {
+      /* This block is now most recently used, so put it at the start. */
+      if (i > 0) {
+        tmp = c->blocks[0];
+        c->blocks[0] = c->blocks[i];
+        c->blocks[i] = tmp;
+      }
+
+      c->stats.hits++;
+      *start = c->blocks[0].start;
+      *size = c->blocks[0].size;
+      return c->blocks[0].data;
+    }
+  }
+
+  c->stats.misses++;
+  return NULL;
+}
+
+int
+put_block (blkcache *c, uint64_t start, uint64_t size, char *data)
+{
+  size_t i;
+
+  /* Eject the least recently used block. */
+  i = c->maxdepth-1;
+  if (c->blocks[i].data != NULL)
+    free (c->blocks[i].data);
+
+  for (; i >= 1; --i)
+    c->blocks[i] = c->blocks[i-1];
+
+  /* The new block is most recently used, so it goes at the start. */
+  c->blocks[0].start = start;
+  c->blocks[0].size = size;
+  c->blocks[0].data = data;
+
+  return 0;
+}
+
+void
+blkcache_get_stats (blkcache *c, blkcache_stats *ret)
+{
+  memcpy (ret, &c->stats, sizeof (c->stats));
+}

filters/xz/blkcache.h
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2013-2018 Red Hat Inc.
+ * Copyright (C) 2013 Red Hat Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -31,61 +31,20 @@
  * SUCH DAMAGE.
  */
 
-#include <config.h>
+#ifndef NBDKIT_BLKCACHE_H
+#define NBDKIT_BLKCACHE_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <inttypes.h>
-#include <string.h>
-#include <unistd.h>
+typedef struct blkcache blkcache;
 
-#include <guestfs.h>
+typedef struct blkcache_stats {
+  size_t hits;
+  size_t misses;
+} blkcache_stats;
 
-#include "test.h"
+extern blkcache *new_blkcache (size_t maxdepth);
+extern void free_blkcache (blkcache *);
+extern char *get_block (blkcache *, uint64_t offset, uint64_t *start, uint64_t *size);
+extern int put_block (blkcache *, uint64_t start, uint64_t size, char *data);
+extern void blkcache_get_stats (blkcache *, blkcache_stats *ret);
 
-int
-main (int argc, char *argv[])
-{
-  guestfs_h *g;
-  int r;
-  char *data;
-
-  if (test_start_nbdkit ("--filter=xz", "file", "disk.xz", NULL) == -1)
-    exit (EXIT_FAILURE);
-
-  g = guestfs_create ();
-  if (g == NULL) {
-    perror ("guestfs_create");
-    exit (EXIT_FAILURE);
-  }
-
-  r = guestfs_add_drive_opts (g, "",
-                              GUESTFS_ADD_DRIVE_OPTS_READONLY, 1,
-                              GUESTFS_ADD_DRIVE_OPTS_FORMAT, "raw",
-                              GUESTFS_ADD_DRIVE_OPTS_PROTOCOL, "nbd",
-                              GUESTFS_ADD_DRIVE_OPTS_SERVER, server,
-                              -1);
-  if (r == -1)
-    exit (EXIT_FAILURE);
-
-  if (guestfs_launch (g) == -1)
-    exit (EXIT_FAILURE);
-
-  /* disk.xz contains one partition and a test file called "hello.txt" */
-  if (guestfs_mount_ro (g, "/dev/sda1", "/") == -1)
-    exit (EXIT_FAILURE);
-
-  data = guestfs_cat (g, "/hello.txt");
-  if (!data)
-    exit (EXIT_FAILURE);
-
-  if (strcmp (data, "hello,world") != 0) {
-    fprintf (stderr, "%s FAILED: unexpected content of /hello.txt file (actual: %s, expected: \"hello,world\")\n",
-             program_name, data);
-    exit (EXIT_FAILURE);
-  }
-
-  guestfs_close (g);
-  exit (EXIT_SUCCESS);
-}
+#endif /* NBDKIT_XZFILE_H */

filters/xz/nbdkit-xz-filter.pod
@@ -1,0 +1,93 @@
+=head1 NAME
+
+nbdkit-xz-filter - nbdkit xz filter
+
+=head1 SYNOPSIS
+
+ nbdkit --filter=xz file FILENAME.xz
+
+ nbdkit --filter=xz curl url=https://example.com/FILENAME.xz
+
+=head1 DESCRIPTION
+
+C<nbdkit-xz-filter> is a filter for L<nbdkit(1)> which uncompresses
+the underlying plugin on the fly.  The filter only supports read-only
+connections.
+
+=head2 Getting best random access performance from xz
+
+L<xz(1)> files are split into streams and blocks.  Most xz files
+contain only one stream which contains one or more blocks.  You can
+find out how many streams and blocks are in an xz file by doing:
+
+ $ xz --list winxp.img.xz
+ Strms  Blocks   Compressed Uncompressed  Ratio  Check   Filename
+     1       1  2,100.0 MiB  6,144.0 MiB  0.342  CRC64   winxp.img.xz
+    ↑↑↑     ↑↑↑
+  streams  blocks
+
+xz files are seekable on block boundaries only.  Seeking is done by
+seeking directly to the lower block boundary, then uncompressing data
+until the precise byte is reached.  As uncompressing xz data is slow,
+B<to get best random access performance, you must prepare your xz
+files with many small blocks.>
+
+Use the I<--block-size> option with a small-ish block size.  For
+example this is the same image compressed with a 16 MB block size:
+
+ $ xz --best --block-size=16777216 winxp.img
+             ─────────────────────
+ $ xz --list winxp.img.xz
+ Strms  Blocks   Compressed Uncompressed  Ratio  Check   Filename
+     1     384  2,120.1 MiB  6,144.0 MiB  0.345  CRC64   winxp.img.xz
+           ↑↑↑
+          blocks
+
+This file can be accessed randomly.  At most 16 MB will have to be
+uncompressed to seek to any byte.
+
+As you would expect, xz cannot compress as efficiently when using a
+smaller block size.  The space penalty in the above example is
+S<E<lt> 1%> of the compressed file size.
+
+=head1 PARAMETERS
+
+=over 4
+
+=item B<xz-max-block=>SIZE
+
+The maximum block size that the filter will read.  The filter will
+refuse to read xz files that contain any block larger than this size.
+
+See the discussion above about creating xz files with small block
+sizes in order to reduce memory usage and increase performance.
+
+This parameter is optional.  If not specified it defaults to 512M.
+
+=item B<xz-max-depth=>N
+
+Maximum number of blocks stored in the LRU block cache.
+
+This parameter is optional.  If not specified it defaults to 8.
+
+The filter may allocate up to
+S<maximum block size in file * maxdepth>
+bytes of memory I<per connection>.
+
+=back
+
+=head1 SEE ALSO
+
+L<nbdkit(1)>,
+L<nbdkit-filter(3)>,
+L<nbdkit-curl-plugin(1)>,
+L<nbdkit-file-plugin(1)>,
+L<xz(1)>.
+
+=head1 AUTHORS
+
+Richard W.M. Jones
+
+=head1 COPYRIGHT
+
+Copyright (C) 2013-2018 Red Hat Inc.

commit 35db543e38293808e910b291ff26b17284640ec9

xz: Fix error message to refer to xz-max-block parameter
In the original xz plugin the parameter was called "maxblock", but
when we converted the plugin to a filter (commit c879d31047 "Rewrite
xz plugin as a filter.") we changed the parameter name to
"xz-max-block" but retained the old name in the error message.

Fixes: commit c879d31047ae149cedabd5abbdb0a95e610f01c5

filters/xz/xz.c
@@ -143,12 +143,12 @@ xz_prepare (nbdkit_next *next, void *handle,
     return -1;
 
   if (maxblock < xzfile_max_uncompressed_block_size (h->xz)) {
-    nbdkit_error ("xz file largest block is bigger than maxblock\n"
+    nbdkit_error ("xz file largest block is bigger than xz-max-block\n"
                   "Either recompress the xz file with smaller blocks "
                   "(see nbdkit-xz-filter(1))\n"
-                  "or make maxblock parameter bigger.\n"
-                  "maxblock = %" PRIu64 " (bytes)\n"
-                  "largest block in xz file = %" PRIu64 " (bytes)",
+                  "or make xz-max-block parameter bigger.\n"
+                  "Current xz-max-block = %" PRIu64 " (bytes)\n"
+                  "Largest block in xz file = %" PRIu64 " (bytes)",
                   maxblock,
                   xzfile_max_uncompressed_block_size (h->xz));
     return -1;