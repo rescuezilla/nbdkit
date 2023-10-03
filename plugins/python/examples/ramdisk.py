NetworkBlockDevice/nbd

commit 185539eed1aafa601ee2ed3471ff8d868d23cba5

python: New plugin that lets you write plugins as Python scripts.

plugins/python/nbdkit-python-plugin.pod
@@ -1,0 +1,266 @@
+=encoding utf8
+
+=head1 NAME
+
+nbdkit-python-plugin - nbdkit python plugin
+
+=head1 SYNOPSIS
+
+ nbdkit python script=/path/to/plugin.py [arguments...]
+
+=head1 DESCRIPTION
+
+C<nbdkit-python-plugin> is an embedded Python interpreter for
+L<nbdkit(1)>, allowing you to write nbdkit plugins in Python.
+
+Broadly speaking, Python nbdkit plugins work like C ones, so you should
+read L<nbdkit-plugin(3)> first.
+
+=head2 USING A PYTHON NBDKIT PLUGIN
+
+Assuming you have a Python script which is an nbdkit plugin, you run it
+like this:
+
+ nbdkit python script=/path/to/plugin.py
+
+You may have to add further C<key=value> arguments to the command
+line.  Read the Python script to see if it requires any.  C<script=...>
+I<must> come first on the command line.
+
+=head1 WRITING A PYTHON NBDKIT PLUGIN
+
+There is an example Python nbdkit plugin called C<example.py> which
+ships with the nbdkit source.
+
+To write a Python nbdkit plugin, you create a Python file which
+contains at least the following required functions (in the top level
+C<__main__> module):
+
+ def open(readonly):
+   # see below
+ def get_size(h):
+   # see below
+ def pread(h, count, offset):
+   # see below
+
+Note that the subroutines must have those literal names (like C<open>),
+because the C part looks up and calls those functions directly.  You
+may want to include documentation and globals (eg. for storing global
+state).  Any other top level statements are run when the script is
+loaded, just like ordinary Python.
+
+The file does not need to include a C<#!> (hash-bang) at the top, and
+does not need to be executable.  In fact it's a good idea not to do
+that, because running the plugin directly as a Python script won't
+work.
+
+=head2 EXCEPTIONS
+
+Python callbacks should throw exceptions to indicate errors.
+
+=head2 PYTHON CALLBACKS
+
+This just documents the arguments to the callbacks in Python, and any
+way that they differ from the C callbacks.  In all other respects they
+work the same way as the C callbacks, so you should go and read
+nbdkit-plugin(3).
+
+=over 4
+
+=item C<config>
+
+(Optional)
+
+ def config(key, value):
+   # no return value
+
+=item C<config_complete>
+
+(Optional)
+
+There are no arguments or return value.
+
+=item C<open>
+
+(Required)
+
+ def open(readonly):
+   # return handle
+
+You can return any non-NULL Python value as the handle.  It is passed
+back in subsequent calls.
+
+=item C<close>
+
+(Optional)
+
+ def close(h):
+   # no return value
+
+After C<close> returns, the reference count of the handle is
+decremented in the C part, which usually means that the handle and its
+contents will be garbage collected.
+
+=item C<get_size>
+
+(Required)
+
+ def get_size(h):
+   # return the size of the disk
+
+=item C<can_write>
+
+(Optional)
+
+ def can_write(h):
+   # return a boolean
+
+=item C<can_flush>
+
+(Optional)
+
+ def can_flush(h):
+   # return a boolean
+
+=item C<is_rotational>
+
+(Optional)
+
+ def is_rotational(h):
+   # return a boolean
+
+=item C<can_trim>
+
+(Optional)
+
+ def can_trim(h):
+   # return a boolean
+
+=item C<pread>
+
+(Required)
+
+ def pread(h, count, offset):
+   # construct a bytearray of length count bytes and return it
+
+The body of your C<pread> function should construct a buffer of length
+(at least) C<count> bytes.  You should read C<count> bytes from the
+disk starting at C<offset>.
+
+NBD only supports whole reads, so your function should try to read
+the whole region (perhaps requiring a loop).  If the read fails or
+is partial, your function should throw an exception.
+
+=item C<pwrite>
+
+(Optional)
+
+ def pwrite(h, buf, offset):
+   length = len (buf)
+   # no return value
+
+The body of your C<pwrite> function should write the C<buf> string to
+the disk.  You should write C<count> bytes to the disk starting at
+C<offset>.
+
+NBD only supports whole writes, so your function should try to
+write the whole region (perhaps requiring a loop).  If the write
+fails or is partial, your function should throw an exception.
+
+=item C<flush>
+
+(Optional)
+
+ def flush(h):
+   # no return value
+
+The body of your C<flush> function should do a L<sync(2)> or
+L<fdatasync(2)> or equivalent on the backing store.
+
+=item C<trim>
+
+(Optional)
+
+ def trim(h, count, offset):
+   # no return value
+
+The body of your C<trim> function should "punch a hole" in the
+backing store.
+
+=back
+
+=head2 MISSING CALLBACKS
+
+=over 4
+
+=item Missing: C<load> and C<unload>
+
+These are not needed because you can just use ordinary Python
+constructs.
+
+=item Missing: C<name>, C<version>, C<longname>, C<description>, C<config_help>
+
+These are not yet supported.
+
+=back
+
+=head2 THREADS
+
+The thread model for Python callbacks currently cannot be set from
+Python.  It is hard-coded in the C part to
+C<NBDKIT_THREAD_MODEL_SERIALIZE_ALL_REQUESTS>.  This may change or be
+settable in future.
+
+=head1 SEE ALSO
+
+L<nbdkit(1)>,
+L<nbdkit-plugin(3)>,
+L<python(1)>.
+
+=head1 AUTHORS
+
+Richard W.M. Jones
+
+=head1 COPYRIGHT
+
+Copyright (C) 2013-2014 Red Hat Inc.
+
+=head1 LICENSE
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+=over 4
+
+=item *
+
+Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+=item *
+
+Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+=item *
+
+Neither the name of Red Hat nor the names of its contributors may be
+used to endorse or promote products derived from this software without
+specific prior written permission.
+
+=back
+
+THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.

configure.ac
@@ -151,6 +151,51 @@ AC_SUBST([PERL_ARCHLIB])
 AC_SUBST([PERL_CFLAGS])
 AC_SUBST([PERL_LDOPTS])
 
+dnl Check for Python, for embedding in the python plugin.
+AC_CHECK_PROG([PYTHON],[python],[python],[no])
+AC_ARG_ENABLE([python],
+    AS_HELP_STRING([--disable-python], [disable Python embed plugin]),
+    [],
+    [enable_python=yes])
+AS_IF([test "x$PYTHON" != "xno" && test "x$enable_python" != "xno"],[
+    AC_MSG_CHECKING([version of $PYTHON])
+    PYTHON_VERSION_MAJOR=`$PYTHON -c "import sys; print (sys.version_info@<:@0@:>@)"`
+    PYTHON_VERSION_MINOR=`$PYTHON -c "import sys; print (sys.version_info@<:@1@:>@)"`
+    PYTHON_VERSION="$PYTHON_VERSION_MAJOR.$PYTHON_VERSION_MINOR"
+    AS_IF([test -n "$PYTHON_VERSION"],[
+        AC_MSG_RESULT([$PYTHON_VERSION])
+    ],[
+        AC_MSG_NOTICE([Python embed module disabled])
+        enable_python=no
+    ])
+
+    dnl Check for Python CFLAGS, libraries.
+    dnl On Debian: python-X.Y.pc
+    PKG_CHECK_MODULES([PYTHON], [python-"$PYTHON_VERSION"], [
+        AC_SUBST([PYTHON_CFLAGS])
+        AC_SUBST([PYTHON_LIBS])
+        AC_SUBST([PYTHON_VERSION])
+        AC_DEFINE([HAVE_PYTHON],[1],[Python library found at compile time])
+    ],[
+        dnl On Fedora: python.pc
+        PKG_CHECK_MODULES([PYTHON], [python], [
+            AC_SUBST([PYTHON_CFLAGS])
+            AC_SUBST([PYTHON_LIBS])
+            AC_SUBST([PYTHON_VERSION])
+            AC_DEFINE([HAVE_PYTHON],[1],[Python library found at compile time])
+        ],[
+            AC_MSG_WARN([python $PYTHON_VERSION not found])
+            enable_python=no
+        ])
+    ])
+
+    dnl XXX Could check these actually work.
+])
+AM_CONDITIONAL([HAVE_PYTHON],[test "x$enable_python" != "xno" && test "x$PYTHON" != "xno"])
+AC_SUBST([PYTHON_CFLAGS])
+AC_SUBST([PYTHON_LIBS])
+AC_SUBST([PYTHON_LDFLAGS])
+
 dnl Check for libvirt (only if you want to compile the libvirt plugin).
 AC_ARG_WITH([libvirt],[
     AS_HELP_STRING([--without-libvirt],
@@ -260,6 +305,7 @@ AC_CONFIG_FILES([Makefile
                  plugins/gzip/Makefile
                  plugins/libvirt/Makefile
                  plugins/perl/Makefile
+                 plugins/python/Makefile
                  plugins/vddk/Makefile
                  plugins/xz/Makefile
                  src/Makefile

docs/nbdkit-plugin.pod
@@ -46,7 +46,8 @@ For example plugins, take a look at the source of nbdkit, in the
 C<plugins> directory.
 
 To write plugins in other languages, see:
-L<nbdkit-perl-plugin(3)>.
+L<nbdkit-perl-plugin(3)>,
+L<nbdkit-python-plugin(3)>.
 
 =head1 C<nbdkit-plugin.h>
 
@@ -511,7 +512,8 @@ L<nbdkit(1)>,
 L<nbdkit-example1-plugin(1)>,
 L<nbdkit-example2-plugin(1)>,
 L<nbdkit-example3-plugin(1)>,
-L<nbdkit-perl-plugin(3)>.
+L<nbdkit-perl-plugin(3)>,
+L<nbdkit-python-plugin(3)>.
 
 =head1 AUTHORS
 

plugins/python/Makefile.am
@@ -30,15 +30,40 @@
 # OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 
-SUBDIRS = \
-	example1 \
-	example2 \
-	example3 \
-	file \
-	guestfs \
-	gzip \
-	libvirt \
-	perl \
-	python \
-	vddk \
-	xz
+EXTRA_DIST = \
+	nbdkit-python-plugin.pod \
+	example.pl
+
+plugindir = $(libdir)/nbdkit/plugins
+
+if HAVE_PYTHON
+
+plugin_LTLIBRARIES = nbdkit-python-plugin.la
+
+nbdkit_python_plugin_la_SOURCES = \
+	python.c \
+	$(top_srcdir)/include/nbdkit-plugin.h
+
+nbdkit_python_plugin_la_CPPFLAGS = \
+	-I$(top_srcdir)/include
+nbdkit_python_plugin_la_CFLAGS = \
+	$(WARNINGS_CFLAGS) \
+	$(PYTHON_CFLAGS)
+nbdkit_python_plugin_la_LIBADD = \
+	$(PYTHON_LIBS)
+nbdkit_python_plugin_la_LDFLAGS = \
+	-module -avoid-version -shared \
+	$(PYTHON_LDFLAGS)
+
+if HAVE_POD2MAN
+
+man_MANS = nbdkit-python-plugin.3
+
+nbdkit-python-plugin.3: nbdkit-python-plugin.pod
+	$(POD2MAN) -u --stderr --section=3 --center=nbdkit --release=nbdkit --name=`basename $@ .3` $< $@
+
+endif
+
+endif
+
+CLEANFILES = *~

plugins/python/example.py
@@ -1,0 +1,58 @@
+# Example Python plugin.
+#
+# This example can be freely used for any purpose.
+
+# Run it from the build directory like this:
+#
+#   ./src/nbdkit -f -v ./plugins/python/.libs/nbdkit-python-plugin.so  \
+#       script=./plugins/python/example.py test1=foo test2=bar
+#
+# Or run it after installing nbdkit like this:
+#
+#   nbdkit -f -v python script=./plugins/python/example.py test1=foo test2=bar
+#
+# The -f -v arguments are optional.  They cause the server to stay in
+# the foreground and print debugging, which is useful when testing.
+#
+# You can connect to the server using guestfish or qemu, eg:
+#
+#   guestfish --format=raw -a nbd://localhost
+#   ><fs> run
+#   ><fs> part-disk /dev/sda mbr
+#   ><fs> mkfs ext2 /dev/sda1
+#   ><fs> list-filesystems
+#   ><fs> mount /dev/sda1 /
+#   ><fs> [etc]
+
+# This is the string used to store the emulated disk (initially all
+# zero bytes).  There is one disk per nbdkit instance, so if you
+# reconnect to the same server you should see the same disk.  You
+# could also put this into the handle, so there would be a fresh disk
+# per handle.
+disk = "\0" * (1024*1024);
+
+# This just prints the extra command line parameters, but real plugins
+# should parse them and reject any unknown parameters.
+def config(key, value):
+    print ("ignored parameter %s=%s" % (key, value))
+
+def open(readonly):
+    print ("open: readonly=%d" % readonly)
+
+    # You can return any non-NULL Python object from open, and the
+    # same object will be passed as the first arg to the other
+    # callbacks [in the client connected phase].
+    return 1
+
+def get_size(h):
+    global disk
+    return len (disk)
+
+def pread(h, count, offset):
+    global disk
+    return bytearray (disk[offset:offset+count])
+
+def pwrite(h, buf, offset):
+    global disk
+    end = offset + len (buf)
+    disk = disk[:offset] + buf + disk[end:]

plugins/python/python.c
@@ -1,0 +1,575 @@
+/* nbdkit
+ * Copyright (C) 2013-2014 Red Hat Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/* This has to be included first, else definitions conflict with
+ * glibc header files.  Python is broken.
+ */
+#define PY_SSIZE_T_CLEAN 1
+#include <Python.h>
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <string.h>
+#include <unistd.h>
+#include <assert.h>
+#include <errno.h>
+
+#include <nbdkit-plugin.h>
+
+static const char *script;
+static PyObject *module;
+
+/* Is a callback defined? */
+static int
+callback_defined (const char *name, PyObject **obj_rtn)
+{
+  PyObject *obj;
+
+  assert (script != NULL);
+  assert (module != NULL);
+
+  obj = PyObject_GetAttrString (module, name);
+  if (!obj)
+    return 0;
+
+  if (obj_rtn != NULL)
+    *obj_rtn = obj;
+  else
+    Py_DECREF (obj);
+
+  return 1;
+}
+
+static int
+check_python_failure (const char *callback)
+{
+  if (PyErr_Occurred ()) {
+    nbdkit_error ("%s: callback failed: %s", script, callback);
+    /* How to turn this into a string? XXX */
+    PyErr_Print ();
+    return -1;
+  }
+  return 0;
+}
+
+static void
+py_load (void)
+{
+  Py_Initialize ();
+}
+
+static void
+py_unload (void)
+{
+  if (module)
+    Py_DECREF (module);
+
+  Py_Finalize ();
+}
+
+static int
+py_config (const char *key, const char *value)
+{
+  FILE *fp;
+  PyObject *modname;
+  PyObject *fn;
+  PyObject *args;
+  PyObject *r;
+
+  if (!script) {
+    /* The first parameter MUST be "script". */
+    if (strcmp (key, "script") != 0) {
+      nbdkit_error ("the first parameter must be script=/path/to/python/script.py");
+      return -1;
+    }
+    script = value;
+
+    /* Load the Python script. */
+    fp = fopen (script, "r");
+    if (!fp) {
+      nbdkit_error ("%s: cannot open file: %m", script);
+      return -1;
+    }
+
+    if (PyRun_SimpleFileEx (fp, script, 1) == -1) {
+      nbdkit_error ("%s: error running this script", script);
+      return -1;
+    }
+    /* Note that because closeit flag == 1, fp is now closed. */
+
+    /* The script should define a module called __main__. */
+    modname = PyString_FromString ("__main__");
+    module = PyImport_Import (modname);
+    Py_DECREF (modname);
+    if (!module) {
+      nbdkit_error ("%s: cannot find __main__ module", script);
+      return -1;
+    }
+
+    /* Minimal set of callbacks which are required (by nbdkit itself). */
+    if (!callback_defined ("open", NULL) ||
+        !callback_defined ("get_size", NULL) ||
+        !callback_defined ("pread", NULL)) {
+      nbdkit_error ("%s: one of the required callbacks 'open', 'get_size' or 'pread' is not defined by this Python script.  nbdkit requires these callbacks.", script);
+      return -1;
+    }
+  }
+  else if (callback_defined ("config", &fn)) {
+    /* Other parameters are passed to the Python .config callback. */
+    PyErr_Clear ();
+
+    args = PyTuple_New (2);
+    PyTuple_SetItem (args, 0, PyString_FromString (key));
+    PyTuple_SetItem (args, 1, PyString_FromString (value));
+    r = PyObject_CallObject (fn, args);
+    Py_DECREF (fn);
+    Py_DECREF (args);
+    if (check_python_failure ("config") == -1)
+      return -1;
+    Py_DECREF (r);
+  }
+  else {
+    /* Emulate what core nbdkit does if a config callback is NULL. */
+    nbdkit_error ("%s: this plugin does not need command line configuration",
+                  script);
+    return -1;
+  }
+
+  return 0;
+}
+
+static int
+py_config_complete (void)
+{
+  PyObject *fn;
+  PyObject *r;
+
+  if (callback_defined ("config_complete", &fn)) {
+    PyErr_Clear ();
+
+    r = PyObject_CallObject (fn, NULL);
+    Py_DECREF (fn);
+    if (check_python_failure ("config_complete") == -1)
+      return -1;
+    Py_DECREF (r);
+  }
+
+  return 0;
+}
+
+static void *
+py_open (int readonly)
+{
+  PyObject *fn;
+  PyObject *args;
+  PyObject *handle;
+
+  if (!callback_defined ("open", &fn)) {
+    nbdkit_error ("%s: missing callback: %s", script, "open");
+    return NULL;
+  }
+
+  PyErr_Clear ();
+
+  args = PyTuple_New (1);
+  PyTuple_SetItem (args, 0, PyBool_FromLong (readonly));
+  handle = PyObject_CallObject (fn, args);
+  Py_DECREF (fn);
+  Py_DECREF (args);
+  if (check_python_failure ("open") == -1)
+    return NULL;
+
+  return handle;
+}
+
+static void
+py_close (void *handle)
+{
+  PyObject *obj = handle;
+  PyObject *fn;
+  PyObject *args;
+  PyObject *r;
+
+  if (callback_defined ("close", &fn)) {
+    PyErr_Clear ();
+
+    args = PyTuple_New (1);
+    Py_INCREF (obj); /* decremented by Py_DECREF (args) */
+    PyTuple_SetItem (args, 0, obj);
+    r = PyObject_CallObject (fn, args);
+    Py_DECREF (fn);
+    Py_DECREF (args);
+    check_python_failure ("close");
+    if (r)
+      Py_DECREF (r);
+  }
+
+  Py_DECREF (obj);
+}
+
+static int64_t
+py_get_size (void *handle)
+{
+  PyObject *obj = handle;
+  PyObject *fn;
+  PyObject *args;
+  PyObject *r;
+  int64_t ret;
+
+  if (!callback_defined ("get_size", &fn)) {
+    nbdkit_error ("%s: missing callback: %s", script, "get_size");
+    return -1;
+  }
+
+  PyErr_Clear ();
+
+  args = PyTuple_New (1);
+  Py_INCREF (obj); /* decremented by Py_DECREF (args) */
+  PyTuple_SetItem (args, 0, obj);
+  r = PyObject_CallObject (fn, args);
+  Py_DECREF (fn);
+  Py_DECREF (args);
+  if (check_python_failure ("get_size") == -1)
+    return -1;
+
+  ret = PyLong_AsLongLong (r);
+  Py_DECREF (r);
+  if (check_python_failure ("PyLong_AsLongLong") == -1)
+    return -1;
+
+  return ret;
+}
+
+static int
+py_pread (void *handle, void *buf,
+          uint32_t count, uint64_t offset)
+{
+  PyObject *obj = handle;
+  PyObject *fn;
+  PyObject *args;
+  PyObject *r;
+
+  if (!callback_defined ("pread", &fn)) {
+    nbdkit_error ("%s: missing callback: %s", script, "pread");
+    return -1;
+  }
+
+  PyErr_Clear ();
+
+  args = PyTuple_New (3);
+  Py_INCREF (obj); /* decremented by Py_DECREF (args) */
+  PyTuple_SetItem (args, 0, obj);
+  PyTuple_SetItem (args, 1, PyLong_FromLong (count));
+  PyTuple_SetItem (args, 2, PyLong_FromUnsignedLongLong (offset));
+  r = PyObject_CallObject (fn, args);
+  Py_DECREF (fn);
+  Py_DECREF (args);
+  if (check_python_failure ("pread") == -1)
+    return -1;
+
+  if (!PyByteArray_Check (r)) {
+    nbdkit_error ("%s: value returned from pread method is not a byte array",
+                  script);
+    Py_DECREF (r);
+    return -1;
+  }
+
+  if (PyByteArray_Size (r) < count) {
+    nbdkit_error ("%s: byte array returned from pread is too small", script);
+    Py_DECREF (r);
+    return -1;
+  }
+
+  memcpy (buf, PyByteArray_AsString (r), count);
+  Py_DECREF (r);
+
+  return 0;
+}
+
+static int
+py_pwrite (void *handle, const void *buf,
+           uint32_t count, uint64_t offset)
+{
+  PyObject *obj = handle;
+  PyObject *fn;
+  PyObject *args;
+  PyObject *r;
+
+  if (callback_defined ("pwrite", &fn)) {
+    PyErr_Clear ();
+
+    args = PyTuple_New (3);
+    Py_INCREF (obj); /* decremented by Py_DECREF (args) */
+    PyTuple_SetItem (args, 0, obj);
+    PyTuple_SetItem (args, 1, PyByteArray_FromStringAndSize (buf, count));
+    PyTuple_SetItem (args, 2, PyLong_FromUnsignedLongLong (offset));
+    r = PyObject_CallObject (fn, args);
+    Py_DECREF (fn);
+    Py_DECREF (args);
+    if (check_python_failure ("pwrite") == -1)
+      return -1;
+    Py_DECREF (r);
+  }
+  else {
+    nbdkit_error ("%s not implemented", "pwrite");
+    return -1;
+  }
+
+  return 0;
+}
+
+static int
+py_flush (void *handle)
+{
+  PyObject *obj = handle;
+  PyObject *fn;
+  PyObject *args;
+  PyObject *r;
+
+  if (callback_defined ("flush", &fn)) {
+    PyErr_Clear ();
+
+    args = PyTuple_New (1);
+    Py_INCREF (obj); /* decremented by Py_DECREF (args) */
+    PyTuple_SetItem (args, 0, obj);
+    r = PyObject_CallObject (fn, args);
+    Py_DECREF (fn);
+    Py_DECREF (args);
+    if (check_python_failure ("flush") == -1)
+      return -1;
+    Py_DECREF (r);
+  }
+  else {
+    nbdkit_error ("%s not implemented", "flush");
+    return -1;
+  }
+
+  return 0;
+}
+
+static int
+py_trim (void *handle, uint32_t count, uint64_t offset)
+{
+  PyObject *obj = handle;
+  PyObject *fn;
+  PyObject *args;
+  PyObject *r;
+
+  if (callback_defined ("trim", &fn)) {
+    PyErr_Clear ();
+
+    args = PyTuple_New (3);
+    Py_INCREF (obj); /* decremented by Py_DECREF (args) */
+    PyTuple_SetItem (args, 0, obj);
+    PyTuple_SetItem (args, 1, PyLong_FromLong (count));
+    PyTuple_SetItem (args, 2, PyLong_FromUnsignedLongLong (offset));
+    r = PyObject_CallObject (fn, args);
+    Py_DECREF (fn);
+    Py_DECREF (args);
+    if (check_python_failure ("trim") == -1)
+      return -1;
+    Py_DECREF (r);
+  }
+  else {
+    nbdkit_error ("%s not implemented", "trim");
+    return -1;
+  }
+
+  return 0;
+}
+
+static int
+py_can_write (void *handle)
+{
+  PyObject *obj = handle;
+  PyObject *fn;
+  PyObject *args;
+  PyObject *r;
+  int ret;
+
+  if (callback_defined ("can_write", &fn)) {
+    PyErr_Clear ();
+
+    args = PyTuple_New (1);
+    Py_INCREF (obj); /* decremented by Py_DECREF (args) */
+    PyTuple_SetItem (args, 0, obj);
+    r = PyObject_CallObject (fn, args);
+    Py_DECREF (fn);
+    Py_DECREF (args);
+    if (check_python_failure ("can_write") == -1)
+      return -1;
+    ret = r == Py_True;
+    Py_DECREF (r);
+    return ret;
+  }
+  /* No Perl can_write callback, but there's a Perl pwrite callback
+   * defined, so return 1.  (In C modules, nbdkit would do this).
+   */
+  else if (callback_defined ("pwrite", NULL))
+    return 1;
+  else
+    return 0;
+}
+
+static int
+py_can_flush (void *handle)
+{
+  PyObject *obj = handle;
+  PyObject *fn;
+  PyObject *args;
+  PyObject *r;
+  int ret;
+
+  if (callback_defined ("can_flush", &fn)) {
+    PyErr_Clear ();
+
+    args = PyTuple_New (1);
+    Py_INCREF (obj); /* decremented by Py_DECREF (args) */
+    PyTuple_SetItem (args, 0, obj);
+    r = PyObject_CallObject (fn, args);
+    Py_DECREF (fn);
+    Py_DECREF (args);
+    if (check_python_failure ("can_flush") == -1)
+      return -1;
+    ret = r == Py_True;
+    Py_DECREF (r);
+    return ret;
+  }
+  /* No Perl can_flush callback, but there's a Perl flush callback
+   * defined, so return 1.  (In C modules, nbdkit would do this).
+   */
+  else if (callback_defined ("flush", NULL))
+    return 1;
+  else
+    return 0;
+}
+
+static int
+py_is_rotational (void *handle)
+{
+  PyObject *obj = handle;
+  PyObject *fn;
+  PyObject *args;
+  PyObject *r;
+  int ret;
+
+  if (callback_defined ("is_rotational", &fn)) {
+    PyErr_Clear ();
+
+    args = PyTuple_New (1);
+    Py_INCREF (obj); /* decremented by Py_DECREF (args) */
+    PyTuple_SetItem (args, 0, obj);
+    r = PyObject_CallObject (fn, args);
+    Py_DECREF (fn);
+    Py_DECREF (args);
+    if (check_python_failure ("is_rotational") == -1)
+      return -1;
+    ret = r == Py_True;
+    Py_DECREF (r);
+    return ret;
+  }
+  else
+    return 0;
+}
+
+static int
+py_can_trim (void *handle)
+{
+  PyObject *obj = handle;
+  PyObject *fn;
+  PyObject *args;
+  PyObject *r;
+  int ret;
+
+  if (callback_defined ("can_trim", &fn)) {
+    PyErr_Clear ();
+
+    args = PyTuple_New (1);
+    Py_INCREF (obj); /* decremented by Py_DECREF (args) */
+    PyTuple_SetItem (args, 0, obj);
+    r = PyObject_CallObject (fn, args);
+    Py_DECREF (fn);
+    Py_DECREF (args);
+    if (check_python_failure ("can_trim") == -1)
+      return -1;
+    ret = r == Py_True;
+    Py_DECREF (r);
+    return ret;
+  }
+  /* No Perl can_trim callback, but there's a Perl trim callback
+   * defined, so return 1.  (In C modules, nbdkit would do this).
+   */
+  else if (callback_defined ("trim", NULL))
+    return 1;
+  else
+    return 0;
+}
+
+#define py_config_help \
+  "script=<FILENAME>     (required) The Python plugin to run.\n" \
+  "[other arguments may be used by the plugin that you load]"
+
+#define THREAD_MODEL NBDKIT_THREAD_MODEL_SERIALIZE_ALL_REQUESTS
+
+static struct nbdkit_plugin plugin = {
+  .name              = "python",
+  .version           = PACKAGE_VERSION,
+
+  .load              = py_load,
+  .unload            = py_unload,
+
+  .config            = py_config,
+  .config_complete   = py_config_complete,
+  .config_help       = py_config_help,
+
+  .open              = py_open,
+  .close             = py_close,
+
+  .get_size          = py_get_size,
+  .can_write         = py_can_write,
+  .can_flush         = py_can_flush,
+  .is_rotational     = py_is_rotational,
+  .can_trim          = py_can_trim,
+
+  .pread             = py_pread,
+  .pwrite            = py_pwrite,
+  .flush             = py_flush,
+  .trim              = py_trim,
+};
+
+NBDKIT_REGISTER_PLUGIN(plugin)

rescuezilla/nbdkit

commit 9e7869e0f23917fe9e56e8f2ae0c2d8f678d2c68

python: Clarify possible handle values returned by open() method
Previously we stated that any "non-NULL" Python value could be
returned as the handle, which doesn't make any sense.  In fact any
Python object can be returned, even None.  The way to indicate an
error is to throw an exception.  Clarify the documentation and
example.

This goes all the way back to when the Python plugin was introduced in
commit 185539eed1 ("python: New plugin that lets you write plugins as
Python scripts", Feb 2014).

Fixes: commit 185539eed1aafa601ee2ed3471ff8d868d23cba5

plugins/python/examples/ramdisk.py
@@ -49,9 +49,10 @@ def config(key, value):
 def open(readonly):
     nbdkit.debug("open: readonly=%d" % readonly)
 
-    # You can return any non-NULL Python object from open, and the
-    # same object will be passed as the first arg to the other
-    # callbacks [in the client connected phase].
+    # You can return any Python object from open (even None), and the
+    # same object will be passed as the first arg 'h' to the other
+    # callbacks below.  To return an error from this method you must
+    # throw an exception.
     return 1
 
 

plugins/python/nbdkit-python-plugin.pod
@@ -307,8 +307,9 @@ There are no arguments or return value.
  def open(readonly):
    # return handle
 
-You can return any non-NULL Python value as the handle.  It is passed
-back in subsequent calls.
+You can return any Python value (even C<None>) as the handle.  It is
+passed back as the first arg C<'h'> in subsequent calls.  To return an
+error from this method you must throw an exception.
 
 =item C<close>
 