rescuezilla/nbdkit

commit 7b9301a4c569456a4f96784229a2cd48e8957662

partition: enhance filter to work with 512 and 4k sector disks
Also:

 - ensure partition filter only reads in full sectors at a time
 - set default sector size based on next's block_size min
 - update docs

Reviewed-by: Richard W.M. Jones <rjones@redhat.com>

filters/partition/partition-mbr.c
@@ -76,7 +76,7 @@ find_mbr_partition (nbdkit_next *next,
   struct mbr_partition partition;
   uint32_t ep_start_sector, ep_nr_sectors;
   uint64_t ebr, next_ebr;
-  uint8_t sector[sector_size];
+  uint8_t sector[SECTOR_SIZE_4K]; /* may be reading 512b or 4k at a time */
 
   /* Primary partition. */
   if (partnum <= 4) {
@@ -86,6 +86,12 @@ find_mbr_partition (nbdkit_next *next,
           partition.part_type_byte != 0 &&
           !is_extended (partition.part_type_byte) &&
           partnum == i+1) {
+        if (partition.part_type_byte == 0xEE) {
+          nbdkit_error ("rejecting GPT protective entry from MBR, "
+                        "if the underlying storage uses 4K sectors "
+                        "try using partition-sectorsize=4k");
+          return -1;
+        }
         *offset_r = partition.start_sector * (int64_t) sector_size;
         *range_r = partition.nr_sectors * (int64_t) sector_size;
         return 0;
@@ -132,7 +138,7 @@ find_mbr_partition (nbdkit_next *next,
 
       /* Read the EBR sector. */
       nbdkit_debug ("partition: reading EBR at %" PRIi64, ebr);
-      if (next->pread (next, sector, sizeof sector, ebr, 0, &errno) == -1)
+      if (next->pread (next, sector, sector_size, ebr, 0, &errno) == -1)
         return -1;
 
       if (partnum == i) {

filters/partition/nbdkit-partition-filter.pod
@@ -29,6 +29,21 @@ The partition number to serve, counting from 1.
 
 This parameter is required.
 
+=item B<partition-sectorsize=>SECTOR_SIZE
+
+The default for this parameter is chosen according to the minimum block
+size advertised by the underlying device.  If it is 512 or 4K, then the
+sector size is set to that.  If the underlying device does not advertise
+a minimum block size, or it is something other than 512 or 4K, the
+default is 512 bytes.
+
+The user may specify 512 or 4096 as the sector size manually via config.
+
+However set, this parameter instructs the partition table
+logic to treat the underlying storage as having the
+indicated sector size.  This is important for MBR
+and GPT logic using LBA (logical block addressing).
+
 =back
 
 =head1 EXAMPLE

filters/partition/partition-gpt.c
@@ -44,7 +44,7 @@
 
 #include "partition.h"
 
-static void
+static int
 get_gpt_header (uint8_t *sector,
                 uint32_t *nr_partition_entries,
                 uint32_t *size_partition_entry)
@@ -52,6 +52,19 @@ get_gpt_header (uint8_t *sector,
   struct gpt_header *header = (struct gpt_header *) sector;
   *nr_partition_entries = le32toh (header->nr_partition_entries);
   *size_partition_entry = le32toh (header->size_partition_entry);
+
+  /* Many things are not checked here, but in particular, previously
+   * written code has always needed partition_entries_lba to equal 2
+   * because the original logic assumed the partition entries followed
+   * the header, rather than looking in the header to see what LBA
+   * they actually start at.  Let's at least check for that now.
+   */
+   if (header->partition_entries_lba != 2) {
+     nbdkit_error ("non-standard GPT layout: "
+                   "partition entries are not adjacent to header");
+     return -1;
+   }
+   return 0;
 }
 
 static void
@@ -70,21 +83,29 @@ find_gpt_partition (nbdkit_next *next,
                     int64_t size, uint8_t *header_bytes,
                     int64_t *offset_r, int64_t *range_r)
 {
-  uint8_t partition_bytes[128];
-  uint32_t nr_partition_entries, size_partition_entry;
+  uint8_t partition_entry_sector[SECTOR_SIZE_4K]; /* May only use 512 bytes */
+  uint8_t *partition_bytes;
+  uint32_t nr_partition_entries, size_partition_entry, entries_per_sector;
   uint8_t partition_type_guid[16];
   uint64_t first_lba, last_lba;
   int i;
   int err;
 
-  get_gpt_header (header_bytes, &nr_partition_entries, &size_partition_entry);
+  if (get_gpt_header (header_bytes,
+                      &nr_partition_entries, &size_partition_entry) == -1) {
+    nbdkit_error ("cannot support non-standard GPT header");
+    return -1;
+  }
+
   if (partnum > nr_partition_entries) {
     nbdkit_error ("GPT partition number out of range");
     return -1;
   }
 
-  if (size_partition_entry < 128) {
-    nbdkit_error ("GPT partition entry size is < 128 bytes");
+  if (size_partition_entry < 128 || size_partition_entry > sector_size ||
+      sector_size % size_partition_entry != 0) {
+    nbdkit_error ("GPT partition entry size is invalid (%d bytes)",
+      size_partition_entry);
     return -1;
   }
 
@@ -98,11 +119,18 @@ find_gpt_partition (nbdkit_next *next,
     return -1;
   }
 
+  entries_per_sector = sector_size / size_partition_entry;
+
   for (i = 0; i < nr_partition_entries; ++i) {
     /* We already checked these are within bounds above. */
-    if (next->pread (next, partition_bytes, sizeof partition_bytes,
-                     2 * sector_size + i * size_partition_entry, 0, &err) == -1)
-      return -1;
+    if (i % entries_per_sector == 0) {
+      if (next->pread (next, partition_entry_sector, sector_size,
+                       sector_size * (2 + i / entries_per_sector), 0,
+                       &err) == -1)
+        return -1;
+    }
+    partition_bytes =
+      &partition_entry_sector[(i % entries_per_sector) * size_partition_entry];
     get_gpt_partition (partition_bytes,
                        partition_type_guid, &first_lba, &last_lba);
     if (memcmp (partition_type_guid,

filters/partition/partition.c
@@ -48,7 +48,11 @@
 
 unsigned partnum = 0;
 
-unsigned sector_size = 512;
+/* sector_size will default to 512, may get set to 4096.
+ * finalized in prepare. user can override via config.
+ * if still zero at prepare time, apply default.
+ */
+unsigned sector_size = 0;
 
 /* Called for each key=value passed on the command line. */
 static int
@@ -60,6 +64,15 @@ partition_config (nbdkit_next_config *next, nbdkit_backend *nxdata,
       return -1;
     if (partnum == 0) {
       nbdkit_error ("invalid partition number");
+      return -1;
+    }
+    return 0;
+  }
+  else if (strcmp (key, "partition-sectorsize") == 0) {
+    sector_size = nbdkit_parse_size (value);
+    /* N.B. error from parse_size() -> -1 is covered by below: -1 != [512, 4096] */
+    if ( sector_size != SECTOR_SIZE_512 && sector_size != SECTOR_SIZE_4K ) {
+      nbdkit_error ("invalid partition-sectorsize, it must be '512' or '4086'");
       return -1;
     }
     return 0;
@@ -126,9 +139,23 @@ partition_prepare (nbdkit_next *next,
 {
   struct handle *h = handle;
   int64_t size;
-  uint8_t lba01[2*sector_size]; /* LBA 0 and 1 */
+  uint8_t lba01[2*SECTOR_SIZE_4K]; /* LBA 0 and 1, may only use 2*512 bytes */
   int r;
   int err;
+
+  if (sector_size == 0) {
+    uint32_t minimum, preferred, maximum;
+    if (next->block_size (next, &minimum, &preferred, &maximum) != 0)
+      return -1;
+    if (minimum == SECTOR_SIZE_512 || minimum == SECTOR_SIZE_4K) {
+      nbdkit_debug ("underlying storage has a minimum read blocksize: "
+                    "setting partition-sectorsize=%d",
+                    minimum);
+      sector_size = minimum;
+    } else {
+      sector_size = SECTOR_SIZE_DEFAULT; /* aka 512 */
+    }
+  }
 
   size = next->get_size (next);
   if (size == -1)
@@ -140,11 +167,23 @@ partition_prepare (nbdkit_next *next,
 
   nbdkit_debug ("disk size=%" PRIi64, size);
 
-  if (next->pread (next, lba01, sizeof lba01, 0, 0, &err) == -1)
+  if (next->pread (next, lba01, 2 * sector_size, 0, 0, &err) == -1)
     return -1;
 
   /* Is it GPT? */
-  if (size >= 2 * 34 * sector_size &&
+  /* for 512b sectors, this used to be 2 * 34 * sector_size.
+   * Which was pmbr sector + primary gpt header sector + 32 sectors of entries,
+   * and then doubled.
+   * (though the secondary only has header & entries, so the pmbr shouldn't
+   * have been part of the doubling, but was harmless overcounting,
+   * since the minimum disk should have at least one LBA in a partition, and
+   * in practice is much, much larger.
+   * Now that we are 512b/4k sector-aware, count the entries as the 16kb they
+   * are.  Only the pmbr and headers are tied to the sector size.
+   * This also now more closely matches the similar calculation done in
+   * partition-gpt.c
+   */
+  if (size >= 3 * sector_size + 2 * 128 * 128  &&
       memcmp (&lba01[sector_size], "EFI PART", 8) == 0) {
     r = find_gpt_partition (next, size, &lba01[sector_size], &h->offset,
                             &h->range);

filters/partition/partition.h
@@ -35,6 +35,10 @@
 
 #include <nbdkit-filter.h>
 
+#define SECTOR_SIZE_4K      4096
+#define SECTOR_SIZE_512     512
+#define SECTOR_SIZE_DEFAULT SECTOR_SIZE_512
+
 extern unsigned partnum;
 
 extern unsigned sector_size;

commit cd761c9bf770b23f678fd82f0d1c8d4cce2ed1b5

partition: Suggest alternate partition-sectorsize
When we reach this error it means that we have failed to detect the
"EFI PART" signature (indicating GPT) and we've fallen back to parsing
MBR, but in doing so we have discovered a GPT protective MBR which
should only happen for GPT.  A possible cause for missing the
signature was because we have the wrong sector size.

Therefore check for the current sector size (which should be either
512 or 4096) and suggest that the user sets the other sector size.

Also avoids the case where the user already set partition-sectorsize=4k
and we were suggesting that they set it again.

Reported-by: Ming Xie
Fixes: commit 7b9301a4c569456a4f96784229a2cd48e8957662
Fixes: https://issues.redhat.com/browse/RHEL-19815

filters/partition/partition-mbr.c
@@ -87,9 +87,16 @@ find_mbr_partition (nbdkit_next *next,
           !is_extended (partition.part_type_byte) &&
           partnum == i+1) {
         if (partition.part_type_byte == 0xEE) {
-          nbdkit_error ("rejecting GPT protective entry from MBR, "
-                        "if the underlying storage uses 4K sectors "
-                        "try using partition-sectorsize=4k");
+          if (sector_size == 512)
+            nbdkit_error ("rejecting GPT protective entry from MBR, "
+                          "if the underlying storage uses 4K sectors "
+                          "try using partition-sectorsize=4k");
+          else if (sector_size == 4096)
+            nbdkit_error ("rejecting GPT protective entry from MBR, "
+                          "if the underlying storage uses 512 byte sectors "
+                          "try using partition-sectorsize=512");
+          else
+            nbdkit_error ("rejecting GPT protective entry from MBR");
           return -1;
         }
         *offset_r = partition.start_sector * (int64_t) sector_size;