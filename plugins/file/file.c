rescuezilla/nbdkit

commit 525b242caae89f9b8fcfc9aba749516e4fd98703

file: Return minimum and preferred I/O size for block devices
When this plugin is serving a block device, read the minimum and
preferred I/O sizes and return those to the NBD client.  There is no
upper limit.

For files this does nothing at the moment.  In theory we could
synthesize that information as well from filesystem block size.

This document was useful:
https://access.redhat.com/articles/3911611

plugins/file/file.c
@@ -60,7 +60,7 @@
 #endif
 
 #if defined (__linux__) && HAVE_LINUX_FS_H
-#include <linux/fs.h>       /* For BLKZEROOUT, BLKROTATIONAL */
+#include <linux/fs.h>       /* For BLK* constants. */
 #endif
 
 #define NBDKIT_API_VERSION 2
@@ -68,6 +68,7 @@
 
 #include "cleanup.h"
 #include "isaligned.h"
+#include "ispowerof2.h"
 #include "fdatasync.h"
 #include "utils.h"
 
@@ -472,6 +473,7 @@ struct handle {
   bool is_block_device;
   int sector_size;
   unsigned short rotational;
+  uint32_t minimum, preferred, maximum;
   bool can_write;
   bool can_punch_hole;
   bool can_zero_range;
@@ -677,6 +679,28 @@ file_open (int readonly)
   }
 #endif
 
+  h->minimum = h->preferred = h->maximum = 0;
+#if defined(BLKIOMIN) && defined(BLKIOOPT)
+  if (h->is_block_device) {
+    unsigned int minimum = 0, preferred = 0;
+
+    if (ioctl (h->fd, BLKIOMIN, &minimum) != 0)
+      nbdkit_debug ("cannot get BLKIOMIN: %s: %m", file);
+    if (ioctl (h->fd, BLKIOOPT, &preferred) != 0)
+      nbdkit_debug ("cannot get BLKIOOPT: %s: %m", file);
+
+    /* Check the values are sane before assigning them.  There is no
+     * upper limit on request sizes.
+     */
+    if (minimum >= 512 && preferred >= minimum &&
+        is_power_of_2 (minimum) && is_power_of_2 (preferred)) {
+      h->minimum = minimum;
+      h->preferred = preferred;
+      h->maximum = 0xffffffff;
+    }
+  }
+#endif
+
 #ifdef FALLOC_FL_PUNCH_HOLE
   h->can_punch_hole = true;
 #else
@@ -733,6 +757,19 @@ file_is_rotational (void *handle)
   struct handle *h = handle;
 
   return h->rotational;
+}
+
+/* Return minimum, preferred and maximum block size. */
+static int
+file_block_size (void *handle, uint32_t *minimum,
+                 uint32_t *preferred, uint32_t *maximum)
+{
+  struct handle *h = handle;
+
+  *minimum = h->minimum;
+  *preferred = h->preferred;
+  *maximum = h->maximum;
+  return 0;
 }
 
 /* Check if file is read-only. */
@@ -1163,6 +1200,7 @@ static struct nbdkit_plugin plugin = {
   .close             = file_close,
   .get_size          = file_get_size,
   .is_rotational     = file_is_rotational,
+  .block_size        = file_block_size,
   .can_write         = file_can_write,
   .can_multi_conn    = file_can_multi_conn,
   .can_trim          = file_can_trim,

commit 780599d2e77c7cc4c1a7e99d0a933289761a9b27

file: Rethink calculation of block device I/O size preferences
While testing this and after examining the kernel source I found that
BLKIOOPT is almost always returned as 0.  Only the MD (RAID) device
calculates it (as the stripe size).  Change the calculation to use max
of the minimum size and 4K in this case.

This commit also adds tests.  Testing the block size constraints
through libnbd works as expected.  However through libguestfs it does
not give the right results.  I didn't investigate this further as the
problem could be anywhere in qemu or Linux.

Also, fix error checking of ioctl to be consistent with other code in
the same source file.

Fixes: commit 525b242caae89f9b8fcfc9aba749516e4fd98703

.gitignore
@@ -154,6 +154,7 @@ plugins/*/*.3
 /tests/test-exitwhen-pipe-closed
 /tests/test-ext2
 /tests/test-file-block
+/tests/test-file-block-nbd
 /tests/test-golang
 /tests/test-gzip
 /tests/test-gzip-curl

plugins/file/file.c
@@ -67,9 +67,10 @@
 #include <nbdkit-plugin.h>
 
 #include "cleanup.h"
+#include "fdatasync.h"
 #include "isaligned.h"
 #include "ispowerof2.h"
-#include "fdatasync.h"
+#include "minmax.h"
 #include "utils.h"
 
 static enum {
@@ -682,20 +683,24 @@ file_open (int readonly)
   h->minimum = h->preferred = h->maximum = 0;
 #if defined(BLKIOMIN) && defined(BLKIOOPT)
   if (h->is_block_device) {
-    unsigned int minimum = 0, preferred = 0;
+    unsigned int minimum_io_size = 0, optimal_io_size = 0;
 
-    if (ioctl (h->fd, BLKIOMIN, &minimum) != 0)
+    if (ioctl (h->fd, BLKIOMIN, &minimum_io_size) == -1)
       nbdkit_debug ("cannot get BLKIOMIN: %s: %m", file);
-    if (ioctl (h->fd, BLKIOOPT, &preferred) != 0)
-      nbdkit_debug ("cannot get BLKIOOPT: %s: %m", file);
 
-    /* Check the values are sane before assigning them.  There is no
-     * upper limit on request sizes.
-     */
-    if (minimum >= 512 && preferred >= minimum &&
-        is_power_of_2 (minimum) && is_power_of_2 (preferred)) {
-      h->minimum = minimum;
-      h->preferred = preferred;
+    if (ioctl (h->fd, BLKIOOPT, &optimal_io_size) == -1)
+      nbdkit_debug ("cannot get BLKIOOPT: %s: %m", file);
+    else if (optimal_io_size == 0)
+      /* All devices in the Linux kernel except for MD report optimal
+       * as 0.  In that case guess a good value.
+       */
+      optimal_io_size = MAX (minimum_io_size, 4096);
+
+    /* Check the values are sane before using them. */
+    if (minimum_io_size >= 512 && is_power_of_2 (minimum_io_size) &&
+        optimal_io_size >= minimum_io_size && is_power_of_2 (optimal_io_size)) {
+      h->minimum = minimum_io_size;
+      h->preferred = optimal_io_size;
       h->maximum = 0xffffffff;
     }
   }

tests/Makefile.am
@@ -170,6 +170,7 @@ check-valgrind:
 # To run only tests which require root, use 'make check-root':
 root_tests = \
 	test-file-block \
+	test-file-block-nbd \
 	test-nbd-client.sh \
 	test-nbd-client-tls.sh \
 	test-swap.sh \
@@ -873,10 +874,15 @@ EXTRA_DIST += \
 	test-file-dirfd.sh \
 	$(NULL)
 LIBGUESTFS_TESTS += test-file-block
+LIBNBD_TESTS += test-file-block-nbd
 
 test_file_block_SOURCES = test-file-block.c test.h
 test_file_block_CFLAGS = $(WARNINGS_CFLAGS) $(LIBGUESTFS_CFLAGS)
 test_file_block_LDADD = libtest.la $(LIBGUESTFS_LIBS)
+
+test_file_block_nbd_SOURCES = test-file-block-nbd.c
+test_file_block_nbd_CFLAGS = $(WARNINGS_CFLAGS) $(LIBNBD_CFLAGS)
+test_file_block_nbd_LDADD = $(LIBNBD_LIBS)
 
 # floppy plugin test.
 TESTS += \

tests/test-file-block-nbd.c
@@ -40,9 +40,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-#include <guestfs.h>
-
-#include "test.h"
+#include <libnbd.h>
 
 #define DISK_SIZE (100 * 1024 * 1024)
 
@@ -52,21 +50,19 @@ static void detach_loopdev (void);
 int
 main (int argc, char *argv[])
 {
-  guestfs_h *g;
+  struct nbd_handle *nbd;
   int r;
   int fd;
   char cmd[64], buf[64];
   char disk[] = LARGE_TMPDIR "/diskXXXXXX"; /* Backing disk. */
   FILE *pp;
-  char *data;
   size_t len;
-  char *s;
   int64_t size;
 
   /* This test can only be run as root, and will be skipped otherwise. */
   if (geteuid () != 0) {
     fprintf (stderr, "%s: this test has to be run as root.\n",
-             program_name);
+             argv[0]);
     exit (77);
   }
 
@@ -74,7 +70,7 @@ main (int argc, char *argv[])
   r = access ("/dev/loop-control", W_OK);
   if (r != 0) {
     fprintf (stderr, "%s: /dev/loop-control is not writable.\n",
-             program_name);
+             argv[0]);
     exit (77);
   }
 
@@ -82,7 +78,7 @@ main (int argc, char *argv[])
   r = system ("losetup --version");
   if (r != 0) {
     fprintf (stderr, "%s: losetup program must be installed.\n",
-             program_name);
+             argv[0]);
     exit (77);
   }
 
@@ -108,7 +104,7 @@ main (int argc, char *argv[])
   }
   if (fgets (buf, sizeof buf, pp) == NULL) {
     fprintf (stderr, "%s: could not read loop device name from losetup\n",
-             program_name);
+             argv[0]);
     unlink (disk);
     exit (EXIT_FAILURE);
   }
@@ -133,106 +129,45 @@ main (int argc, char *argv[])
   strcpy (loopdev, buf);
   atexit (detach_loopdev);
 
+  /* Create the nbd handle. */
+  nbd = nbd_create ();
+  if (nbd == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
   /* Start nbdkit. */
-  if (test_start_nbdkit ("-D", "file.zero=1",
-                         "file", loopdev, NULL) == -1)
-    exit (EXIT_FAILURE);
-
-  g = guestfs_create ();
-  if (g == NULL) {
-    perror ("guestfs_create");
+  if (nbd_connect_command (nbd,
+                           (char *[]) {
+                             "nbdkit", "-s", "--exit-with-parent",
+                             "file", loopdev,
+                             NULL }) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
 
-  r = guestfs_add_drive_opts (g, "",
-                              GUESTFS_ADD_DRIVE_OPTS_FORMAT, "raw",
-                              GUESTFS_ADD_DRIVE_OPTS_PROTOCOL, "nbd",
-                              GUESTFS_ADD_DRIVE_OPTS_SERVER, server,
-                              -1);
-  if (r == -1)
+  /* Check the disk size matches the loop device size. */
+  size = nbd_get_size (nbd);
+  if (size == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
-
-  if (guestfs_launch (g) == -1)
-    exit (EXIT_FAILURE);
-
-  /* Check that the size of the disk seen through libguestfs & NBD
-   * matches the size of the file.  This ensures that the common/utils
-   * device_size function is working, but also that the size is passed
-   * through many layers up to us.
-   */
-  size = guestfs_blockdev_getsize64 (g, "/dev/sda");
-  if (size == -1)
-    exit (EXIT_FAILURE);
+  }
   if (size != DISK_SIZE) {
-    fprintf (stderr,
-             "%s FAILED: disk size incorrect "
-             "(actual: %" PRIi64 ", expected: %d)\n",
-             program_name, size, DISK_SIZE);
+    fprintf (stderr, "%s: incorrect export size, "
+             "expected: %d actual: %" PRIi64 "\n",
+             argv[0], DISK_SIZE, size);
     exit (EXIT_FAILURE);
   }
 
-  /* Print (but don't check) the minimum and optimal io sizes.  In
-   * theory these should come from the host loop device, via the file
-   * .block_size callback.  There's no official way to read these
-   * through libguestfs, and in any case with Linux 6.9 I don't see
-   * the correct values here.
-   */
-  const char *head_cmd[] = {
-    "head", "-1", "/sys/block/*/queue/*_io_size", NULL
-  };
-  guestfs_debug (g, "sh", (char **) head_cmd);
+  /* Print (don't check) the block size preferences. */
+  printf ("minimum = %" PRIu64 "\n",
+          nbd_get_block_size (nbd, LIBNBD_SIZE_MINIMUM));
+  printf ("preferred = %" PRIu64 "\n",
+          nbd_get_block_size (nbd, LIBNBD_SIZE_PREFERRED));
+  printf ("maximum = %" PRIu64 "\n",
+          nbd_get_block_size (nbd, LIBNBD_SIZE_MAXIMUM));
 
-  /* Partition the disk. */
-  if (guestfs_part_disk (g, "/dev/sda", "mbr") == -1)
-    exit (EXIT_FAILURE);
-  if (guestfs_mkfs (g, "ext4", "/dev/sda1") == -1)
-    exit (EXIT_FAILURE);
-
-  if (guestfs_mount_options (g, "discard", "/dev/sda1", "/") == -1)
-    exit (EXIT_FAILURE);
-
-#define filename "/hello.txt"
-#define content "hello, people of the world"
-
-  if (guestfs_write (g, filename, content, strlen (content)) == -1)
-    exit (EXIT_FAILURE);
-
-  data = guestfs_cat (g, filename);
-  if (!data)
-    exit (EXIT_FAILURE);
-
-  if (strcmp (data, content) != 0) {
-    fprintf (stderr,
-             "%s FAILED: unexpected content of %s file "
-             "(actual: %s, expected: %s)\n",
-             program_name, filename, data, content);
-    exit (EXIT_FAILURE);
-  }
-
-  /* Run sync to test flush path. */
-  if (guestfs_sync (g) == -1)
-    exit (EXIT_FAILURE);
-
-  /* Run fstrim to test trim path.  However only recent versions of
-   * libguestfs have this, and it probably only works in recent
-   * versions of qemu.
-   */
-#ifdef GUESTFS_HAVE_FSTRIM
-  if (guestfs_fstrim (g, "/", -1) == -1)
-    exit (EXIT_FAILURE);
-#endif
-
-  /* Run fallocate(1) on the device to test zero path. */
-  if (guestfs_umount (g, "/") == -1)
-    exit (EXIT_FAILURE);
-  const char *sh[] = { "fallocate", "-nzl", "64k", "/dev/sda", NULL };
-  s = guestfs_debug (g, "sh", (char **) sh);
-  free (s);
-
-  if (guestfs_shutdown (g) == -1)
-    exit (EXIT_FAILURE);
-
-  guestfs_close (g);
+  nbd_close (nbd);
 
   /* The atexit handler should detach the loop device and clean up
    * the backing disk.