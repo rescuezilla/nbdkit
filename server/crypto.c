rescuezilla/nbdkit

commit f889d822dd96bcf8c0427bd541d730d58b166d83

server/crypto.c: Check <gnutls/socket.h> works before including it
(cherry picked from commit 47987e43bdeb8c0ed6b7b85fe46a970a36e82376)

server/crypto.c
@@ -55,8 +55,12 @@
 #include <gnutls/gnutls.h>
 #include <gnutls/x509.h>
 
-#if defined HAVE_GNUTLS_TRANSPORT_IS_KTLS_ENABLED && \
-  defined HAVE_GNUTLS_SOCKET_H
+/* See comment in configure.ac */
+#ifdef HAVE_GNUTLS_SOCKET_H
+#include <gnutls/socket.h>
+#endif
+
+#ifdef HAVE_GNUTLS_TRANSPORT_IS_KTLS_ENABLED
 #define TRY_KTLS 1
 #else
 #define TRY_KTLS 0

configure.ac
@@ -683,6 +683,18 @@ AS_IF([test "x$GNUTLS_LIBS" != "x"],[
     AC_DEFINE_UNQUOTED([TLS_PRIORITY],["$tls_priority"],
                        [Default TLS session priority string])
 
+    # Check for working <gnutls/socket.h>.  This is only needed for
+    # gnutls 3.7.9 and no other version (unfortunately used by Debian
+    # 12 and others).  In that release kTLS functionality was added to
+    # <gnutls/socket.h> incorrectly, before being moved to
+    # <gnutls/gnutls.h> in 3.8.0.  <gnutls/socket.h> breaks on
+    # Windows, so we must make inclusion conditional.  When we move
+    # the minimum version of gnutls >= 3.8.0 we can get rid of this.
+    old_CFLAGS="$CFLAGS"
+    CFLAGS="$GNUTLS_CFLAGS $CFLAGS"
+    AC_CHECK_HEADERS([gnutls/socket.h])
+    CFLAGS="$old_CFLAGS"
+
     # Check for APIs which may not be present.
     old_LIBS="$LIBS"
     LIBS="$GNUTLS_LIBS $LIBS"

commit 14a2646d4221d50b4b194d5dd2d6010808ea642b

nbdkit 1.38: Fix inclusion of <gnutls/socket.h> with old gnutls
Development branch commit 8ce65bac5a ("build: Move to minimum gnutls
>= 3.5.18") changed the way that <gnutls/socket.h> is included.  I
didn't cherry pick this commit to the stable-1.38 branch since we
don't want to force people to update gnutls.

However when I cherry picked commit 47987e43bd ("server/crypto.c:
Check <gnutls/socket.h> works before including it"), I missed that
there was still a place where <gnutls/socket.h> was included (which
would have been fixed by the commit above).

The upshot is that compilation of nbdkit 1.38.3 fails on mingw:

In file included from ../../server/crypto.c:69:
/usr/i686-w64-mingw32/sys-root/mingw/include/gnutls/socket.h:32:10: fatal error: sys/socket.h: No such file or directory
   32 | #include <sys/socket.h>
      |          ^~~~~~~~~~~~~~
compilation terminated.

Fixes: commit f889d822dd96bcf8c0427bd541d730d58b166d83

server/crypto.c
@@ -65,9 +65,6 @@
 #else
 #define TRY_KTLS 0
 #endif
-#if TRY_KTLS
-#include <gnutls/socket.h>
-#endif
 
 static int crypto_auth;
 #define CRYPTO_AUTH_CERTIFICATES 1