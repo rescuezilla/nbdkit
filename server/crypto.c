rescuezilla/nbdkit

commit a3759199c93300cf9de24a129c4141609dacb047

server: Add nbdkit_peer_tls_dn to read the TLS Distinguished Name

server/crypto.c
@@ -786,6 +786,99 @@ crypto_negotiate_tls (int sockin, int sockout)
   return -1;
 }
 
+NBDKIT_DLL_PUBLIC char *
+nbdkit_peer_tls_dn (void)
+{
+  GET_CONN;
+  gnutls_session_t session = conn->crypto_session;
+  gnutls_credentials_type_t cred;
+  const gnutls_datum_t *cert_list;
+  unsigned int cert_list_size = 0;
+  gnutls_x509_crt_t cert = NULL;
+  gnutls_datum_t dn = { 0 };
+  int r;
+  char *ret = NULL;
+
+  if (!session) {
+    nbdkit_debug ("nbdkit_peer_tls_dn: no TLS session");
+    goto out_no_dn;
+  }
+
+  cred = gnutls_auth_get_type (session);
+  if (cred != GNUTLS_CRD_CERTIFICATE) {
+    nbdkit_debug ("nbdkit_peer_tls_dn: "
+                  "TLS session not using certificates");
+    goto out_no_dn;
+  }
+
+  if (gnutls_certificate_type_get (session) != GNUTLS_CRT_X509) {
+    nbdkit_debug ("nbdkit_peer_tls_dn: "
+                  "TLS session not using X.509 certificates");
+    goto out_no_dn;
+  }
+
+  cert_list = gnutls_certificate_get_peers (session, &cert_list_size);
+  if (cert_list == NULL || cert_list_size == 0) {
+    /* This can happen if tls_verify_peer is not set.  It could also
+     * happen because gnutls cannot allocate the certificate list but
+     * that should be a rare error.  Return no DN in this case.
+     */
+    nbdkit_debug ("nbdkit_peer_tls_dn: "
+                  "no client certificates (is --tls-verify-peer set?)");
+    goto out_no_dn;
+  }
+
+  /* XXX According to the spec, certificates should be sent with the
+   * client's certificate first, but the man page for
+   * gnutls_certificate_get_peers says there are X.509 clients which
+   * send them in a random order.  However it's unlikely that there
+   * are any NBD clients doing this.  Anyway we only consider the
+   * first certificate in the list.
+   */
+  r = gnutls_x509_crt_init (&cert);
+  if (r != 0) {
+    nbdkit_error ("gnutls_x509_crt_init: %s", gnutls_strerror (r));
+    goto out;
+  }
+  r = gnutls_x509_crt_import (cert, &cert_list[0], GNUTLS_X509_FMT_DER);
+  if (r != 0) {
+    nbdkit_error ("gnutls_x509_crt_import: %s", gnutls_strerror (r));
+    goto out;
+  }
+
+  r = gnutls_x509_crt_get_dn3 (cert, &dn, 0);
+  if (r != 0) {
+    nbdkit_error ("gnutls_x509_crt_get_dn3: %s", gnutls_strerror (r));
+    goto out;
+  }
+  ret = strdup ((char *) dn.data);
+  if (ret == NULL) {
+    nbdkit_error ("strdup: %m");
+    ret = NULL;
+    goto out;
+  }
+
+ out:
+  if (cert)
+    gnutls_x509_crt_deinit (cert);
+  if (dn.data)
+    gnutls_free (dn.data);
+  return ret;
+
+  /* Not an error, but no DN for this session. */
+ out_no_dn:
+  if (cert)
+    gnutls_x509_crt_deinit (cert);
+  if (dn.data)
+    gnutls_free (dn.data);
+  ret = strdup ("");
+  if (ret == NULL) {
+    nbdkit_error ("strdup: %m");
+    return NULL;
+  }
+  return ret;
+}
+
 #else /* !HAVE_GNUTLS */
 
 /* GnuTLS was not available at compile time.  These are stub versions
@@ -819,6 +912,13 @@ crypto_negotiate_tls (int sockin, int sockout)
 {
   /* Should never be called because tls == 0. */
   abort ();
+}
+
+NBDKIT_DLL_PUBLIC char *
+nbdkit_peer_tls_dn (void)
+{
+  nbdkit_error ("nbdkit_peer_tls_dn is not supported on this platform");
+  return -1;
 }
 
 #endif /* !HAVE_GNUTLS */

.gitignore
@@ -39,6 +39,7 @@ docs/nbdkit_parse_int.3
 docs/nbdkit_parse_probability.3
 docs/nbdkit_parse_size.3
 docs/nbdkit_peer_name.3
+docs/nbdkit_peer_tls_dn.3
 docs/nbdkit-plugin.3
 docs/nbdkit_read_password.3
 docs/nbdkit_realpath.3

docs/Makefile.am
@@ -130,6 +130,7 @@ generated_mans = \
 	nbdkit_parse_probability.3 \
 	nbdkit_parse_size.3 \
 	nbdkit_peer_name.3 \
+	nbdkit_peer_tls_dn.3 \
 	nbdkit-plugin.3 \
 	nbdkit-probing.1 \
 	nbdkit-protocol.1 \
@@ -251,6 +252,11 @@ nbdkit_parse_size.3: nbdkit_parse_size.pod $(top_builddir)/podwrapper.pl
 	    $<
 
 nbdkit_peer_name.3: nbdkit_peer_name.pod $(top_builddir)/podwrapper.pl
+	$(PODWRAPPER) --section=3 --man $@ \
+	    --html $(top_builddir)/html/$@.html \
+	    $<
+
+nbdkit_peer_tls_dn.3: nbdkit_peer_tls_dn.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<

docs/nbdkit-plugin.pod
@@ -1392,7 +1392,9 @@ using L<nbdkit_peer_name(3)>.  If the client is connected over a Unix
 domain socket, you may also be able to get the client process ID, user
 ID, group ID and security context using: L<nbdkit_peer_pid(3)>,
 L<nbdkit_peer_uid(3)>, L<nbdkit_peer_gid(3)> and
-L<nbdkit_peer_security_context(3)>.
+L<nbdkit_peer_security_context(3)>.  If the client is connected using
+TLS and presented a client certificate then the Distinguished Name
+(DN) may be read using L<nbdkit_peer_tls_dn(3)>.
 
 =head1 SHUTDOWN
 
@@ -1638,6 +1640,7 @@ L<nbdkit_peer_gid(3)>,
 L<nbdkit_peer_name(3)>,
 L<nbdkit_peer_pid(3)>,
 L<nbdkit_peer_security_context(3)>,
+L<nbdkit_peer_tls_dn(3)>.
 L<nbdkit_peer_uid(3)>,
 L<nbdkit_printf_intern(3)>,
 L<nbdkit_read_password(3)>,

docs/nbdkit-tls.pod
@@ -413,6 +413,7 @@ L<nbdkit(1)>,
 L<nbdkit-luks-filter(1)>,
 L<nbdkit-tls-fallback-filter(1)>,
 L<nbdkit_is_tls(3)>,
+L<nbdkit_peer_tls_dn(3)>,
 L<nbdcopy(1)>,
 L<nbdfuse(1)>,
 L<nbdinfo(1)>,

docs/nbdkit_peer_tls_dn.pod
@@ -1,0 +1,89 @@
+=head1 NAME
+
+nbdkit_peer_tls_dn - read the client TLS X.509 Distinguished Name
+
+=head1 SYNOPSIS
+
+ #include <nbdkit-plugin.h>
+
+ char *nbdkit_peer_tls_dn (void);
+
+=head1 DESCRIPTION
+
+If all of these conditions are met:
+
+=over 4
+
+=item *
+
+the client connects over TLS (see L<nbdkit-tls(1)>)
+
+=item *
+
+the TLS connection is using X.509 certificates
+
+=item *
+
+the client presents a client certificate
+
+=item *
+
+the nbdkit I<--tls-verify-peer> option is used
+
+=back
+
+then this API can be used to get the Distinguished Name (DN) of the
+certificate.  This is returned as a string of the form
+C<"CN=xx,O=yy,[...]C=zz"> (see S<RFC 4514> for the exact format).
+
+If the DN is not available, C<""> is returned (this is not an error).
+
+=head2 EXAMPLE CERTIFICATE AND DISTINGUISHED NAME
+
+Please see L<nbdkit-tls(1)> for details of how to generate client
+certificates.  Following those instructions, if you were to generate a
+certificate from this F<client.info> input:
+
+ country = US
+ state = New York
+ locality = New York
+ organization = BigCo
+ cn = client.example.com
+ tls_www_client
+ encryption_key
+ signing_key
+
+then the client DN would be:
+
+ CN=client.example.com,O=BigCo,L=New York,ST=New York,C=US
+
+=head1 RETURN VALUE
+
+On success this function returns a TLS Distinguished Name as a string,
+or C<"">.  The caller must free this string.
+
+If there is an error it calls L<nbdkit_error(3)> and returns C<NULL>.
+
+=head1 LANGUAGE BINDINGS
+
+=head1 HISTORY
+
+C<nbdkit_peer_tls_dn> was added in nbdkit 1.40.
+
+=head1 SEE ALSO
+
+L<nbdkit(1)>,
+L<nbdkit-plugin(3)>,
+L<nbdkit-filter(3)>,
+L<nbdkit-tls(1)>,
+L<nbdkit-ip-filter(1)>,
+L<nbdkit_is_tls(3)>,
+L<nbdkit_peer_name(3)>.
+
+=head1 AUTHORS
+
+Richard W.M. Jones
+
+=head1 COPYRIGHT
+
+Copyright Red Hat

include/nbdkit-common.h
@@ -142,6 +142,7 @@ NBDKIT_EXTERN_DECL (int64_t, nbdkit_peer_pid, (void));
 NBDKIT_EXTERN_DECL (int64_t, nbdkit_peer_uid, (void));
 NBDKIT_EXTERN_DECL (int64_t, nbdkit_peer_gid, (void));
 NBDKIT_EXTERN_DECL (char *, nbdkit_peer_security_context, (void));
+NBDKIT_EXTERN_DECL (char *, nbdkit_peer_tls_dn, (void));
 NBDKIT_EXTERN_DECL (void, nbdkit_shutdown, (void));
 NBDKIT_EXTERN_DECL (void, nbdkit_disconnect, (int force));
 

server/nbdkit.syms
@@ -79,6 +79,7 @@
     nbdkit_peer_name;
     nbdkit_peer_pid;
     nbdkit_peer_security_context;
+    nbdkit_peer_tls_dn;
     nbdkit_peer_uid;
     nbdkit_printf_intern;
     nbdkit_read_password;

commit 7563596fdb7587dfb8708b5eeeb4b97d738ba79d

server/crypto.c: Fix fallback functions used when gnutls is unavailable
The return value was wrong resulting in these errors:

crypto.c: In function ‘nbdkit_peer_tls_dn’:
crypto.c:945:3: error: return makes pointer from integer without a cast [-Werror]
   return -1;
   ^
crypto.c: In function ‘nbdkit_peer_tls_issuer_dn’:
crypto.c:953:3: error: return makes pointer from integer without a cast [-Werror]
   return -1;
   ^

Fixes: commit a3759199c93300cf9de24a129c4141609dacb047
Fixes: commit bf77f76112e3ffbc9149a54fbeb0a505310f28a2

server/crypto.c
@@ -942,7 +942,7 @@ nbdkit_peer_tls_dn (void)
 {
   nbdkit_error ("%s is not supported on this platform",
                 "nbdkit_peer_tls_dn");
-  return -1;
+  return NULL;
 }
 
 NBDKIT_DLL_PUBLIC char *
@@ -950,7 +950,7 @@ nbdkit_peer_tls_issuer_dn (void)
 {
   nbdkit_error ("%s is not supported on this platform",
                 "nbdkit_peer_tls_issuer_dn");
-  return -1;
+  return NULL;
 }
 
 #endif /* !HAVE_GNUTLS */