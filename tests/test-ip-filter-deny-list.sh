rescuezilla/nbdkit

commit 017a4b06b48b11260197b3bc878219c38591bdf5

ip: Allow matching on the TLS Distinguished Name

filters/ip/ip.c
@@ -40,6 +40,10 @@
 #include <string.h>
 #include <assert.h>
 
+#ifdef HAVE_FNMATCH_H
+#include <fnmatch.h>
+#endif
+
 #ifdef HAVE_SYS_SOCKET_H
 #include <sys/socket.h>
 #endif
@@ -71,19 +75,25 @@ struct rule {
   struct rule *next;
   enum { BAD = 0,
          ANY, ANYV4, ANYV6, IPV4, IPV6,
-         ANYUNIX, PID, UID, GID, SECURITY,
+         ANYUNIX, DN, PID, UID, GID, SECURITY,
          ANYVSOCK, VSOCKCID, VSOCKPORT } type;
   union {
     struct in_addr ipv4;        /* for IPV4, IPV6 */
     struct in6_addr ipv6;
     int64_t id;                 /* for PID, UID, GID, VSOCKCID, VSOCKPORT */
     char *label;                /* for SECURITY (must be freed) */
+    const char *glob;           /* for DN */
   } u;
   unsigned prefixlen;           /* for IPV4, IPV6 */
 };
 
 static struct rule *allow_rules, *allow_rules_last;
 static struct rule *deny_rules, *deny_rules_last;
+
+/* If there are any dn: rules then we must do more expensive "late
+ * filtering" in the .open method.
+ */
+static bool late_filtering = false;
 
 static void
 print_rule (const char *name, const struct rule *rule, const char *suffix)
@@ -118,6 +128,9 @@ print_rule (const char *name, const struct rule *rule, const char *suffix)
 
   case ANYUNIX:
     nbdkit_debug ("%s=anyunix%s", name, suffix);
+    break;
+  case DN:
+    nbdkit_debug ("%s=dn:%s%s", name, rule->u.glob, suffix);
     break;
   case PID:
     nbdkit_debug ("%s=pid:%" PRIi64 "%s", name, rule->u.id, suffix);
@@ -279,6 +292,12 @@ parse_rule (const char *paramname,
     return 0;
   }
 
+  if (n >= 3 && ascii_strncasecmp (value, "dn:", 3) == 0) {
+    late_filtering = true;
+    new_rule->type = DN;
+    new_rule->u.glob = &value[3];
+    return 0;
+  }
   if (n >= 4 && ascii_strncasecmp (value, "pid:", 4) == 0) {
     new_rule->type = PID;
     if (nbdkit_parse_int64_t ("pid:", &value[4], &new_rule->u.id) == -1)
@@ -407,6 +426,10 @@ parse_rules (const char *paramname,
 {
   size_t n;
 
+  /* Special exception for dn: since they contain commas. */
+  if (ascii_strncasecmp (value, "dn:", 3) == 0)
+    return parse_rule (paramname, rules, rules_last, value, strlen (value));
+
   while (*value != '\0') {
     n = strcspn (value, ",");
     if (n == 0) {
@@ -489,6 +512,42 @@ ipv6_equal (struct in6_addr addr1, struct in6_addr addr2, unsigned prefixlen)
   return true;
 }
 
+#ifdef HAVE_FNMATCH_H
+static bool
+dn_matches (const char *glob)
+{
+  char *dn = nbdkit_peer_tls_dn ();
+  int r;
+  bool ret;
+
+  if (dn == NULL)
+    return false;
+
+  if (ip_debug_rules)
+    nbdkit_debug ("ip: TLS DN = \"%s\"", dn);
+
+  r = fnmatch (glob, dn, FNM_CASEFOLD);
+  switch (r) {
+  case 0:
+    ret = true;
+    break;
+  case FNM_NOMATCH:
+    ret = false;
+    break;
+  default:
+    /* I even looked at the glibc code to see what the unspecified
+     * "errors" were and it was not clear.  The best we can do here is
+     * report the code and errno, but continue as if it doesn't match.
+     */
+    nbdkit_error ("fnmatch returned error code %d: %m", r);
+    ret = false;
+  }
+
+  free (dn);
+  return ret;
+}
+#endif
+
 static bool
 matches_rule (const struct rule *rule,
               const struct sockaddr *addr)
@@ -528,6 +587,13 @@ matches_rule (const struct rule *rule,
 
   case ANYUNIX:
     return family == AF_UNIX;
+
+  case DN:
+#ifdef HAVE_FNMATCH_H
+    return dn_matches (rule->u.glob);
+#else
+    return false;
+#endif
 
     /* Note these work even if the underlying nbdkit_peer_* call fails. */
   case PID:
@@ -676,21 +742,44 @@ check_if_allowed (void)
   return true;
 }
 
+/* Normal/early filtering, if there are no dn: rules. */
 static int
 ip_preconnect (nbdkit_next_preconnect *next, nbdkit_backend *nxdata,
                int readonly)
 {
-  /* Follow the rules. */
-  if (check_if_allowed () == false) {
-    nbdkit_error ("client not permitted to connect "
-                  "because of source address restriction");
-    return -1;
+  if (!late_filtering) {
+    /* Follow the rules. */
+    if (check_if_allowed () == false) {
+      nbdkit_error ("client not permitted to connect "
+                    "because of source address restriction");
+      return -1;
+    }
   }
 
   if (next (nxdata, readonly) == -1)
     return -1;
 
   return 0;
+}
+
+/* Late filtering, if there are dn: rules. */
+static void *
+ip_open (nbdkit_next_open *next, nbdkit_context *nxdata,
+         int readonly, const char *exportname, int is_tls)
+{
+  if (late_filtering) {
+    /* Follow the rules. */
+    if (check_if_allowed () == false) {
+      nbdkit_error ("client not permitted to connect "
+                    "because of source address restriction");
+      return NULL;
+    }
+  }
+
+  if (next (nxdata, readonly, exportname) == -1)
+    return NULL;
+
+  return NBDKIT_HANDLE_NOT_NEEDED;
 }
 
 static int
@@ -708,6 +797,7 @@ static struct nbdkit_filter filter = {
   .config_complete   = ip_config_complete,
   .config_help       = ip_config_help,
   .preconnect        = ip_preconnect,
+  .open              = ip_open,
 };
 
 NBDKIT_REGISTER_FILTER (filter)

filters/ip/nbdkit-ip-filter.pod
@@ -135,6 +135,30 @@ address representations can be used (see S<RFC 5952>).
 
 This matches a range of IPv6 addresses C<A:B:.../NN>.
 
+=item B<dn:>GLOB
+
+(nbdkit E<ge> 1.40, not Windows)
+
+This matches the TLS Distinguished Name (DN) of the client certificate
+against the wildcard C<GLOB>.  See L<nbdkit_peer_tls_dn(3)> and
+L<nbdkit-tls(1)> for further information.
+
+Because DNs contain commas (C<,>) but comma is also normally used to
+split allow/deny list elements, this filter has a special exception
+where if the first 3 characters of the allow or deny parameter match
+C<"dn:"> then the parameter is I<not> split.  This lets you match on
+DNs naturally using:
+
+ nbdkit --filter=ip allow=dn:"*,O=BigCo,*" allow=dn:"*,C=US" [...]
+
+B<Note:> If your allow or deny patterns contain any DN rules then
+I<all> filtering has to be done later in the
+L<connection lifecycle|nbdkit-plugin(3)/Callback lifecycle> since we
+must wait until TLS negotiation has been completed.  This makes
+filtering more expensive and potentially easier to cause DoS attacks.
+You should try to combine DN rules with early filtering outside nbdkit
+like using a firewall or VPN.
+
 =item B<pid:>PID
 
 (nbdkit E<ge> 1.24, Linux only)
@@ -255,7 +279,9 @@ L<nbdkit_peer_name(3)>,
 L<nbdkit_peer_pid(3)>,
 L<nbdkit_peer_uid(3)>,
 L<nbdkit_peer_gid(3)>,
-L<nbdkit_peer_security_context(3)>.
+L<nbdkit_peer_security_context(3)>,
+L<nbdkit_peer_tls_dn(3)>,
+L<nbdkit-tls(1)>.
 
 =head1 AUTHORS
 

tests/Makefile.am
@@ -1795,6 +1795,7 @@ TESTS += \
 	test-ip-filter.sh \
 	test-ip-filter-anyunix.sh \
 	test-ip-filter-anyvsock.sh \
+	test-ip-filter-dn.sh \
 	test-ip-filter-pid.sh \
 	test-ip-filter-uid.sh \
 	test-ip-filter-gid.sh \
@@ -1804,6 +1805,7 @@ EXTRA_DIST += \
 	test-ip-filter.sh \
 	test-ip-filter-anyunix.sh \
 	test-ip-filter-anyvsock.sh \
+	test-ip-filter-dn.sh \
 	test-ip-filter-pid.sh \
 	test-ip-filter-uid.sh \
 	test-ip-filter-gid.sh \

configure.ac
@@ -379,6 +379,7 @@ AC_CHECK_HEADERS([\
         arpa/inet.h \
         byteswap.h \
         endian.h \
+        fnmatch.h \
         grp.h \
         linux/fs.h \
         netdb.h \

tests/test-ip-filter-dn.sh
@@ -1,0 +1,81 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright Red Hat
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# Test the ip filter with dn: parameter.
+
+source ./functions.sh
+set -e
+set -x
+
+requires_nbdinfo
+requires_run
+
+# Does the nbdkit binary support TLS?
+if ! nbdkit --dump-config | grep -sq tls=yes; then
+    echo "$0: nbdkit built without TLS support"
+    exit 77
+fi
+
+# RHEL 8 libnbd / nbdinfo doesn't support the tls-certificates
+# parameter in URIs, so connections always fail.  It's hard to detect
+# if libnbd supports this, so just go off version number.  The libnbd
+# commit adding this feature was 847e0b9830, added in libnbd 1.9.5.
+requires_libnbd_version 1.10
+
+# Did we create the PKI files?
+# Probably 'certtool' is missing.
+pkidir="$PWD/pki"
+if [ ! -f "$pkidir/ca-cert.pem" ]; then
+    echo "$0: PKI files were not created by the test harness"
+    exit 77
+fi
+
+# This is expected to succeed.
+nbdkit -v --tls=require --tls-certificates="$pkidir" --tls-verify-peer \
+       -D nbdkit.tls.session=1 \
+       null \
+       -D ip.rules=1 --filter=ip \
+       allow=dn:"*,O=Test,*,C=US" \
+       deny=all \
+       --run 'nbdinfo "$uri"'
+
+# This is expected to fail.
+if nbdkit -v --tls=require --tls-certificates="$pkidir" --tls-verify-peer \
+       -D nbdkit.tls.session=1 \
+       null \
+       -D ip.rules=1 --filter=ip \
+       allow=dn:"CN=foobar,*,C=US" \
+       deny=all \
+       --run 'nbdinfo "$uri"'; then
+    echo "$0: expected test to fail"
+    exit 1
+fi

commit b4a847976f252640a7cf829960c8d0cd792bbc28

ip: Do late filtering in list_exports as well as open
When a client lists NBD exports (eg. nbdinfo --list) then they can get
to the .list_exports callback before .open.  This could mean that a
banned client can list exports.

This is only the case when late filtering rules are in play (see
nbdkit-ip-filter(3)) which is not the normal way this filter is used.

In fact this is the case:

  $ mkdir /var/tmp/p
  $ touch /var/tmp/p/secret_export
  $ nbdkit --filter=ip file dir=/var/tmp/p allow=dn:123 deny=all \
      --run 'nbdsh --opt-mode -u "$uri" -c "h.opt_list(lambda *args: print(*args))"'
  secret_export

Above I used the 'dn:' rule, which uses late filtering.  The client
should not be able to connect at all, but can still list the exports,
thus seeing that there is a 'secret_export' (although the client
cannot open it).

When this patch is applied, the client cannot list any exports:

  $ nbdkit --filter=ip file dir=/var/tmp/p allow=dn:123 deny=all \
      --run 'nbdsh --opt-mode -u "$uri" -c "h.opt_list(lambda *args: print(*args))"'
  nbdkit: file[1]: error: client not permitted to connect because of source address restriction
  nbdsh: command line script failed: nbd_opt_list: server replied with error to list request: Operation not supported

Fixes: commit 017a4b06b48b11260197b3bc878219c38591bdf5

filters/ip/ip.c
@@ -786,7 +786,25 @@ ip_preconnect (nbdkit_next_preconnect *next, nbdkit_backend *nxdata,
   return 0;
 }
 
-/* Late filtering, if there are dn:/issuer-dn: rules. */
+/* Late filtering. */
+static int
+ip_list_exports (nbdkit_next_list_exports *next, nbdkit_backend *nxdata,
+                 int readonly, int is_tls,
+                 struct nbdkit_exports *exports)
+{
+  if (late_filtering) {
+    /* Follow the rules. */
+    if (check_if_allowed () == false) {
+      nbdkit_error ("client not permitted to connect "
+                    "because of source address restriction");
+      return -1;
+    }
+  }
+
+  return next (nxdata, readonly, exports);
+}
+
+/* Late filtering. */
 static void *
 ip_open (nbdkit_next_open *next, nbdkit_context *nxdata,
          int readonly, const char *exportname, int is_tls)
@@ -821,6 +839,7 @@ static struct nbdkit_filter filter = {
   .config_complete   = ip_config_complete,
   .config_help       = ip_config_help,
   .preconnect        = ip_preconnect,
+  .list_exports      = ip_list_exports,
   .open              = ip_open,
 };
 

filters/ip/nbdkit-ip-filter.pod
@@ -274,7 +274,8 @@ lifecycle in more detail.
 However if your allow or deny patterns contain any C<dn:> or
 C<issuer-dn:> rules then I<all> filtering has to be done later in the
 connection lifecycle, since we must wait until TLS negotiation has
-been completed.  Filtering happens in the C<.open> callback instead.
+been completed.  Filtering happens in the C<.open> or C<.list_exports>
+callback instead.
 
 In practice this makes filtering more expensive and potentially makes
 it easier to cause Denial of Service (DoS) attacks.  You should try to

tests/Makefile.am
@@ -1812,6 +1812,7 @@ TESTS += \
 	test-ip-filter-uid.sh \
 	test-ip-filter-gid.sh \
 	test-ip-filter-security.sh \
+	test-ip-filter-deny-list.sh \
 	$(NULL)
 EXTRA_DIST += \
 	test-ip-filter.sh \
@@ -1823,6 +1824,7 @@ EXTRA_DIST += \
 	test-ip-filter-uid.sh \
 	test-ip-filter-gid.sh \
 	test-ip-filter-security.sh \
+	test-ip-filter-deny-list.sh \
 	$(NULL)
 
 # limit filter test.

tests/test-ip-filter-deny-list.sh
@@ -1,0 +1,79 @@
+#!/usr/bin/env bash
+# nbdkit
+# Copyright Red Hat
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# Test that listing exports is denied.
+
+source ./functions.sh
+set -e
+set -x
+
+requires nbdsh --version
+requires_nbdsh_uri
+requires_nbdinfo
+requires_run
+
+# Listing exports should work in the ordinary case.
+nbdkit -v null -D ip.rules=1 --filter=ip allow=all --run 'nbdinfo --list "$uri"'
+
+# Listing exports should be denied in the early filtering case.
+nbdkit -v null \
+       -D ip.rules=1 --filter=ip deny=all \
+       --run 'export uri; nbdsh -c -' <<'EOF'
+import os
+uri = os.getenv('uri')
+h = nbd.NBD()
+h.set_opt_mode(True)
+try:
+    h.connect_uri(uri)
+    h.opt_list(lambda *args: print(*args))
+    assert False
+except:
+    # Expect connect_uri to fail.
+    pass
+EOF
+
+# Same in the late filtering case.
+nbdkit -v null \
+       -D ip.rules=1 --filter=ip allow=dn:123 deny=all \
+       --run 'export uri; nbdsh -c -' <<'EOF'
+import os
+uri = os.getenv('uri')
+h = nbd.NBD()
+h.set_opt_mode(True)
+try:
+    h.connect_uri(uri)
+    h.opt_list(lambda *args: print(*args))
+    assert False
+except:
+    # Expect opt_list to fail.
+    pass
+EOF