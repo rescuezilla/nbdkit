rescuezilla/nbdkit

commit 2b5663f6b0ff7bc0814c775c01d54ec8ab8312d5

Introduce lzip-filter
This implements a filter for random access into lzip-compressed files
with multiple members. Compressed files with a single member are only
partially supported. The decompression is done using liblzma.

The function lzma_lzip_decoder() is not available on all platforms, so
there is a corresponding check in configure.ac. Unsupported platforms
include the following:

* x86_64-almalinux-9
* x86_64-centos-stream-9
* x86_64-debian-11
* x86_64-opensuse-leap-15
* x86_64-ubuntu-2204

This filter is based on the xz filter and uses filters/xz/blkcache.c.

Closes: https://gitlab.com/nbdkit/nbdkit/-/issues/23
Closes: https://gitlab.com/nbdkit/nbdkit/-/merge_requests/62
Reviewed-by: Richard W.M. Jones <rjones@redhat.com>

filters/lzip/lzipindex.c
@@ -1,0 +1,121 @@
+/* nbdkit lzip index
+ * Copyright Jan Felix Langenbach
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+
+#include <assert.h>
+#include <stdlib.h>
+
+#include "lzipindex.h"
+
+int
+lzip_index_prepend (lzip_index *index, lzip_index_member const *member)
+{
+  assert (index);
+  assert (member);
+
+  lzip_index_member *new_members =
+    realloc (index->members, (index->member_count + 1) * sizeof (index->members[0]));
+  if (!new_members) return 1;
+
+  index->members = new_members;
+  index->members[index->member_count++] = *member;
+  return 0;
+}
+
+void
+lzip_index_finalize (lzip_index *index)
+{
+  assert (index);
+
+  uint64_t combined_data_size = 0;
+  uint64_t indexable_data_size = 0;
+
+  for (size_t j = 0; j < index->member_count; ++j) {
+    size_t i = index->member_count - j - 1;
+    lzip_index_member *member = &index->members[i];
+
+    member->data_offset = combined_data_size;
+    combined_data_size += member->data_size;
+
+    if (j == 0) {
+      indexable_data_size = member->data_size;
+      continue;
+    }
+
+    if (i == 0 || indexable_data_size == 0) {
+      continue;
+    }
+
+    if (member->data_size != indexable_data_size) {
+      indexable_data_size = 0;
+    }
+  }
+
+  index->combined_data_size = combined_data_size;
+  index->indexable_data_size = indexable_data_size;
+}
+
+static int
+comparator (void const *key, void const *element)
+{
+  uint64_t const *data_offset_ptr = key;
+  uint64_t data_offset = *data_offset_ptr;
+
+  lzip_index_member const *member = element;
+
+  if (data_offset < member->data_offset) return +1;
+  if (data_offset < member->data_offset + member->data_size) return 0;
+  return -1;
+}
+
+lzip_index_member const *
+lzip_index_search (lzip_index const *index, uint64_t data_offset)
+{
+  if (!index) return NULL;
+  if (data_offset > index->combined_data_size) return NULL;
+
+  if (index->indexable_data_size) {
+    size_t member_index = data_offset / index->indexable_data_size;
+    return &index->members[index->member_count - member_index - 1];
+  }
+
+  return bsearch (&data_offset, index->members, index->member_count,
+                  sizeof(index->members[0]), comparator);
+}
+
+void
+lzip_index_destroy (lzip_index *index)
+{
+  if (!index) return;
+  free (index->members);
+  *index = (lzip_index) {};
+}

.gitignore
@@ -91,6 +91,7 @@ plugins/*/*.3
 /docs/filter-links.pod
 /docs/lang-plugin-links.pod
 /docs/plugin-links.pod
+/filters/lzip/blkcache.c
 /fuzzing/sync_dir/
 /fuzzing/testcase_dir/[0-f]*
 /html/*.html

configure.ac
@@ -132,6 +132,7 @@ filters="\
         limit \
         log \
         luks \
+        lzip \
         multi-conn \
         nocache \
         noextents \
@@ -1429,10 +1430,10 @@ AC_ARG_ENABLE([nbd-plugin],
 AM_CONDITIONAL([HAVE_LIBNBD],
   [test "x$LIBNBD_LIBS" != "x" && test "x$enable_nbd_plugin" = "xyes"])
 
-dnl Check for liblzma (only if you want to compile the xz filter).
+dnl Check for liblzma (only if you want to compile the xz and lzip filters).
 AC_ARG_WITH([liblzma],
     [AS_HELP_STRING([--without-liblzma],
-                    [disable xz filter @<:@default=check@:>@])],
+                    [disable xz and lzip filters @<:@default=check@:>@])],
     [],
     [with_liblzma=check])
 AS_IF([test "$with_liblzma" != "no"],[
@@ -1441,9 +1442,22 @@ AS_IF([test "$with_liblzma" != "no"],[
         AC_SUBST([LIBLZMA_LIBS])
         AC_DEFINE([HAVE_LIBLZMA],[1],[liblzma found at compile time.])
     ],
-    [AC_MSG_WARN([liblzma not found, xz filter will be disabled])])
+    [AC_MSG_WARN([liblzma not found, xz and lzip filters will be disabled])])
 ])
 AM_CONDITIONAL([HAVE_LIBLZMA],[test "x$LIBLZMA_LIBS" != "x"])
+
+AS_IF([test "x$LIBLZMA_LIBS" != "x"],[
+    old_LIBS="$LIBS"
+    LIBS="$LIBLZMA_LIBS $LIBS"
+    AC_CHECK_FUNCS([lzma_lzip_decoder])
+    LIBS="$old_LIBS"
+    AS_IF([test "x$ac_cv_func_lzma_lzip_decoder" != "xyes"], [
+        AC_MSG_WARN([liblzma without lzip support, lzip filter will be disabled])
+    ])
+])
+AM_CONDITIONAL([HAVE_LZMA_LZIP_DECODER],
+               [test "x$LIBLZMA_LIBS" != "x" && \
+                test "x$ac_cv_func_lzma_lzip_decoder" = "xyes"])
 
 dnl Check for zstd (only if you want to compile allocator=zstd).
 AC_ARG_WITH([libzstd],
@@ -1685,6 +1699,7 @@ AC_CONFIG_FILES([Makefile
                  filters/limit/Makefile
                  filters/log/Makefile
                  filters/luks/Makefile
+                 filters/lzip/Makefile
                  filters/multi-conn/Makefile
                  filters/nocache/Makefile
                  filters/noextents/Makefile
@@ -1792,6 +1807,7 @@ feature "bzip2"               test "x$HAVE_BZLIB_TRUE" = "x"
 feature "ext2"                test "x$HAVE_EXT2_TRUE" = "x"
 feature "gzip"                test "x$HAVE_ZLIB_TRUE" = "x"
 feature "luks"                test "x$HAVE_GNUTLS_PBKDF2_TRUE" = "x"
+feature "lzip"                test "x$HAVE_LZMA_LZIP_DECODER_TRUE" = "x"
 feature "stats"               test "x$HAVE_CXX_TRUE" = "x"
 feature "xz"                  test "x$HAVE_LIBLZMA_TRUE" = "x"
 

filters/bzip2/nbdkit-bzip2-filter.pod
@@ -14,6 +14,14 @@ of L<nbdkit-file-plugin(1)> to decompress a local F<.bz2> file.
 
 The filter only allows read-only connections.
 
+B<Note> that bzip2 files are not very good for random access in large
+files because seeking to a position in the bzip2 file involves
+decompressing all data before that point in the file.  A more
+practical method to compress large disk images is to use L<xz(1)>
+or L<plzip(1)>.  Both L<nbdkit-xz-filter(1)> and L<nbdkit-lzip-filter(1)>
+support block-level decompression if the file was compressed using the
+right options.
+
 To allow seeking this filter has to keep the contents of the complete
 uncompressed file, which it does in a hidden temporary file under
 C<$TMPDIR>.
@@ -56,6 +64,7 @@ from the C<nbdkit-gzip-filter> which first appeared in nbdkit 1.22.
 
 L<nbdkit-file-plugin(1)>,
 L<nbdkit-gzip-filter(1)>,
+L<nbdkit-lzip-filter(1)>,
 L<nbdkit-tar-filter(1)>,
 L<nbdkit-xz-filter(1)>,
 L<nbdkit(1)>,

filters/gzip/nbdkit-gzip-filter.pod
@@ -26,8 +26,10 @@ The filter only allows read-only connections.
 B<Note> that gzip files are not very good for random access in large
 files because seeking to a position in the gzip file involves
 decompressing all data before that point in the file.  A more
-practical method to compress large disk images is to use the L<xz(1)>
-format and L<nbdkit-xz-filter(1)>.
+practical method to compress large disk images is to use L<xz(1)>
+or L<plzip(1)>.  Both L<nbdkit-xz-filter(1)> and L<nbdkit-lzip-filter(1)>
+support block-level decompression if the file was compressed using the
+right options.
 
 To allow seeking this filter has to keep the contents of the complete
 uncompressed file, which it does in a hidden temporary file under
@@ -72,6 +74,7 @@ from C<nbdkit-gzip-plugin> which first appeared in nbdkit 1.0.
 L<nbdkit-bzip2-filter(1)>,
 L<nbdkit-curl-plugin(1)>,
 L<nbdkit-file-plugin(1)>,
+L<nbdkit-lzip-filter(1)>,
 L<nbdkit-tar-filter(1)>,
 L<nbdkit-xz-filter(1)>,
 L<nbdkit(1)>,

filters/lzip/Makefile.am
@@ -1,0 +1,101 @@
+# nbdkit lzip filter
+# Copyright Jan Felix Langenbach
+# Copyright Red Hat
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# * Neither the name of Red Hat nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+include $(top_srcdir)/common-rules.mk
+
+EXTRA_DIST = nbdkit-lzip-filter.pod
+
+if HAVE_LZMA_LZIP_DECODER
+
+filter_LTLIBRARIES = nbdkit-lzip-filter.la
+
+# This filter shares some source files with nbdkit-xz-filter.  In
+# RHEL 7 we cannot add the C files from ../xz directly to SOURCES
+# because subdir-objects is broken.  Instead we create symlinks to
+# them.
+BUILT_SOURCES = \
+	blkcache.c \
+	$(NULL)
+blkcache.c: $(srcdir)/../xz/blkcache.c
+	ln -f -s $(srcdir)/../xz/$@
+CLEANFILES += $(BUILT_SOURCES)
+
+nbdkit_lzip_filter_la_SOURCES = \
+	lzip.c \
+	lzipfile.c \
+	lzipfile.h \
+	lzipindex.c \
+	lzipindex.h \
+	$(BUILT_SOURCES) \
+	$(srcdir)/../xz/blkcache.h \
+	$(top_srcdir)/include/nbdkit-filter.h \
+	$(NULL)
+
+nbdkit_lzip_filter_la_CPPFLAGS = \
+	-I$(top_srcdir)/include \
+	-I$(top_builddir)/include \
+	-I$(top_srcdir)/filters/xz \
+	-I$(top_srcdir)/common/include \
+	-I$(top_srcdir)/common/utils \
+	$(NULL)
+nbdkit_lzip_filter_la_CFLAGS = \
+	$(WARNINGS_CFLAGS) \
+	$(LIBLZMA_CFLAGS) \
+	$(NULL)
+nbdkit_lzip_filter_la_LIBADD = \
+	$(LIBLZMA_LIBS) \
+	$(top_builddir)/common/utils/libutils.la \
+	$(top_builddir)/common/replacements/libcompat.la \
+	$(IMPORT_LIBRARY_ON_WINDOWS) \
+	$(NULL)
+nbdkit_lzip_filter_la_LDFLAGS = \
+	-module -avoid-version -shared $(NO_UNDEFINED_ON_WINDOWS) \
+	$(NULL)
+if USE_LINKER_SCRIPT
+nbdkit_lzip_filter_la_LDFLAGS += \
+	-Wl,--version-script=$(top_srcdir)/filters/filters.syms
+endif
+
+if HAVE_POD
+
+man_MANS = nbdkit-lzip-filter.1
+CLEANFILES += $(man_MANS)
+
+nbdkit-lzip-filter.1: nbdkit-lzip-filter.pod \
+		$(top_builddir)/podwrapper.pl
+	$(PODWRAPPER) --section=1 --man $@ \
+		--html $(top_builddir)/html/$@.html \
+		$<
+
+endif HAVE_POD
+
+endif HAVE_LZMA_LZIP_DECODER

filters/lzip/lzip.c
@@ -1,0 +1,277 @@
+/* nbdkit lzip filter
+ * Copyright Jan Felix Langenbach
+ * Copyright Red Hat
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <string.h>
+
+#include <lzma.h>
+
+#include <nbdkit-filter.h>
+
+#include "blkcache.h"
+#include "lzipfile.h"
+
+static uint64_t maxblock = 512 * 1024 * 1024;
+static uint32_t maxdepth = 8;
+
+static int
+lzip_config (nbdkit_next_config *next, nbdkit_backend *nxdata,
+             const char *key, const char *value)
+{
+  if (strcmp (key, "lzip-max-block") == 0) {
+    int64_t r = nbdkit_parse_size (value);
+    if (r == -1)
+      return -1;
+    maxblock = (uint64_t) r;
+    return 0;
+  }
+  else if (strcmp (key, "lzip-max-depth") == 0) {
+    if (nbdkit_parse_uint32_t ("lzip-max-depth", value, &maxdepth) == -1)
+      return -1;
+    if (maxdepth == 0) {
+      nbdkit_error ("'lzip-max-depth' parameter must be >= 1");
+      return -1;
+    }
+    return 0;
+  }
+  else
+    return next (nxdata, key, value);
+}
+
+#define lzip_config_help \
+  "lzip-max-block=<SIZE> (optional) Maximum block size allowed (default: 512M)\n"\
+  "lzip-max-depth=<N>    (optional) Maximum blocks in cache (default: 8)\n"
+
+/* The per-connection handle. */
+struct lzip_handle {
+  lzipfile *lz;
+
+  /* Block cache. */
+  blkcache *c;
+};
+
+/* Create the per-connection handle. */
+static void *
+lzip_open (nbdkit_next_open *next, nbdkit_context *nxdata,
+           int readonly, const char *exportname, int is_tls)
+{
+  struct lzip_handle *h;
+
+  /* Always pass readonly=1 to the underlying plugin. */
+  if (next (nxdata, 1, exportname) == -1)
+    return NULL;
+
+  h = malloc (sizeof *h);
+  if (h == NULL) {
+    nbdkit_error ("malloc: %m");
+    return NULL;
+  }
+
+  h->c = new_blkcache (maxdepth);
+  if (!h->c) {
+    free (h);
+    return NULL;
+  }
+
+  /* Initialized in lz_prepare. */
+  h->lz = NULL;
+
+  return h;
+}
+
+/* Free up the per-connection handle. */
+static void
+lzip_close (void *handle)
+{
+  struct lzip_handle *h = handle;
+  blkcache_stats stats;
+
+  blkcache_get_stats (h->c, &stats);
+  nbdkit_debug ("cache: hits = %zu, misses = %zu", stats.hits, stats.misses);
+
+  lzipfile_close (h->lz);
+  free_blkcache (h->c);
+  free (h);
+}
+
+static int
+lzip_prepare (nbdkit_next *next, void *handle,
+              int readonly)
+{
+  struct lzip_handle *h = handle;
+
+  h->lz = lzipfile_open (next);
+  if (!h->lz)
+    return -1;
+
+  if (maxblock < lzipfile_max_uncompressed_block_size (h->lz)) {
+    nbdkit_error ("lzip file largest block is bigger than lzip-max-block\n"
+                  "Either recompress the lzip file with smaller blocks "
+                  "(see nbdkit-lzip-filter(1))\n"
+                  "or make lzip-max-block parameter bigger.\n"
+                  "Current lzip-max-block = %" PRIu64 " (bytes)\n"
+                  "Largest block in lzip file = %" PRIu64 " (bytes)",
+                  maxblock,
+                  lzipfile_max_uncompressed_block_size (h->lz));
+    return -1;
+  }
+
+  return 0;
+}
+
+/* Description. */
+static const char *
+lzip_export_description (nbdkit_next *next,
+                         void *handle)
+{
+  const char *base = next->export_description (next);
+
+  if (!base)
+    return NULL;
+  return nbdkit_printf_intern ("expansion of lzip-compressed image: %s", base);
+}
+
+/* Get the file size. */
+static int64_t
+lzip_get_size (nbdkit_next *next, void *handle)
+{
+  struct lzip_handle *h = handle;
+
+  return lzipfile_get_size (h->lz);
+}
+
+/* We need this because otherwise the layer below can_write is called
+ * and that might return true (eg. if the plugin has a pwrite method
+ * at all), resulting in writes being passed through to the layer
+ * below.  This is possibly a bug in nbdkit.
+ */
+static int
+lzip_can_write (nbdkit_next *next,
+                void *handle)
+{
+  return 0;
+}
+
+/* Whatever the plugin says, this filter is consistent across connections. */
+static int
+lzip_can_multi_conn (nbdkit_next *next,
+                     void *handle)
+{
+  return 1;
+}
+
+/* Similar to above. */
+static int
+lzip_can_extents (nbdkit_next *next,
+                  void *handle)
+{
+  return 0;
+}
+
+/* Cache */
+static int
+lzip_can_cache (nbdkit_next *next,
+                void *handle)
+{
+  /* We are already operating as a cache regardless of the plugin's
+   * underlying .can_cache, but it's easiest to just rely on nbdkit's
+   * behavior of calling .pread for caching.
+   */
+  return NBDKIT_CACHE_EMULATE;
+}
+
+/* Read data from the file. */
+static int
+lzip_pread (nbdkit_next *next,
+            void *handle, void *buf, uint32_t count, uint64_t offset,
+            uint32_t flags, int *err)
+{
+  struct lzip_handle *h = handle;
+  char *data;
+  uint64_t start, size;
+  uint32_t n;
+
+  /* Find the block in the cache. */
+  data = get_block (h->c, offset, &start, &size);
+  if (!data) {
+    /* Not in the cache.  We need to read the block from the xz file. */
+    data = lzipfile_read_block (h->lz, next, flags, err,
+                                offset, &start, &size);
+    if (data == NULL)
+      return -1;
+    put_block (h->c, start, size, data);
+  }
+
+  /* It's possible if the blocks are really small or oddly aligned or
+   * if the requests are large that we need to read the following
+   * block to satisfy the request.
+   */
+  n = count;
+  if (start + size - offset < n)
+    n = start + size - offset;
+
+  memcpy (buf, &data[offset-start], n);
+  buf += n;
+  count -= n;
+  offset += n;
+  if (count > 0)
+    return lzip_pread (next, h, buf, count, offset, flags, err);
+
+  return 0;
+}
+
+static int lzip_thread_model (void)
+{
+  return NBDKIT_THREAD_MODEL_SERIALIZE_REQUESTS;
+}
+
+static struct nbdkit_filter filter = {
+  .name               = "lzip",
+  .longname           = "nbdkit lzip filter",
+  .config             = lzip_config,
+  .config_help        = lzip_config_help,
+  .thread_model       = lzip_thread_model,
+  .open               = lzip_open,
+  .close              = lzip_close,
+  .prepare            = lzip_prepare,
+  .export_description = lzip_export_description,
+  .get_size           = lzip_get_size,
+  .can_write          = lzip_can_write,
+  .can_extents        = lzip_can_extents,
+  .can_cache          = lzip_can_cache,
+  .can_multi_conn     = lzip_can_multi_conn,
+  .pread              = lzip_pread,
+};
+
+NBDKIT_REGISTER_FILTER (filter)

commit 2803832d7f3a140e3faffac3d2f145b4de98a997

lzip: Include <config.h>
Not strictly necessary in this file, but ensures consistent size of
off_t, consistent use of _GNU_SOURCE, HAVE_* macros etc across the
code base.

Fixes: commit 2b5663f6b0ff7bc0814c775c01d54ec8ab8312d5

filters/lzip/lzipindex.c
@@ -30,6 +30,7 @@
  * SUCH DAMAGE.
  */
 
+#include <config.h>
 
 #include <assert.h>
 #include <stdlib.h>