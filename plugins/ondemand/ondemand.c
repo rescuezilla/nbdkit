rescuezilla/nbdkit

commit 1ef7e85cdb2e648d9e86ccff68f5c82afd7d767d

ondemand: Be less strict about filenames
When iPXE is used as a client, export names contain the MAC address,
which contains ':', resulting in failures:

nbdkit: ondemand[8]: error: invalid exportname ‘52:54:00:12:34:56’ rejected

Be less strict in general about the filenames allowed.

Reported-by: Markus Hartung
Fixes: https://gitlab.com/nbdkit/nbdkit/-/issues/25

plugins/ondemand/ondemand.c
@@ -272,6 +272,28 @@ ondemand_can_fua (void *handle)
   return NBDKIT_FUA_NATIVE;
 }
 
+/* Verify that the export name is valid. */
+static int
+check_exportname (const char *exportname)
+{
+  size_t len = strlen (exportname);
+
+  assert (len > 0); /* see .default_export */
+
+  if (len > NAME_MAX)
+    return -1;
+
+  /* Don't allow hidden files, or access to "." or "..". */
+  if (exportname[0] == '.')
+    return -1;
+
+  /* Don't allow directories. */
+  if (strchr (exportname, '/'))
+    return -1;
+
+  return 0;
+}
+
 /* This creates and runs the full "mkfs" (or whatever) command. */
 static int
 run_command (const char *disk)
@@ -367,13 +389,9 @@ ondemand_open (int readonly)
     nbdkit_error ("internal error: expected nbdkit_export_name () != NULL");
     goto error;
   }
-  assert (strcmp (h->exportname, "") != 0); /* see .default_export */
 
   /* Verify that the export name is valid. */
-  if (strlen (h->exportname) > NAME_MAX ||
-      strchr (h->exportname, '.') ||
-      strchr (h->exportname, '/') ||
-      strchr (h->exportname, ':')) {
+  if (check_exportname (h->exportname) == -1) {
     nbdkit_error ("invalid exportname ‘%s’ rejected", h->exportname);
     goto error;
   }

TODO
@@ -128,7 +128,8 @@ nbdkit-ondemand-plugin:
 * Implement more callbacks, eg. .zero
 
 * Allow client to select size up to a limit, eg. by sending export
-  names like ‘export:4G’.
+  names like ‘export:4G’.  This would have to be controlled by a
+  flag, since existing clients might be using ':'.
 
 nbdkit-data-plugin:
 

plugins/ondemand/nbdkit-ondemand-plugin.pod
@@ -38,10 +38,10 @@ client-configurable in future.  The filesystem type and label may also
 be specified, otherwise C<ext4> and no label is used.
 
 Export names must be E<le> C<NAME_MAX> (usually 255) bytes in length
-and must not contain certain characters including C<.>, C</> and C<:>.
-There may be other limitations added in future.  Client requests which
-do not obey these restrictions are rejected.  As a special case,
-export name C<""> is mapped to the file name F<default>.
+and must not contain certain characters including C<.> at the start,
+and C</>.  There may be other limitations added in future.  Client
+requests which do not obey these restrictions are rejected.  As a
+special case, export name C<""> is mapped to the file name F<default>.
 
 =head2 Security considerations
 

tests/test-ondemand.sh
@@ -68,7 +68,7 @@ test -f $dir/default
 test -f $dir/test
 
 # These should fail because the exportname is invalid.
-for e in /bad .bad bad. bad:bad ; do
+for e in /bad .bad . .. ./etc ; do
     if nbdinfo "nbd+unix:///$e?socket=$sock" ||
        qemu-img info "nbd+unix:///$e?socket=$sock" ||
        qemu-img info nbd:unix:$sock:exportname=$e

commit 50dc54c794deb55481a9dce14806ccfd960d20b5

ondemand: Filter out correct invalid exports in .list_exports
Previous commit 1ef7e85cdb ("ondemand: Be less strict about
filenames") permitted more exportnames, but the old test was still
contained in .list_exports, so it was possible to create an export but
it would not show up.  Use the same test in both places.

Fixes: https://gitlab.com/nbdkit/nbdkit/-/issues/25
Fixes: commit 1ef7e85cdb2e648d9e86ccff68f5c82afd7d767d

plugins/ondemand/ondemand.c
@@ -234,15 +234,16 @@ ondemand_list_exports (int readonly, int default_only,
 
   /* XXX Output is not sorted.  Does it matter? */
   while (errno = 0, (d = readdir (exportsdir)) != NULL) {
-    /* Skip any file containing non-permitted characters '.' and ':'.
-     * As a side effect this skips all dot-files.  Commands can use
-     * dot-files to "hide" files in the export dir (eg. if needing to
-     * keep state).
+    /* Skip any file that would be an invalid exportname.  Commands
+     * can use dot-files to "hide" files in the export dir (eg. if
+     * needing to keep state).
      */
-    if (strchr (d->d_name, '.') || strchr (d->d_name, ':'))
+    if (! is_valid_exportname (d->d_name))
       continue;
 
-    /* Skip the "default" filename which refers to the "" export. */
+    /* Skip the "default" filename which refers to the "" export,
+     * since we returned it first in the list above.
+     */
     if (strcmp (d->d_name, "default") == 0)
       continue;
 