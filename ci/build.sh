rescuezilla/nbdkit

commit 32b395a7709d028df6787aaefd4ced3f07da3c02

Add CI for GitLab
Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

ci/build.sh
@@ -1,0 +1,175 @@
+#!/bin/sh
+
+set -xe
+
+skip_tests() {
+    # Add a way to run all the tests, even the skipped ones, with an environment
+    # variable, so that it can be set fora branch or fork in GitLab.
+    if test "$SKIPPED_TESTS" != "force"
+    then
+        # Skip tests from ci/skipped_tests if this is the right OS version
+        # The file
+        os_id="$(sh -c '. /etc/os-release; echo "${NAME}-${VERSION_ID}"')"
+
+        echo OS ID: $os_id
+
+        # Skips comments and empty lines
+        grep '^[^#]' ci/skipped_tests | while read skipper
+        do
+            regex="${skipper%%;*}"
+            tests="${skipper#*;}"
+
+            echo SKIPPER: "$skipper"
+            echo REGEX: "$regex"
+            echo TESTS: "$tests"
+
+            # Ignore lines not meant for current $os_id
+            if ! echo "$os_id" | grep -q "$regex"; then echo NOPE; continue; fi
+
+            echo SKIPPING $tests
+
+            for test_case in $tests
+            do
+                test_case_bckup="${test_case}_skipped_test"
+                if ! git ls-files "$test_case" | grep -q "$test_case"
+                then
+                    $MAKE -C "$(dirname "$test_case")" "$(basename "$test_case")" 2>/dev/null || :
+                fi
+                echo Backing up "$test_case" to "${test_case_bckup}"
+                cp "$test_case" "${test_case_bckup}"
+
+                echo 'echo "Test skipped based on ci/skipped_tests file"'> "$test_case"
+                echo 'exit 77'>> "$test_case"
+                chmod +x "$test_case"
+            done
+        done
+    fi
+}
+
+restore_tests() {
+    find . -name '*_skipped_test' -print | while read test_case_bckup
+    do
+        test_case="${test_case_bckup%_skipped_test}"
+
+        echo Moving "${test_case_bckup}" back to "${test_case}"
+        rm -f "${test_case}"
+        mv -f "${test_case_bckup}" "${test_case}"
+    done
+}
+
+run_checks() {
+    skip_tests
+    failed=0
+    $MAKE "$@" || failed=1
+    restore_tests
+    return $failed
+}
+
+main() {
+    MAKE="${MAKE-make -j $(getconf _NPROCESSORS_ONLN)}"
+
+    autoreconf -if
+
+    CONFIG_ARGS="\
+--disable-libguestfs-tests
+--enable-gcc-warnings \
+--enable-option-checking
+--enable-linuxdisk
+--disable-valgrind
+--disable-libfuzzer
+--enable-plugins
+--enable-python
+--enable-ruby
+--enable-tcl
+--enable-lua
+--enable-torrent
+--with-iconv
+--with-curl
+--with-ssh
+--with-iso
+--with-libvirt
+--with-zlib
+--with-libnbd
+--with-liblzma
+--with-libzstd
+--with-libguestfs
+--with-ext2
+"
+
+    if test "$GOLANG" = "skip"
+    then
+        CONFIG_ARGS="$CONFIG_ARGS --disable-golang"
+    else
+        CONFIG_ARGS="$CONFIG_ARGS --enable-golang"
+    fi
+
+    if test "$RUST" = "skip"
+    then
+        CONFIG_ARGS="$CONFIG_ARGS --disable-rust"
+    else
+        CONFIG_ARGS="$CONFIG_ARGS --enable-rust"
+    fi
+
+    case $CROSS in
+        mingw64)
+            CONFIG_ARGS="\
+$CONFIG_ARGS
+--disable-ocaml
+--disable-perl
+--disable-vddk
+"
+            ;;
+        mingw32)
+            CONFIG_ARGS="\
+$CONFIG_ARGS
+--disable-ocaml
+--disable-perl
+--disable-vddk
+"
+            ;;
+        *)
+            CONFIG_ARGS="\
+$CONFIG_ARGS
+--enable-ocaml
+--enable-perl
+--enable-vddk
+"
+            ;;
+    esac
+
+    ./configure $CONFIGURE_OPTS $CONFIG_ARGS
+
+    $MAKE
+
+    if test -n "$CROSS" -a "$CROSS" != "i686"
+    then
+        echo "Possibly run tests with an emulator in the future"
+        return 0
+    fi
+
+    if test "$(uname)" != "Linux"
+    then
+        echo "Tests are temporarily skipped on non-Linux platforms"
+        return 0
+    fi
+
+    run_checks check
+
+    if test "$CHECK_VALGRIND" = "force"
+    then
+        run_checks check-valgrind
+    fi
+
+    if test "$DIST" != "skip"
+    then
+        $MAKE dist
+        $MAKE maintainer-check-extra-dist
+    fi
+
+    if test "$DISTCHECK" = "force"
+    then
+        $MAKE distcheck
+    fi
+}
+
+main "$@"

.gitlab-ci.yml
@@ -1,0 +1,83 @@
+variables:
+  GIT_DEPTH: 100
+  DOCKER_DRIVER: overlay2
+
+stages:
+  - containers
+  - builds
+
+.script_variables: &script_variables |
+  export MAKEFLAGS="-j $(getconf _NPROCESSORS_ONLN)"
+  export CCACHE_BASEDIR="$(pwd)"
+  export CCACHE_DIR="$CCACHE_BASEDIR/ccache"
+  export CCACHE_MAXSIZE="500M"
+  export PATH="$CCACHE_WRAPPERSDIR:$PATH"
+
+include: '/ci/gitlab.yml'
+
+.base_build_job:
+  stage: builds
+  cache:
+    paths:
+      - ccache/
+    key: "$CI_JOB_NAME"
+  before_script:
+    - *script_variables
+  script:
+    - ci/build.sh
+  artifacts:
+    paths:
+      - "config.log"
+      - "**/test-suite.log"
+    public: true
+    when: always
+
+.native_build_job:
+  extends:
+    - .base_build_job
+    - .gitlab_native_build_job
+
+.cross_build_job:
+  extends:
+    - .base_build_job
+    - .gitlab_cross_build_job
+
+# Jobs that we delegate to Cirrus CI because they require an operating
+# system other than Linux. These jobs will only run if the required
+# setup has been performed on the GitLab account (see ci/README.rst).
+#
+# The Cirrus CI configuration is generated by replacing target-specific
+# variables in a generic template: some of these variables are provided
+# when the GitLab CI job is defined, others are taken from a shell
+# snippet generated using lcitool.
+#
+# Note that the $PATH environment variable has to be treated with
+# special care, because we can't just override it at the GitLab CI job
+# definition level or we risk breaking it completely.
+.cirrus_build_job:
+  stage: builds
+  image: registry.gitlab.com/libvirt/libvirt-ci/cirrus-run:master
+  needs: []
+  script:
+    - source ci/cirrus/$NAME.vars
+    - sed -e "s|[@]CI_REPOSITORY_URL@|$CI_REPOSITORY_URL|g"
+          -e "s|[@]CI_COMMIT_REF_NAME@|$CI_COMMIT_REF_NAME|g"
+          -e "s|[@]CI_COMMIT_SHA@|$CI_COMMIT_SHA|g"
+          -e "s|[@]CIRRUS_VM_INSTANCE_TYPE@|$CIRRUS_VM_INSTANCE_TYPE|g"
+          -e "s|[@]CIRRUS_VM_IMAGE_SELECTOR@|$CIRRUS_VM_IMAGE_SELECTOR|g"
+          -e "s|[@]CIRRUS_VM_IMAGE_NAME@|$CIRRUS_VM_IMAGE_NAME|g"
+          -e "s|[@]UPDATE_COMMAND@|$UPDATE_COMMAND|g"
+          -e "s|[@]UPGRADE_COMMAND@|$UPGRADE_COMMAND|g"
+          -e "s|[@]INSTALL_COMMAND@|$INSTALL_COMMAND|g"
+          -e "s|[@]PATH@|$PATH_EXTRA${PATH_EXTRA:+:}\$PATH|g"
+          -e "s|[@]PKG_CONFIG_PATH@|$PKG_CONFIG_PATH|g"
+          -e "s|[@]PKGS@|$PKGS|g"
+          -e "s|[@]MAKE@|$MAKE|g"
+          -e "s|[@]PYTHON@|$PYTHON|g"
+          -e "s|[@]PIP3@|$PIP3|g"
+          -e "s|[@]PYPI_PKGS@|$PYPI_PKGS|g"
+      <ci/cirrus/build.yml >ci/cirrus/$NAME.yml
+    - cat ci/cirrus/$NAME.yml
+    - cirrus-run -v --show-build-log always ci/cirrus/$NAME.yml
+  rules:
+    - if: "$CIRRUS_GITHUB_REPO && $CIRRUS_API_TOKEN"

Makefile.am
@@ -119,7 +119,11 @@ maintainer-check-extra-dist:
 	@zcat $(PACKAGE_NAME)-$(VERSION).tar.gz | tar tf - | sort | \
 	  sed 's,^$(PACKAGE_NAME)-$(VERSION)/,,' > tarfiles
 	@git ls-files | \
-	  sort > gitfiles
+      grep -v \
+	    -e '^\.cirrus.yml' \
+	    -e '^\.gitlab-ci.yml' \
+	    -e '^ci/' | \
+	    sort > gitfiles
 	@comm -13 tarfiles gitfiles > comm-out
 	@echo Checking for differences between EXTRA_DIST and git ...
 	@cat comm-out

ci/cirrus/build.yml
@@ -1,0 +1,24 @@
+@CIRRUS_VM_INSTANCE_TYPE@:
+  @CIRRUS_VM_IMAGE_SELECTOR@: @CIRRUS_VM_IMAGE_NAME@
+
+env:
+  CI_REPOSITORY_URL: "@CI_REPOSITORY_URL@"
+  CI_COMMIT_REF_NAME: "@CI_COMMIT_REF_NAME@"
+  CI_COMMIT_SHA: "@CI_COMMIT_SHA@"
+  PATH: "@PATH@"
+  PKG_CONFIG_PATH: "@PKG_CONFIG_PATH@"
+  PYTHON: "@PYTHON@"
+  MAKE: "@MAKE@"
+
+build_task:
+  install_script:
+    - @UPDATE_COMMAND@
+    - @UPGRADE_COMMAND@
+    - @INSTALL_COMMAND@ @PKGS@
+    - if test -n "@PYPI_PKGS@" ; then @PIP3@ install @PYPI_PKGS@ ; fi
+  clone_script:
+    - git clone --depth 100 "$CI_REPOSITORY_URL" .
+    - git fetch origin "$CI_COMMIT_REF_NAME"
+    - git reset --hard "$CI_COMMIT_SHA"
+  build_script:
+    - ci/build.sh

ci/cirrus/freebsd-12.vars
@@ -1,0 +1,16 @@
+# THIS FILE WAS AUTO-GENERATED
+#
+#  $ lcitool manifest ci/manifest.yml
+#
+# https://gitlab.com/libvirt/libvirt-ci
+
+CCACHE='/usr/local/bin/ccache'
+CPAN_PKGS=''
+CROSS_PKGS=''
+MAKE='/usr/local/bin/gmake'
+NINJA='/usr/local/bin/ninja'
+PACKAGING_COMMAND='pkg'
+PIP3='/usr/local/bin/pip-3.8'
+PKGS='autoconf automake bash bash-completion ca_root_nss ccache cdrkit-genisoimage curl e2fsprogs expect git gmake gnutls go gzip iconv jq libssh libtool libtorrent libvirt lua54 ocaml p5-Pod-Simple perl5 perl5-devel py38-boto3 py38-flake8 python3 qemu rust socat tcl87 xorriso zstd'
+PYPI_PKGS=''
+PYTHON='/usr/local/bin/python3'

ci/cirrus/freebsd-13.vars
@@ -1,0 +1,16 @@
+# THIS FILE WAS AUTO-GENERATED
+#
+#  $ lcitool manifest ci/manifest.yml
+#
+# https://gitlab.com/libvirt/libvirt-ci
+
+CCACHE='/usr/local/bin/ccache'
+CPAN_PKGS=''
+CROSS_PKGS=''
+MAKE='/usr/local/bin/gmake'
+NINJA='/usr/local/bin/ninja'
+PACKAGING_COMMAND='pkg'
+PIP3='/usr/local/bin/pip-3.8'
+PKGS='autoconf automake bash bash-completion ca_root_nss ccache cdrkit-genisoimage curl e2fsprogs expect git gmake gnutls go gzip iconv jq libssh libtool libtorrent libvirt lua54 ocaml p5-Pod-Simple perl5 perl5-devel py38-boto3 py38-flake8 python3 qemu rust socat tcl87 xorriso zstd'
+PYPI_PKGS=''
+PYTHON='/usr/local/bin/python3'

ci/cirrus/freebsd-current.vars
@@ -1,0 +1,16 @@
+# THIS FILE WAS AUTO-GENERATED
+#
+#  $ lcitool manifest ci/manifest.yml
+#
+# https://gitlab.com/libvirt/libvirt-ci
+
+CCACHE='/usr/local/bin/ccache'
+CPAN_PKGS=''
+CROSS_PKGS=''
+MAKE='/usr/local/bin/gmake'
+NINJA='/usr/local/bin/ninja'
+PACKAGING_COMMAND='pkg'
+PIP3='/usr/local/bin/pip-3.8'
+PKGS='autoconf automake bash bash-completion ca_root_nss ccache cdrkit-genisoimage curl e2fsprogs expect git gmake gnutls go gzip iconv jq libssh libtool libtorrent libvirt lua54 ocaml p5-Pod-Simple perl5 perl5-devel py38-boto3 py38-flake8 python3 qemu rust socat tcl87 xorriso zstd'
+PYPI_PKGS=''
+PYTHON='/usr/local/bin/python3'

ci/cirrus/macos-11.vars
@@ -1,0 +1,16 @@
+# THIS FILE WAS AUTO-GENERATED
+#
+#  $ lcitool manifest ci/manifest.yml
+#
+# https://gitlab.com/libvirt/libvirt-ci
+
+CCACHE='/usr/local/bin/ccache'
+CPAN_PKGS='ExtUtils::Embed'
+CROSS_PKGS=''
+MAKE='/usr/local/bin/gmake'
+NINJA='/usr/local/bin/ninja'
+PACKAGING_COMMAND='brew'
+PIP3='/usr/local/bin/pip3'
+PKGS='autoconf automake bash bash-completion ccache cpanminus curl e2fsprogs expect flake8 git glibc-common gnutls golang gzip jq kernel-headers libcom_err libnbd libssh libtool libtorrent libvirt losetup lua make ocaml perl perl5-devel podman python3 python3-boto3 python3-libnbd qemu rust sfdisk socat tcl xorriso xz zlib zstd'
+PYPI_PKGS=''
+PYTHON='/usr/local/bin/python3'

commit a4359a33c9a431e122c8ec2adb84e5cad292dfdb

ci: Don't skip tests on non-Linux
Since we added CI tests in commit 32b395a770 ("Add CI for GitLab") we
have skipped the tests on non-Linux platforms (FreeBSD and macOS being
the only ones in our CI currently).  The tests ought to work on these
platforms so let's actually run them.

Fixes: commit 32b395a7709d028df6787aaefd4ced3f07da3c02

ci/build.sh
@@ -146,12 +146,6 @@ $CONFIG_ARGS
         return 0
     fi
 
-    if test "$(uname)" != "Linux"
-    then
-        echo "Tests are temporarily skipped on non-Linux platforms"
-        return 0
-    fi
-
     run_checks check
 
     # if test "$CHECK_VALGRIND" = "force"