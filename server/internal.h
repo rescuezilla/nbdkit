rescuezilla/nbdkit

commit f83bb4f98e763344a6246947ce121a6274095647

server: Implement --timeout option

server/internal.h
@@ -139,6 +139,7 @@ extern const char *run;
 extern bool listen_stdin;
 extern const char *selinux_label;
 extern unsigned threads;
+extern unsigned timeout_secs, timeout_nsecs;
 extern int tls;
 extern const char *tls_certificates_dir;
 extern const char *tls_psk;
@@ -191,6 +192,13 @@ extern void run_command (void);
 /* socket-activation.c */
 #define FIRST_SOCKET_ACTIVATION_FD 3 /* defined by systemd ABI */
 extern unsigned int get_socket_activation (void);
+
+/* timeout.c */
+struct connection;
+extern int start_timeout (struct connection *conn)
+  __attribute__ ((__nonnull__ (1)));
+extern void cancel_timeout (struct connection *conn)
+  __attribute__ ((__nonnull__ (1)));
 
 /* usergroup.c */
 extern void change_user (void);
@@ -281,6 +289,10 @@ struct connection {
   uint32_t cflags;
   uint16_t eflags;
   bool handshake_complete;
+#ifdef HAVE_TIMER_CREATE
+  timer_t timer;
+  bool timer_set;
+#endif
   bool using_tls;
   bool structured_replies;
   bool meta_context_base_allocation;

server/main.c
@@ -112,6 +112,7 @@ bool listen_stdin;              /* -s */
 const char *selinux_label;      /* --selinux-label */
 bool swap;                      /* --swap */
 unsigned threads;               /* -t */
+unsigned timeout_secs, timeout_nsecs; /* --timeout */
 int tls;                        /* --tls : 0=off 1=on 2=require */
 const char *tls_certificates_dir; /* --tls-certificates */
 const char *tls_psk;            /* --tls-psk */
@@ -397,6 +398,18 @@ main (int argc, char *argv[])
     case SWAP_OPTION:
       swap = 1;
       break;
+
+    case TIMEOUT_OPTION:
+#ifdef HAVE_TIMER_CREATE
+      if (nbdkit_parse_delay ("timeout", optarg,
+                              &timeout_secs, &timeout_nsecs) == -1)
+        exit (EXIT_FAILURE);
+      break;
+#else
+      fprintf (stderr, "%s: --timeout is not supported on this platform\n",
+               program_name);
+      exit (EXIT_FAILURE);
+#endif
 
     case TLS_OPTION:
       tls_set_on_cli = true;

server/timeout.c
@@ -1,0 +1,144 @@
+/* nbdkit
+ * Copyright Red Hat
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <time.h>
+#include <assert.h>
+
+#include "internal.h"
+
+#ifdef HAVE_TIMER_CREATE
+
+#include <sys/socket.h>
+
+static void
+connection_timeout (union sigval si)
+{
+  struct connection *conn = si.sival_ptr;
+
+  assert (conn != NULL);
+
+  nbdkit_debug ("connection timed out");
+
+  /* Note this is called asynchronously from a different thread,
+   * making it difficult to do this safely.  The theory here is that
+   * since all connection operations are protected by a lock, we'll
+   * take that lock and check that conn->magic is valid (to ensure it
+   * hasn't been freed already).  However we still must do the minimum
+   * possible, since raw socket or gnutls operations may be taking
+   * place simultaneously.  The safest seems to be just to call
+   * shutdown(2) on the socket.  Calling close(2) is less safe as it
+   * might cause fd reuse.
+   */
+  lock_connection ();
+  if (conn->magic == CONN_MAGIC &&
+      conn->timer_set &&
+      conn->status == STATUS_ACTIVE &&
+      conn->sockout >= 0) {
+    shutdown (conn->sockout, SHUT_RDWR);
+    conn->status = STATUS_DEAD;
+  }
+  unlock_connection ();
+}
+
+int
+start_timeout (struct connection *conn)
+{
+  if (timeout_secs == 0 && timeout_nsecs == 0)
+    return 0;
+
+  struct sigevent sev = {
+    .sigev_notify = SIGEV_THREAD,
+    .sigev_value.sival_ptr = conn,
+    .sigev_notify_function = connection_timeout,
+  };
+  struct itimerspec its = {
+    .it_value.tv_sec = timeout_secs,
+    .it_value.tv_nsec = timeout_nsecs,
+    .it_interval.tv_sec = 0, /* 0 = don't repeat */
+    .it_interval.tv_nsec = 0,
+  };
+
+  if (timer_create (CLOCK_MONOTONIC, &sev, &conn->timer) == -1) {
+    nbdkit_error ("timeout: timer_create: %m");
+    return -1;
+  }
+
+  if (timer_settime (conn->timer, 0, &its, NULL) == -1) {
+    nbdkit_error ("timeout: timer_settime: %m");
+    timer_delete (conn->timer);
+    return -1;
+  }
+
+  /* timer_t is actually a void * in glibc, but there seems to be no
+   * guarantee that '0' means "no timer" (eg. if it was an integral
+   * type in a different implementation).  Therefore record that we
+   * set it and will need to delete it later.
+   */
+  conn->timer_set = true;
+
+  return 0;
+}
+
+void
+cancel_timeout (struct connection *conn)
+{
+  if (timeout_secs == 0 && timeout_nsecs == 0)
+    return;
+
+  if (conn->timer_set) {
+    timer_delete (conn->timer);
+    conn->timer_set = false;
+  }
+}
+
+#else /* !HAVE_TIMER_CREATE */
+
+int
+start_timeout (struct connection *conn)
+{
+  return 0;
+}
+
+void
+cancel_timeout (struct connection *conn)
+{
+  return;
+}
+
+#endif /* !HAVE_TIMER_CREATE */
+

configure.ac
@@ -426,6 +426,17 @@ AC_CHECK_FUNCS([\
         posix_memalign \
         valloc])
 
+dnl Check for timer_create
+dnl It may require -lrt.
+old_LIBS="$LIBS"
+AC_SEARCH_LIBS([timer_create], [rt], [
+        AS_IF([test "x$ac_cv_search_timer_create" != "xnone required"],
+            [RT_LIBS="$ac_cv_search_timer_create"], [RT_LIBS=])
+        AC_SUBST([RT_LIBS])
+    ], [])
+AC_CHECK_FUNCS([timer_create])
+LIBS="$old_LIBS"
+
 dnl Check for structs and members.
 AC_CHECK_MEMBERS([struct dirent.d_type], [], [], [[#include <dirent.h>]])
 AC_CHECK_MEMBERS([struct ucred.uid], [], [],

docs/nbdkit.pod
@@ -474,6 +474,25 @@ once.  Only matters for plugins with thread_model=parallel (where it
 defaults to 16).  To force serialized behavior (useful if the client
 is not prepared for out-of-order responses), set this to 1.
 
+=item B<--timeout> TIMEOUT
+
+If the client does not negotiate a full NBD session within C<TIMEOUT>
+seconds, then drop the connection.  This can be used as an additional
+security measure to ensure that clients don't consume resources by
+opening lots of connections and then not negotiating (or negotiating
+slowly) the full NBD session.  It is particularly useful in
+conjunction with TLS client authentication (see L<nbdkit-tls(1)>).
+
+The timeout is measured from the point of the initial TCP connection
+to the point where NBD session negotiation ends (including TLS, if
+used).  If the timeout is exceeded during this negotiation, the
+connection is dropped.  Setting this too low will cause problems for
+clients listing lots of exports.
+
+The default is I<no timeout>.
+
+C<TIMEOUT> can be any format supported by L<nbdkit_parse_delay(3)>.
+
 =item B<--tls=off>
 
 =item B<--tls=on>

server/Makefile.am
@@ -64,6 +64,7 @@ nbdkit_SOURCES = \
 	socket-activation.c \
 	sockets.c \
 	threadlocal.c \
+	timeout.c \
 	uri.c \
 	usergroup.c \
 	vfprintf.c \
@@ -100,6 +101,7 @@ nbdkit_LDADD = \
 	$(GNUTLS_LIBS) \
 	$(LIBSELINUX_LIBS) \
 	$(DL_LIBS) \
+	$(RT_LIBS) \
 	$(top_builddir)/common/protocol/libprotocol.la \
 	$(top_builddir)/common/utils/libutils.la \
 	$(top_builddir)/common/replacements/libcompat.la \

server/connections.c
@@ -172,6 +172,13 @@ handle_single_connection (int sockin, int sockout)
   if (top->preconnect (top, read_only) == -1)
     goto done;
 
+  /* Start the timeout just before the NBD handshake, but after
+   * preconnect (so we don't bother to create a timer if the client
+   * was rejected by IP rules etc).
+   */
+  if (start_timeout (conn) == -1)
+    goto done;
+
   /* NBD handshake.
    *
    * Note that this calls the backend .open callback when it is safe
@@ -180,6 +187,8 @@ handle_single_connection (int sockin, int sockout)
   if (protocol_handshake () == -1)
     goto done;
   conn->handshake_complete = true;
+
+  cancel_timeout (conn);
 
   if (!nworkers) {
     /* No need for a separate thread. */
@@ -359,6 +368,8 @@ free_connection (struct connection *conn)
 
   if (!conn)
     return;
+
+  cancel_timeout (conn);
 
   conn->close (SHUT_RDWR);
 

server/options.h
@@ -54,6 +54,7 @@ enum {
   SELINUX_LABEL_OPTION,
   SHORT_OPTIONS_OPTION,
   SWAP_OPTION,
+  TIMEOUT_OPTION,
   TLS_OPTION,
   TLS_CERTIFICATES_OPTION,
   TLS_PSK_OPTION,
@@ -106,6 +107,8 @@ static const struct option long_options[] = {
   { "stdin",            no_argument,       NULL, 's' },
   { "swap",             no_argument,       NULL, SWAP_OPTION },
   { "threads",          required_argument, NULL, 't' },
+  { "timeout",          required_argument, NULL, TIMEOUT_OPTION },
+  { "time-out",         required_argument, NULL, TIMEOUT_OPTION },
   { "tls",              required_argument, NULL, TLS_OPTION },
   { "tls-certificates", required_argument, NULL, TLS_CERTIFICATES_OPTION },
   { "tls-psk",          required_argument, NULL, TLS_PSK_OPTION },

commit 9634abcd43e6b2144abb1a300af9d77431a37967

openbsd: server: Fix detection of --timeout option
OpenBSD has timer_create and related functions defined, but they just
return -ENOSYS, breaking configure.  We have to be smarter about
detecting if this feature is available.

Fixes: commit f83bb4f98e763344a6246947ce121a6274095647

server/internal.h
@@ -42,6 +42,20 @@
 #include <sys/socket.h>
 #endif
 
+/* Do we have the --timeout option?
+ *
+ * OpenBSD defines timer_create as a function that returns -ENOSYS,
+ * which breaks configure.  So we have to check for SIGEV_THREAD being
+ * defined as well.
+ * https://github.com/openbsd/src/blob/master/lib/libc/sys/timer_create.c
+ */
+#include <signal.h>
+#include <time.h>
+
+#if defined(HAVE_TIMER_CREATE) && defined(SIGEV_THREAD)
+#define HAVE_TIMEOUT_OPTION 1
+#endif
+
 /* Server supports API versions between 1 and MAX_API_VERSION */
 #define MAX_API_VERSION 2
 
@@ -290,7 +304,7 @@ struct connection {
   uint32_t cflags;
   uint16_t eflags;
   bool handshake_complete;
-#ifdef HAVE_TIMER_CREATE
+#ifdef HAVE_TIMEOUT_OPTION
   timer_t timer;
   bool timer_set;
 #endif

server/main.c
@@ -416,7 +416,7 @@ main (int argc, char *argv[])
       break;
 
     case TIMEOUT_OPTION:
-#ifdef HAVE_TIMER_CREATE
+#ifdef HAVE_TIMEOUT_OPTION
       if (nbdkit_parse_delay ("timeout", optarg,
                               &timeout_secs, &timeout_nsecs) == -1)
         exit (EXIT_FAILURE);

server/timeout.c
@@ -41,7 +41,7 @@
 
 #include "internal.h"
 
-#ifdef HAVE_TIMER_CREATE
+#ifdef HAVE_TIMEOUT_OPTION
 
 #include <sys/socket.h>
 
@@ -126,7 +126,7 @@ cancel_timeout (struct connection *conn)
   }
 }
 
-#else /* !HAVE_TIMER_CREATE */
+#else /* !HAVE_TIMEOUT_OPTION */
 
 int
 start_timeout (struct connection *conn)
@@ -140,5 +140,4 @@ cancel_timeout (struct connection *conn)
   return;
 }
 
-#endif /* !HAVE_TIMER_CREATE */
-
+#endif /* !HAVE_TIMEOUT_OPTION */