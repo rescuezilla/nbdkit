rescuezilla/nbdkit

commit ecf6b15fa84a02b74ea969f06552c82ee418b9b4

file: Add more debugging when -D file.zero=1 is used

plugins/file/file.c
@@ -943,7 +943,17 @@ file_pwrite (void *handle, const void *buf, uint32_t count, uint64_t offset,
 static int
 do_fallocate (int fd, int mode_, off_t offset, off_t len)
 {
-  int r = fallocate (fd, mode_, offset, len);
+  int r;
+
+  r = fallocate (fd, mode_, offset, len);
+
+  if (file_debug_zero)
+    nbdkit_debug ("fallocate ([%s%s ], %" PRIu64 ", %" PRIu64") => %d (%d)",
+                  mode_ & FALLOC_FL_PUNCH_HOLE ? " FALLOC_FL_PUNCH_HOLE" : "",
+                  mode_ & FALLOC_FL_ZERO_RANGE ? " FALLOC_FL_ZERO_RANGE" : "",
+                  (uint64_t) offset, (uint64_t) len, r,
+                  r == -1 ? errno : 0);
+
   if (r == -1 && errno == ENODEV) {
     /* kernel 3.10 fails with ENODEV for block device. Kernel >= 4.9 fails
        with EOPNOTSUPP in this case. Normalize errno to simplify callers. */

commit 419a347054f81c53706637feddbc5008beab77d3

file: Fix do_fallocate debugging on Alpine
Alpine has some weird/old kernel that doesn't support
FALLOC_FL_ZERO_RANGE but does support FALLOC_FL_PUNCH_HOLE, so the
debugging I added in commit ecf6b15fa8 failed to compile with:

  file.c: In function 'do_fallocate':
  file.c:958:27: error: 'FALLOC_FL_ZERO_RANGE' undeclared (first use in this function)
    958 |                   mode_ & FALLOC_FL_ZERO_RANGE ? " FALLOC_FL_ZERO_RANGE" : "",
        |                           ^~~~~~~~~~~~~~~~~~~~
  file.c:958:27: note: each undeclared identifier is reported only once for each function it appears in
  make[3]: *** [Makefile:666: nbdkit_file_plugin_la-file.lo] Error 1

Fixes: commit ecf6b15fa84a02b74ea969f06552c82ee418b9b4

plugins/file/file.c
@@ -939,7 +939,7 @@ file_pwrite (void *handle, const void *buf, uint32_t count, uint64_t offset,
   return 0;
 }
 
-#if defined (FALLOC_FL_PUNCH_HOLE) || defined (FALLOC_FL_ZERO_RANGE)
+#if defined(FALLOC_FL_PUNCH_HOLE) || defined(FALLOC_FL_ZERO_RANGE)
 static int
 do_fallocate (int fd, int mode_, off_t offset, off_t len)
 {
@@ -948,9 +948,20 @@ do_fallocate (int fd, int mode_, off_t offset, off_t len)
   r = fallocate (fd, mode_, offset, len);
 
   if (file_debug_zero)
-    nbdkit_debug ("fallocate ([%s%s ], %" PRIu64 ", %" PRIu64") => %d (%d)",
+    nbdkit_debug ("fallocate (["
+#if defined(FALLOC_FL_PUNCH_HOLE)
+                  "%s"
+#endif
+#if defined(FALLOC_FL_ZERO_RANGE)
+                  "%s"
+#endif
+                  " ], %" PRIu64 ", %" PRIu64") => %d (%d)",
+#if defined(FALLOC_FL_PUNCH_HOLE)
                   mode_ & FALLOC_FL_PUNCH_HOLE ? " FALLOC_FL_PUNCH_HOLE" : "",
+#endif
+#if defined(FALLOC_FL_ZERO_RANGE)
                   mode_ & FALLOC_FL_ZERO_RANGE ? " FALLOC_FL_ZERO_RANGE" : "",
+#endif
                   (uint64_t) offset, (uint64_t) len, r,
                   r == -1 ? errno : 0);
 