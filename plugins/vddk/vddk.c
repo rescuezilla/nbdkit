rescuezilla/nbdkit

commit 7c90116664b1b1a3c3756d6a79d6d483bc6062dd

vddk: Move minimum version of VDDK to 6.7
We can now assume that QueryAllocatedBlocks exists, as well as a
couple of other functions, simplifying the code.

This was released in 2018 (7 years ago) so there's been plenty of time
to upgrade.

plugins/vddk/vddk.c
@@ -705,37 +705,6 @@ vddk_dump_plugin (void)
 /* Lock protecting open/close calls - see above. */
 static pthread_mutex_t open_close_lock = PTHREAD_MUTEX_INITIALIZER;
 
-static inline VixDiskLibConnectParams *
-allocate_connect_params (void)
-{
-  VixDiskLibConnectParams *ret;
-
-  if (VixDiskLib_AllocateConnectParams != NULL) {
-    VDDK_CALL_START (VixDiskLib_AllocateConnectParams, "")
-      ret = VixDiskLib_AllocateConnectParams ();
-    VDDK_CALL_END (VixDiskLib_AllocateConnectParams, 0);
-  }
-  else
-    ret = calloc (1, sizeof (VixDiskLibConnectParams));
-
-  return ret;
-}
-
-static inline void
-free_connect_params (VixDiskLibConnectParams *params)
-{
-  /* Only use FreeConnectParams if AllocateConnectParams was
-   * originally called.  Otherwise use free.
-   */
-  if (VixDiskLib_AllocateConnectParams != NULL) {
-    VDDK_CALL_START (VixDiskLib_FreeConnectParams, "params")
-      VixDiskLib_FreeConnectParams (params);
-    VDDK_CALL_END (VixDiskLib_FreeConnectParams, 0);
-  }
-  else
-    free (params);
-}
-
 /* Create the per-connection handle. */
 static void *
 vddk_open (int readonly)
@@ -797,7 +766,9 @@ vddk_open (int readonly)
   }
   assert (h->filename != NULL);
 
-  h->params = allocate_connect_params ();
+  VDDK_CALL_START (VixDiskLib_AllocateConnectParams, "")
+    h->params = VixDiskLib_AllocateConnectParams ();
+  VDDK_CALL_END (VixDiskLib_AllocateConnectParams, 0);
   if (h->params == NULL) {
     nbdkit_error ("allocate VixDiskLibConnectParams: %m");
     goto err0;
@@ -937,7 +908,9 @@ vddk_open (int readonly)
     VixDiskLib_Disconnect (h->connection);
   VDDK_CALL_END (VixDiskLib_Disconnect, 0);
  err1:
-  free_connect_params (h->params);
+  VDDK_CALL_START (VixDiskLib_FreeConnectParams, "params")
+    VixDiskLib_FreeConnectParams (h->params);
+  VDDK_CALL_END (VixDiskLib_FreeConnectParams, 0);
  err0:
   pthread_mutex_destroy (&h->commands_lock);
   pthread_cond_destroy (&h->commands_cond);
@@ -963,7 +936,9 @@ vddk_close (void *handle)
     VixDiskLib_Disconnect (h->connection);
   VDDK_CALL_END (VixDiskLib_Disconnect, 0);
 
-  free_connect_params (h->params);
+  VDDK_CALL_START (VixDiskLib_FreeConnectParams, "params")
+    VixDiskLib_FreeConnectParams (h->params);
+  VDDK_CALL_END (VixDiskLib_FreeConnectParams, 0);
   pthread_mutex_destroy (&h->commands_lock);
   pthread_cond_destroy (&h->commands_cond);
   command_queue_reset (&h->commands);

plugins/vddk/nbdkit-vddk-plugin.pod
@@ -430,15 +430,13 @@ is I<not> recommended.
 
 =over 4
 
-=item VDDK E<ge> 6.5 (released Nov 2016)
+=item VDDK E<ge> 6.7 (released Apr 2018)
 
 This is the minimum version of VDDK supported.  Older versions will
 not work.
 
-=item VDDK 6.7 (released Apr 2018)
-
-This is the first version that supported the
-C<VixDiskLib_QueryAllocatedBlocks> API.  This is required to provide
+This is also the first version that supported the
+C<VixDiskLib_QueryAllocatedBlocks> API.  This is used to provide
 sparseness (extent) information over NBD.
 
 =item VDDK 8.0.3 (released Jun 2024)

plugins/vddk/vddk-stubs.h
@@ -132,16 +132,22 @@ STUB (VixDiskLib_Wait,
       VixError,
       (VixDiskLibHandle handle));
 
-/* Added in VDDK 6.7, these will be NULL for earlier versions: */
-OPTIONAL_STUB (VixDiskLib_QueryAllocatedBlocks,
-               VixError,
-               (VixDiskLibHandle diskHandle,
-                uint64_t start_sector, uint64_t nr_sectors,
-                uint64_t chunk_size,
-                VixDiskLibBlockList **block_list));
-OPTIONAL_STUB (VixDiskLib_FreeBlockList,
-               VixError,
-               (VixDiskLibBlockList *block_list));
-OPTIONAL_STUB (VixDiskLib_AllocateConnectParams,
-               VixDiskLibConnectParams *,
-               (void));
+/* Added in VDDK 6.7. */
+STUB (VixDiskLib_QueryAllocatedBlocks,
+      VixError,
+      (VixDiskLibHandle diskHandle,
+       uint64_t start_sector, uint64_t nr_sectors,
+       uint64_t chunk_size,
+       VixDiskLibBlockList **block_list));
+STUB (VixDiskLib_FreeBlockList,
+      VixError,
+      (VixDiskLibBlockList *block_list));
+STUB (VixDiskLib_AllocateConnectParams,
+      VixDiskLibConnectParams *,
+      (void));
+
+/* OPTIONAL_STUB can be used to add new APIs which are not supported
+ * by older versions of VDDK, and to make them NULL if not present.
+ * However VDDK >= 6.7 has everything we need for now so we are no
+ * longer using this macro.
+ */

plugins/vddk/worker.c
@@ -309,23 +309,13 @@ do_can_extents (struct command *cmd, struct vddk_handle *h)
   VixError err;
   VixDiskLibBlockList *block_list;
 
-  /* This call was added in VDDK 6.7.  In earlier versions the
-   * function pointer will be NULL and we cannot query extents.
-   */
-  if (VixDiskLib_QueryAllocatedBlocks == NULL) {
-    nbdkit_debug ("can_extents: VixDiskLib_QueryAllocatedBlocks == NULL, "
-                  "probably this is VDDK < 6.7");
-    return 0;
-  }
-
   /* Suppress errors around this call.  See:
    * https://bugzilla.redhat.com/show_bug.cgi?id=1709211#c7
    */
   error_suppression = 1;
 
-  /* However even when the call is available it rarely works well so
-   * the best thing we can do here is to try the call and if it's
-   * non-functional return false.
+  /* Try the QueryAllocatedBlocks call and if it's non-functional
+   * return false.
    */
   VDDK_CALL_START (VixDiskLib_QueryAllocatedBlocks,
                    "handle, 0, %d sectors, %d sectors",

tests/dummy-vddk.c
@@ -116,11 +116,52 @@ VixDiskLib_FreeErrorText (char *text)
   free (text);
 }
 
+NBDKIT_DLL_PUBLIC VixError
+VixDiskLib_QueryAllocatedBlocks (VixDiskLibHandle diskHandle,
+                                 uint64_t start_sector, uint64_t nr_sectors,
+                                 uint64_t chunk_size,
+                                 VixDiskLibBlockList **block_list)
+{
+  VixDiskLibBlockList *ret;
+
+  /* This is safe because ret->blocks is a 1-sized array and we only
+   * use 1 entry here.
+   */
+  ret = calloc (1, sizeof *ret);
+  if (ret == NULL)
+    abort ();
+
+  /* Pretend it's all allocated. */
+  ret->numBlocks = 1;
+  ret->blocks[0].offset = start_sector;
+  ret->blocks[0].length = nr_sectors;
+
+  *block_list = ret;
+  return VIX_OK;
+}
+
+NBDKIT_DLL_PUBLIC VixError
+VixDiskLib_FreeBlockList (VixDiskLibBlockList *block_list)
+{
+  free (block_list);
+  return VIX_OK;
+}
+
+NBDKIT_DLL_PUBLIC VixDiskLibConnectParams *
+VixDiskLib_AllocateConnectParams (void)
+{
+  VixDiskLibConnectParams *ret;
+
+  ret = calloc (1, sizeof *ret);
+  if (ret == NULL)
+    abort ();
+  return ret;
+}
+
 NBDKIT_DLL_PUBLIC void
 VixDiskLib_FreeConnectParams (VixDiskLibConnectParams *params)
 {
-  /* never called since we don't define optional AllocateConnectParams */
-  abort ();
+  free (params);
 }
 
 NBDKIT_DLL_PUBLIC VixError

commit 365184470e03f53a07214b67a2e7d648acee564c

vddk: Fix VDDK version in error message and some comments
Fixes: commit 7c90116664b1b1a3c3756d6a79d6d483bc6062dd

plugins/vddk/vddk.c
@@ -421,7 +421,7 @@ static void
 missing_required_symbol (const char *fn)
 {
   nbdkit_error ("required VDDK symbol \"%s\" is missing. "
-                "VDDK version must be >= 6.5. "
+                "VDDK version must be >= 6.7. "
                 "See nbdkit-vddk-plugin(1) man page "
                 "section \"SUPPORTED VERSIONS OF VDDK\". "
                 "Original dlopen error: %s\n",
@@ -440,7 +440,7 @@ load_library (bool load_error_is_fatal)
     /* Prefer the newest library in case multiple exist.  Check two
      * possible directories: the usual VDDK installation puts .so
      * files in an arch-specific subdirectory of $libdir (our minimum
-     * supported version is VDDK 6.5, which only supports x64-64); but
+     * supported version is VDDK 6.7, which only supports x64-64); but
      * our testsuite is easier to write if we point libdir directly to
      * a stub .so.
      */
@@ -1006,7 +1006,7 @@ vddk_block_size (void *handle,
   return 0;
 }
 
-/* The Flush call was added in VDDK 6.0, since we support minimum 6.5
+/* The Flush call was added in VDDK 6.0, since we support minimum 6.7
  * we are always able to do FUA / flush.
  */
 static int