9bad2c1d4485aa96761fe3bc89d48609be0e4471
Richard W.M. Jones <rjones@redhat.com>
2025-07-22 16:58:06 +0100

    common: utils: vector: Add new vector_uniq function
    It's like the uniq(1) Unix utility.

--- common/utils/vector.h
+++ common/utils/vector.h
@@ -205,6 +205,21 @@
     qsort (v->ptr, v->len, sizeof (type), (void *) compare);            \
   }                                                                     \
                                                                         \
+  /* Remove duplicate adjacent elements (uniq). */                      \
+  static inline void __attribute__ ((__unused__))                       \
+  name##_uniq (name *v,                                                 \
+               int (*compare) (const type *p1, const type *p2))         \
+  {                                                                     \
+    size_t i;                                                           \
+    for (i = 0; i < v->len - 1; ++i) {                                  \
+      if (compare ((const type *) &v->ptr[i],                           \
+                   (const type *) &v->ptr[i+1]) == 0) {                 \
+        name##_remove (v, i);                                           \
+        i--;                                                            \
+      }                                                                 \
+    }                                                                   \
+  }                                                                     \
+                                                                        \
   /* Search for an exactly matching element in the vector using a       \
    * binary search.  Returns a pointer to the element or NULL.          \
    */                                                                   \
